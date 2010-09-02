5a854b0165a2cfad70052b71ea567e7b16cc349d
unknown <wb329102@.wb.ad.worldbank.org>
2010-09-01 23:21:32 -0400

    added RDF export Icon added Data access form icon

--- application/views/catalog/survey_info.php
+++ application/views/catalog/survey_info.php
@@ -31,27 +31,42 @@
         		<img border="0" align="absbottom" src="images/page_attach.png"/> <?php echo t('resource_manager');?>
         	</a>
         </li>  
-<!--
-        <li>                	
-        	<a title="<?php echo t('file_manager');?>" href="<?php echo site_url()."/filemanager/datasets/$dirpath/"; ?>" onclick="IFrameDialog(this);return false;">
-        		<img border="0" align="absbottom" src="images/folder_page_white.png"/> <?php echo t('file_manager');?>
-        	</a>
-        </li>  
--->        
         <li>
         	<a title="<?php echo t('browse_metadata');?>" target="_blank" href="<?php echo site_url();?>/ddibrowser/<?php echo $id;?>" >
         		<img border="0" align="absbottom" src="images/page_white_cd.png"/> <?php echo t('browse_metadata');?> 
         	</a>
         </li>                        
-        <li class="float-right">    	
-        	<a title="<?php echo t('delete');?>" style="cursor: pointer;" href="<?php echo site_url().'/admin/catalog/delete/'.$id; ?>">
-        		<img border="0" align="absbottom" src="images/bin_closed.png"/> <?php echo t('delete');?>
-            </a>
-        </li>
         <li>
             <div class="field" style="display:inline;">
                 <label><input type="checkbox" name="isshared" id="isshared" value="1" onclick="share_ddi(this,<?php echo $id;?>);" <?php echo (get_form_value('isshared',isset($isshared) ? $isshared: '') ? 'checked="checked"' : '') ; ?> /> <?php echo t('share_ddi_w_harvester'); ?></label>
             </div>        
         </li>                  
+        <li class="float-right">
+        	<a title="<?php echo t('delete');?>" style="cursor: pointer;" href="<?php echo site_url().'/admin/catalog/delete/'.$id; ?>">
+        		<img border="0" align="absbottom" src="images/bin_closed.png"/> <?php echo t('delete');?>
+            </a>
+        </li>
     </ul> 
+<span style="float:right;padding-right:10px;">
+	<?php if($model=='direct'): ?>
+        <a href="<?php echo site_url().'/access_direct/'.$id;?>" class="accessform" title="<?php echo t('link_data_direct_hover');?>">
+        <span><img src="images/form_direct.gif" /></span>
+        </a>                    
+    <?php elseif($model=='public'): ?>                    
+        <a href="<?php echo site_url().'/access_public/'.$id;?>" class="accessform"  title="<?php echo t('link_data_public_hover');?>">
+        <span><img src="images/form_public.gif" /></span>
+        </a>                    
+    <?php elseif($model=='licensed'): ?>
+        <a href="<?php echo site_url().'/access_licensed/'.$id;?>" class="accessform"  title="<?php echo t('link_data_licensed_hover');?>">
+        <span><img src="images/form_licensed.gif" /></span>
+        </a>                    
+    <?php elseif($model=='data_enclave'): ?>
+        <a href="<?php echo site_url().'/access_enclave/'.$id;?>" class="accessform"  title="<?php echo t('link_data_enclave_hover');?>">
+        <span><img src="images/form_enclave.gif" /></span>
+        </a>                    
+    <?php endif; ?>    
+        <a href="<?php echo site_url().'/admin/catalog/export_rdf/'.$id;?>"  alt="<?php echo t('rdf_export');?>" title="<?php echo t('rdf_export');?>">
+        <span><img src="images/rdf_metadata_button.32.gif"/></span>
+        </a>                    
+</span>
 </div>
