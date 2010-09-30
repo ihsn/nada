fe28ee8ee73842bf813eb5564136b2c8d976e346
unknown <wb329102@.wb.ad.worldbank.org>
2010-09-30 16:05:38 -0400

    added: news feed url to site configurations updated: news feed uses the nada news page for news feed fixed: post bug report can post bugs to the ihsn

--- application/controllers/admin/admin.php
+++ application/controllers/admin/admin.php
@@ -23,7 +23,10 @@ class Admin extends MY_Controller {
 		$data['cache_files']=$this->_cache_file_count();
 	
 		//user status
-		$data['user_stats']=$this->Dashboard_model->get_user_stats();;
+		$data['user_stats']=$this->Dashboard_model->get_user_stats();
+		
+		//process any bug report submits
+		$this->_submit_bug_report();
 		
 		//load the contents of the page into a variable
 		$content=$this->load->view('dashboard',$data,TRUE);
@@ -44,9 +47,18 @@ class Admin extends MY_Controller {
 		
 		//set cache path from the CI config
 		$this->simplepie->cache_location=$this->config->item("cache_folder");
+
+		//get news feed url from config
+		$feed_url=$this->config->item("news_feed_url");
+		
+		if ($feed_url===FALSE)
+		{
+			//default feed url
+			$feed_url="http://ihsn.org/nada/index.php?q=news/feed";
+		}
 		
 		//get feed
-		$this->simplepie->set_feed_url("http://ihsn.org/mantis/issues_rss.php");
+		$this->simplepie->set_feed_url($feed_url);
 		$this->simplepie->set_timeout(5);//time to wait for the feed
 		$this->simplepie->init();
 		$data['feed'] = $this->simplepie;
@@ -101,6 +113,90 @@ class Admin extends MY_Controller {
 			$k++;
 		}
 		return $k;
+	}
+	
+	
+	function _submit_bug_report()
+	{
+		if (!$this->input->post("submit_bug"))
+		{
+			return;
+		}
+
+		$this->form_validation->set_rules('name', t('name'), 'xss_clean|trim|required|max_length[255]');
+		$this->form_validation->set_rules('email', t('email'), 'xss_clean|trim|required|max_length[255]');
+		$this->form_validation->set_rules('subject', t('subject'), 'xss_clean|trim|required|max_length[255]');
+		$this->form_validation->set_rules('body', t('body'), 'xss_clean|trim|required|max_length[500]');
+
+		//submit
+		if ($this->form_validation->run() == TRUE)
+		{
+			//data for post
+			$data=$_POST;
+					
+			//sanitize
+			foreach($data as $key=>$value)
+			{
+				$data[$key]=$this->input->xss_clean($value);
+			}
+					
+			//addition information
+			$data['nada_app_version']=$this->config->item('app_version');
+			$data['nada_db_version']=$this->config->item('db_version');
+			$data['nada_url']=site_url();
+			$data['php_version']=phpversion();
+			
+			//post form
+			$response=$this->_http_post('http://www.ihsn.org/nada/report_bug.php',$data);
+
+			if ($response==200)
+			{
+				$this->session->set_flashdata('message', t('form_update_success'));
+				
+				//redirect
+				redirect("admin","refresh");
+			}
+			else
+			{
+				$this->form_validation->set_error(t('form_update_fail'));
+			}
+		}
+	}
+
+
+	/**
+	*
+	* Post form using http post to the IHSN server
+	* bug tracker
+	*
+	* author: todo - find the original author of the function
+	**/
+	function _http_post($url, $data)
+	{
+	  $params = array('http' => array(
+				  'method' => 'POST',
+				  'content' => http_build_query($data)
+	   ));
+	
+		//create context		
+		$ctx = stream_context_create($params);
+		
+		//open handle
+		$fp = @fopen($url, 'rb', false, $ctx);
+		
+		if (!$fp) 
+		{
+			return FALSE;
+		}
+		
+		$response = @stream_get_contents($fp);
+		
+		if ($response === FALSE) 
+		{
+			return FALSE;
+		}
+		
+		return $response;	  
 	}
 	
 	
