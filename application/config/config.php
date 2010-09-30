556bfb6c8d4fa09283172536569db393810ff36b
unknown <wb329102@.wb.ad.worldbank.org>
2010-09-30 16:03:42 -0400

    minor

--- application/config/config.php
+++ application/config/config.php
@@ -68,7 +68,7 @@ $config['url_suffix'] = "";
 | than english.
 |
 */
-$config['language']	= "spanish";
+$config['language']	= "english";
 
 //List of supported languages. type language name in lower case
 $config['supported_languages']=array();//array("english","russian");
