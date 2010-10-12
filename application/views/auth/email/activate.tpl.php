3e602d67dcde062c5e87e28135fb8e859230cd11
unknown <wb329102@.wb.ad.worldbank.org>
2010-10-12 16:19:59 -0400

    messages are localized

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -5,16 +5,16 @@
 ?>
 <html>
 <body>
-	<p>Thank you for registering with the <em><?php echo $this->config->item('website_title'); ?></em> website. To complete your registration and activate your user account, please visit the following URL:</p>
+	<p><?php echo sprintf(t('thank_you_for_registration'),$this->config->item('website_title'));?></p>
 	<p><b><?php echo anchor('auth/activate/'. $id .'/'. $activation, site_url().'/auth/activate/'. $id .'/'. $activation);?></b></p>
-	<p>You account details are:</p>
-    <p>Username: <?php echo $email; ?></p>
-    <p>Password: <?php echo $password; ?></p>
+	<p><?php echo t('your_account_details');?>:</p>
+    <p><?php echo t('username');?>: <?php echo $email; ?></p>
+    <p><?php echo t('password');?>: <?php echo $password; ?></p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>-----------------------------------------------------------
     <br/><?php echo $this->config->item('website_title'); ?> - <?php echo site_url(); ?></p>
     <p>&nbsp;</p>
-    <p>DO NOT REPLY TO THIS EMAIL</p>
+    <p><?php echo t('do_not_reply_to_this_email');?></p>
 </body>
 </html>
