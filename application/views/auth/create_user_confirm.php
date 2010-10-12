3dc17be275544e679c7e60fbb60453334c0b1354
unknown <wb329102@.wb.ad.worldbank.org>
2010-10-12 16:19:12 -0400

    localized

--- application/views/auth/create_user_confirm.php
+++ application/views/auth/create_user_confirm.php
@@ -3,7 +3,7 @@
 	Confirmation message for the newly registered user
 */
 ?>
-<h1 class="page-title">Register</h1>
+<h1 class="page-title"><?php echo t('register');?></h1>
 <div style="margin-top:10px;font-size:14px;">
-You account has been created but before you can login, we need to confirm your email address. We have emailed you the instructions to activate your user account.
+<?php echo t('account_created_email_confirmation');?>
 </div>
