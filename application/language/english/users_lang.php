99bc9610377ba860995af62a74d2976334080cb7
unknown <wb329102@.wb.ad.worldbank.org>
2010-10-12 16:13:57 -0400

    missions translations added

--- application/language/english/users_lang.php
+++ application/language/english/users_lang.php
@@ -79,5 +79,25 @@ $lang['forgot_password_failed'] = "Failed to reset your password, please try aga
 $lang['account_activation_successful'] = "Account Activation Successful";
 $lang['activation_email_unsuccessful'] = "Activation email was not sent.";
 
+$lang['notification'] = "Notification";
+$lang['new_user_registration'] = "New user registration";
+$lang['new_user_registration_received'] = "A new user registration is received. User details are:";
+
+$lang['thank_you_for_registration'] = "Thank you for registering with the <em>%s</em> website. To complete your registration and activate your user account, please visit the following URL:";
+$lang['your_account_details'] = "You account details are";
+$lang['do_not_reply_to_this_email'] = "DO NOT REPLY TO THIS EMAIL";
+
+$lang['click_url_to_reset_password'] = "To reset your password, click on the link below or open the url in a web browser";
+$lang['request_password_ignore'] = "If you did not request password reset, ignore this message.";
+
+$lang['enter_email_to_reset_password'] = "Please enter your email address so we can send you an email to reset your password.";
+$lang['password_is_sent'] = "Your password has been reset and a new password is sent to your email address.";
+$lang['login_with_new_password'] = "Click here to login with your new password.";
+$lang['message_sent_to_your_email'] = "An email message has been sent to your email address, please check your inbox.";
+
+$lang['password_reset_to'] = "You password has been reset to <b>%s</b>. To login to the website, your account details are:";
+$lang['forgot_password_verification'] = "Forgotten Password Verification";
+$lang['account_activation'] = "Account activation";
+
 /* End of file users_lang.php */
 /* Location: ./system/language/english/users_lang.php */
