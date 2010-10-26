62b985eeb03fcfcd723d8592df8a2d3ba5962e16
Mehmood Asghar <mehmood@ihsn.org>
2010-10-26 10:37:00 -0400

    added repository information box to show on the ddi browser.

--- application/views/ddibrowser/repository.php
+++ application/views/ddibrowser/repository.php
@@ -7,9 +7,9 @@
 <div id="repo-info-box" style="background-color:#F7F7F7;font-size:14px;padding:10px;margin-top:10px;margin-bottom:20px;border:1px solid gainsboro;">
 <?php if (isset($repository)): ?>
 <h2>Metadata information</h2>
-<p style="font-size:14px;">The metadata is provided by <b><?php echo anchor($repository['url'],$repository['title'],'target="blank_"');?></b>, 
-and does not include any documentation such as the questionnaires, reports, data files, etc. To access complete metadata and documentation, 
-please visit the <em><?php echo $repository['title']; ?></em> data catalog at <?php echo anchor($repository['url']); ?> or directly access the study 
-at <?php echo anchor($repository['url'].'index.php/catalog/'.$repository['surveyid'],$repository['url'].'index.php/catalog/'.$repository['surveyid'],'target="blank_"'); ?></p>
+<p style="font-size:14px;">The metadata are provided by <b><?php echo anchor($repository['url'],$repository['title'],'target="blank_"');?></b>, 
+and do not include any documentation such as the questionnaires, reports, data files, etc. To access complete metadata and documentation, 
+please visit the <em><?php echo $repository['title']; ?></em> data catalog at <?php echo anchor($repository['url']); ?> or directly access the study page
+at <?php echo anchor($repository['url'].'/index.php/catalog/'.$repository['surveyid'],$repository['url'].'index.php/catalog/'.$repository['surveyid'],'target="blank_"'); ?></p>
 <?php endif;?>
 </div>
