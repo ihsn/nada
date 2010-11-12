7a77d9e7b764892c1f0d6a77b649c2643333b631
Mehmood Asghar <mehmood@ihsn.org>
2010-11-12 16:10:49 -0500

    minor

--- themes/wb/layout.php
+++ themes/wb/layout.php
@@ -10,7 +10,8 @@ $data['menus']= $this->Menu_model->select_all();
 $sidebar=$this->load->view('default_menu', $data,true);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo t($title);?></title>
 <base href="<?php echo base_url(); ?>" />
