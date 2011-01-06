b9ead25f4398baaac22e815ad70e8009df5e1f3f
Mehmood Asghar <mehmood@ihsn.org>
2011-01-06 15:08:20 -0500

    cleaned up configurations

--- application/config/cache.php
+++ application/config/cache.php
@@ -1,7 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 //folder path for storing cache files, this folder must have READ/WRITE access
-$config['cache_dir'] = APPPATH.'cache/';
+$config['cache_dir'] = './cache/';
 
 //default cache expiration in seconds
 $config['cache_default_expires'] = 0;
