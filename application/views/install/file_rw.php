7cc013f6c98a02fa97af3ef0d5fbb639e5010680
Mehmood Asghar <mehmood@ihsn.org>
2011-01-10 14:27:02 -0500

    paths updated

--- application/views/install/file_rw.php
+++ application/views/install/file_rw.php
@@ -2,8 +2,8 @@
 	
 	$test_folders=array(
 				'datafiles'=>'Catalog',
-				'application/cache'=>'Cache',
-				'application/logs'=>'Log',
+				$this->config->item('cache_path')=>'Cache',
+				$this->config->item('log_path')=>'Log',
 	);
 ?>
 	
