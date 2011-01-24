f1eeea6fcdc2acb507ffe6b67d462c95d0b48158
Mehmood Asghar <mehmood@ihsn.org>
2011-01-24 12:47:48 -0500

    minor

--- application/config/migrate.php
+++ application/config/migrate.php
@@ -1,9 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 //relative or absolute path to nada2 installation
-$config['nada2_folder_path'] = '../datafirst';
+$config['nada2_folder_path'] = '../nada2';
 
 //repository identifier - see the inc_config.php file in nada2 and paste the value here
-$config['nada2_repository_id'] = 'datafirst.uct.ac.za';
+$config['nada2_repository_id'] = 'default';
 
 ?>
