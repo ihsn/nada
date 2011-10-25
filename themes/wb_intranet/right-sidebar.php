fc4fddad8b8bcac5705d2aed9aa0637eeb48becc
Mehmood Asghar <mehmood@ihsn.org>
2011-10-25 10:41:04 -0400

    added right side bar

---
+++ themes/wb_intranet/right-sidebar.php
@@ -0,0 +1,50 @@
+<!--sidebar-reference-owner-->
+<div class="grey-module" id="right-sidebar">
+    <div class="m-head"> 
+        <h2>Data Catalogs</h2>
+    </div>
+    <div class="m-body">
+        
+    <div class="wb-box-sidebar">
+      <div class="wb-box" style="text-align:center;font-size:14px;line-height:150%;padding:10px;">
+        <div class="stats">
+          <div class="stats-text">As of October 19, 2011 our catalog contains</div>
+          <div class="stats-surveys">651 surveys</div>
+          <div class="stats-citations">956 citations</div>
+          <div class="stats-variables">393,123 variables</div>
+          <div style="margin-top:3px;"><a href="index.php/catalog" title="visit central catalog"><img src="files/catalog-button.gif" alt=""></a></div>
+        </div>
+      </div>
+   
+</div>    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </div><div class="m-footer"><span>&nbsp;</span></div>
+</div>
+<!--end-sidebar-reference-owner-->
+
+
+
+<!--sidebar-reference-owner-->
+<div class="grey-module" id="sidebar-faq">
+    <div class="m-head"> 
+        <h2>FAQ'S</h2>
+    </div>
+    <div class="m-body">
+     <ul>
+      <li><a href="http://microdata.worldbank.org/index.php/faqs#improve">How can I contribute to improving the catalog?</a></li>
+      <li><a href="http://microdata.worldbank.org/index.php/faqs#analyze">Can you help with analyzing the data?</a></li>
+      <li><a href="http://microdata.worldbank.org/index.php/faqs#tools">Can I get help in implementing a survey catalog in my agency?</a></li>	  
+	</ul>
+    </div><div class="m-footer"><span>&nbsp;</span></div>
+</div>
+<!--end-sidebar-reference-owner-->
