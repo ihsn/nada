4a222f2aabee3046e52e877b11ffff2da38a156d
Mehmood Asghar <mehmood@ihsn.org>
2011-10-25 10:37:29 -0400

    Added DA Filter

--- application/views/catalog_search/search_form.php
+++ application/views/catalog_search/search_form.php
@@ -62,6 +62,56 @@ $this->template->add_js($script, $type = 'embed');
         </div>
 	</div>
     <?php endif;?>
+
+
+
+	<h3 class="ui-accordion-header ui-helper-reset ui-state-active ui-corner-top">
+    	<a href="#"><?php echo t('filter_by_data');?><span id="selected-countries" style="font-size:11px;padding-left:10px;"></span></a>
+    </h3> 
+	<div class="ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom" id="datatype-list" style="font-size:11px;">
+    	<div class="flash">
+            <div style="text-align:right;">
+                <a  href="#" onclick="select_da('all');return false;"><?php echo t('link_select_all');?></a> | 
+                <a  href="#" onclick="select_da('none');return false;"><?php echo t('link_clear');?></a> | 
+                <a  href="#" onclick="select_da('toggle');return false;"><?php echo t('link_toggle');?></a>
+            </div>
+
+            <div class="filter-da">
+            <label title="<?php echo t('link_data_direct');?>" for="da_direct">
+            	<input class="chk" type="checkbox"  checked="checked" value="1" name="dtype[]" id="da_direct"/><span class="title"><?php echo t('legend_direct_access');?></span><br/>
+                <span class="da-desc"><?php echo t('data_direct_description');?></span>
+            </label>
+            <label title="<?php echo t('link_data_public_hover');?>" for="da_public">
+            	<input class="chk" type="checkbox"   checked="checked" value="2" name="dtype[]" id="da_public"/><span class="title"><?php echo t('legend_data_public');?></span><br/>
+                <span class="da-desc"><?php echo t('data_public_description');?></span>
+            </label>
+            <label title="<?php echo t('link_data_licensed_hover');?>" for="da_licensed">
+            	<input class="chk" type="checkbox"   checked="checked" value="3" name="dtype[]" id="da_licensed"/><span class="title"><?php echo t('legend_data_licensed');?></span><br/>
+                <span class="da-desc"><?php echo t('data_licensed_description');?></span>
+            </label>
+            <label title="<?php echo t('link_data_enclave_hover');?>" for="da_enclave">
+            	<input class="chk" type="checkbox"   checked="checked" value="4" name="dtype[]" id="da_enclave"/><span class="title"><?php echo t('legend_data_enclave');?></span><br/>
+                <span class="da-desc"><?php echo t('data_enclave_description');?></span>
+            </label>
+            <label title="<?php echo t('link_data_remote_hover');?>" for="da_remote">
+            	<input class="chk" type="checkbox"   checked="checked" value="5" name="dtype[]" id="da_remote"/><span class="title"><?php echo t('legend_data_remote');?></span><br/>
+                <span class="da-desc"><?php echo t('data_remote_description');?></span>
+            </label>
+
+            <label title="<?php echo t('link_data_na');?>" for="da_na">
+            	<input class="chk" type="checkbox"  checked="checked" value="6" name="dtype[]" id="da_na"/><span class="title"><?php echo t('legend_na_access');?></span><br/>
+                <span class="da-desc"><?php echo t('data_da_description');?></span>
+            </label>
+
+            </div>
+
+        </div>
+	</div>
+
+
+
+
+
     <?php if ($this->topic_search=='yes'):?>
     	<!-- topics -->
         <h3 class="ui-accordion-header ui-helper-reset ui-state-default ui-corner-all">
@@ -126,12 +176,19 @@ $this->template->add_js($script, $type = 'embed');
 		<div style="text-align:right;margin-top:-15px;" class="search-buttons">
         	<input class="button" type="submit" id="btnsearch" name="search" value="<?php echo t('search');?>"/>
             <input class="btn-cancel" type="button" id="reset" name="reset" onclick="window.location.href='<?php echo site_url();?>/catalog/<?php echo $repoid;?>?reset=reset'"  value="<?php echo t('reset');?>"/>
-		</div> 
+		</div>
     </div>
-
-	<div id="surveys"><?php echo $search_result; ?></div>
-    
+	<div id="surveys" ><?php echo $search_result; ?></div>    
 </form>
+
+<div class="da-legend">
+<label title="<?php echo t('link_data_direct');?>" for="da_direct"><img src="images/form_direct.gif" /> <input class="chk" type="checkbox"   checked="checked" value="1" name="dtype[]" id="da_direct"/><?php echo t('legend_direct_access');?></label>
+<label title="<?php echo t('link_data_public_hover');?>" for="da_public"><img src="images/form_public.gif" /><input class="chk" type="checkbox"   checked="checked" value="2" name="dtype[]" id="da_public"/> <?php echo t('legend_data_public');?></label>
+<label title="<?php echo t('link_data_licensed_hover');?>" for="da_licensed"><img src="images/form_licensed.gif" /><input class="chk" type="checkbox"   checked="checked" value="3" name="dtype[]" id="da_licensed"/> <?php echo t('legend_data_licensed');?></label>
+<label title="<?php echo t('link_data_enclave_hover');?>" for="da_enclave"><img src="images/form_enclave.gif" /> <input class="chk" type="checkbox"   checked="checked" value="4" name="dtype[]" id="da_enclave"/><?php echo t('legend_data_enclave');?></label>
+<label title="<?php echo t('link_data_remote_hover');?>" for="da_remote"><img src="images/form_remote.gif" /> <input class="chk" type="checkbox"   checked="checked" value="5" name="dtype[]" id="da_remote"/><?php echo t('legend_data_remote');?></label>
+</div>
+
 
 <script type="text/javascript">
 //translations	
