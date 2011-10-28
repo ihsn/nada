35cbbfb2e114ca0c8e560735076b6475dd5b9abd
Mehmood Asghar <mehmood@ihsn.org>
2011-10-28 11:09:02 -0400

    updated formatting

--- application/views/repositories/about.php
+++ application/views/repositories/about.php
@@ -1,5 +1,7 @@
 <style>
 	.about-photo{float:left;margin-right:10px;}
+	.repository-about{}
+	.repository-about .photo-big{float:left;margin-right:10px;}
 </style>
 <div class="repository-about">
 <?php echo $row->long_text;?>
