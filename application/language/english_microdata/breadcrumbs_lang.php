b63d539ad2a1abb7e45d3e25e78bf2e3e2b793b2
Mehmood Asghar <mehmood@ihsn.org>
2011-12-13 10:08:06 -0500

    add more translations strings

---
+++ application/language/english_microdata/breadcrumbs_lang.php
@@ -0,0 +1,8 @@
+<?php 
+$lang['Home']="Home";
+$lang['Data Catalog']="Data catalog";
+$lang['Citations']="Citations";
+
+
+/* End of file breadcrumbs_lang.php */
+/* Location: ./application/language/english_microdata/breadcrumbs_lang.php */
