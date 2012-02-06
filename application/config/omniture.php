fe469eac4325323364c1fad005b553f4bbd1fcb1
Mehmood Asghar <mehmood@ihsn.org>
2012-02-06 16:21:04 -0500

    Omniture configuration file

---
+++ application/config/omniture.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Settings for Omniture site tracking
+|--------------------------------------------------------------------------
+|
+*/
+
+//omniture dev account
+$config['omniture_s_account_dev']='DEVWBTSTSAMP3';
+
+//omniture prod account
+$config['omniture_s_account_prod']='wbnispdecextmdc,wbglobalext';
+
+
+/* End of file config.php */
+/* Location: ./system/application/config/config.php */
