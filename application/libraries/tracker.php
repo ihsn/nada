521512fe69990acef39bce6810bd448dddaab407
Mehmood Asghar <mehmood@ihsn.org>
2012-02-06 16:29:27 -0500

    Replaced hard coded s_account value with value set in omniture config file

--- application/libraries/tracker.php
+++ application/libraries/tracker.php
@@ -25,7 +25,7 @@ class Tracker //extends OmnitureMeasurement
 	{
 		log_message('debug', "Tracker Class Initialized.");
 		$this->ci =& get_instance();
-		
+		$this->ci->load->config('omniture');
 		//parent::__construct();
 	}
 
@@ -34,8 +34,18 @@ class Tracker //extends OmnitureMeasurement
 	{
 		//Instantiate instance
 		$s = new OmnitureMeasurement();
-		//Setup application config variables
-		$s->account = 'DEVWBTSTSAMP3';
+		
+		if (defined('ENVIRONMENT') && ENVIRONMENT=='production')
+		{		
+			//set s_account
+			$s->account = $this->ci->config->item("omniture_s_account_prod");
+		}
+		else
+		{
+			//dev account
+			$s->account = $this->ci->config->item("omniture_s_account_dev");
+		}	
+		
 		//$s->mobile = false;
 		
 		$s->pageName='DDP Microdata > ';
