c03d937117b79b69bb1b7261ac96deb4b8ae1305
Mehmood Asghar <mehmood@ihsn.org>
2012-02-06 16:26:33 -0500

    Added support to load json encoded values from db

--- application/libraries/Site_configurations.php
+++ application/libraries/Site_configurations.php
@@ -14,17 +14,34 @@ class Site_configurations{
 	{
 		$this->ci =& get_instance();
 		$this->ci->load->model('configurations_model');
-
+		
+		//load settings from db
 		$settings=$this->ci->configurations_model->load();
 		
-		//check if configurations were found in database, load from db
+		//list of settings stored in db in JSON format
+		$json_formatted=array('admin_allowed_ip');
+		
+		//update the config array with values from DB
 		if ($settings)
 		{
 			foreach($settings as $setting)
 			{
-				$this->ci->config->set_item($setting['name'], $setting['value']);
+				//setting is stored in DB using JSON array format
+				if (in_array($setting['name'],$json_formatted))
+				{
+					//check if JSON is valid
+					if (json_decode($setting['value'])!==FALSE)
+					{
+						$this->ci->config->set_item($setting['name'], json_decode($setting['value']));
+					}	
+				}
+				else //normal non-json values
+				{
+					$this->ci->config->set_item($setting['name'], $setting['value']);
+				}	
 			}
 		}
+		
 	}
 	
 }
