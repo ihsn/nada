b824dfe333fabb05adb4e1537b5f0de6f4327336
Mehmood Asghar <mehmood@ihsn.org>
2012-02-09 09:52:04 -0500

    lower the chunk size for file uploads

--- application/views/managefiles/plupload.php
+++ application/views/managefiles/plupload.php
@@ -199,7 +199,7 @@ $(function() {
 		runtimes : 'gears,flash,silverlight,browserplus,html5',
 		url : '<?php echo site_url().'/admin/managefiles/'.$this->uri->segment(3).'/process_batch_uploads'; ?>',
 		max_file_size : '300mb',
-		chunk_size : '3mb',
+		chunk_size : '2mb',
 		unique_names : false,
 		multiple_queues:true,
 		multipart_params: { 'upload_folder': 'default', 'overwrite':0},
@@ -209,7 +209,7 @@ $(function() {
 
 		// Specify what files to browse for
 		filters : [
-			{title : "Resources", extensions : "jpg,gif,png,zip,doc,docx,pdf,sav,dta,do,smcl,log,ado,txt,rar,xls,xlsx,rtf"},
+			{title : "Resources", extensions : "jpg,gif,png,zip,doc,docx,pdf,sav,dta,do,smcl,log,ado,txt,rar,xls,xlsx,rtf,csv"},
 			{title : "Others", extensions : "*.*"}
 		],
 
