c5c5a65d3cc8fd86b61d6a2bf364443da06f3f6f
Mehmood Asghar <mehmood@ihsn.org>
2012-02-13 16:18:58 -0500

    misc updates

--- application/helpers/MY_url_helper.php
+++ application/helpers/MY_url_helper.php
@@ -88,6 +88,83 @@ if ( ! function_exists('site_home'))
 	}
 } 
 
+	//check if SSL is available on the server
+	function is_ssl_enabled()
+	{
+		$CI =& get_instance();
+		if ($CI->config->item("proxy_ssl")===FALSE &&  $CI->config->item("enable_ssl")===FALSE)
+		{
+			//dont't check anything
+			return FALSE;
+		}
+		
+		$is_https=FALSE;
+		
+		if ($CI->config->item("proxy_ssl"))
+		{
+			//echo "using proxy";
+			$proxy_ssl_header=$CI->config->item("proxy_ssl_header");
+			
+			//see if the variable is set
+			if (isset($_SERVER[$proxy_ssl_header]))
+			{
+				$is_https=TRUE;
+			}
+		}
+		//check SSL using server HTTPS variablbe
+		else if ($CI->config->item("enable_ssl") && isset($_SERVER['HTTPS']))
+		{
+				$is_https=TRUE;
+		}
+
+		return $is_https;
+	}
+	
+	//check if current page is acccessed using HTTPS
+	function is_ssl_request()
+	{	
+		$CI =& get_instance();
+		if ($CI->config->item("proxy_ssl")===FALSE &&  $CI->config->item("enable_ssl")===FALSE)
+		{
+			//dont't check anything
+			return FALSE;
+		}
+		
+		//current page is loaded using HTTPS?	
+		$is_https=FALSE;
+		
+		if ($CI->config->item("proxy_ssl"))
+		{
+			//echo "using proxy";
+			$proxy_ssl_header=$CI->config->item("proxy_ssl_header");
+			$proxy_ssl_header_value=$CI->config->item("proxy_ssl_header_value");
+			
+			//check if using SSL/Proxy/Headers
+			if ($proxy_ssl_header!='' && $proxy_ssl_header_value!='')
+			{		
+				//see if the variable is set
+				if (isset($_SERVER[$proxy_ssl_header]))
+				{
+					if ($_SERVER[$proxy_ssl_header]==$proxy_ssl_header_value)
+					{
+						$is_https=TRUE;
+					}
+				}
+			}
+		}
+		//check SSL using server HTTPS variablbe
+		else if ($CI->config->item("enable_ssl") && isset($_SERVER['HTTPS']))
+		{
+			//echo "dfoudfdfdfd";
+			if($_SERVER['HTTPS']=="on")
+			{
+				$is_https=TRUE;
+			}	
+		}
+
+		return $is_https;		
+	}
+
 /**
 *
 * Returns JS/CSS base url
@@ -101,10 +178,145 @@ if ( ! function_exists('js_base_url'))
         {
         	return JS_BASE_URL;
 		}
-		
 		return base_url();
 	}
 } 
+
+/**
+ * Anchor Link
+ *
+ * Creates an anchor based on the local URL.
+ *
+ * @access	public
+ * @param	string	the URL
+ * @param	string	the link title
+ * @param	mixed	any attributes
+ * @return	string
+ */
+if ( ! function_exists('anchor'))
+{
+	function anchor($uri = '', $title = '', $attributes = '')
+	{
+		$title = (string) $title;
+
+		if ( ! is_array($uri))
+		{
+			$site_url = ( ! preg_match('!^\w+://! i', $uri)) ? site_url($uri) : $uri;
+		}
+		else
+		{
+			$site_url = site_url($uri);
+		}
+
+		if ($title == '')
+		{
+			$title = $site_url;
+		}
+
+		if ($attributes != '')
+		{
+			$attributes = _parse_attributes($attributes);
+		}
+
+		return '<a href="'.$site_url.'"'.$attributes.'>'.$title.'</a>';
+	}
+}
+
+/*
+//TODO:REMOVE
+	function force_proxy_ssl($site_url)
+	{
+		//check if proxy_ssl =TRUE
+		$CI =& get_instance();
+		if ($CI->config->item("proxy_ssl")===TRUE || $CI->config->item("enable_ssl")===TRUE)
+		{
+			//Force SSL for URLs containing /auth/
+			if (strpos(current_url(),"/auth/")!==FALSE)
+			{
+				$site_url=str_replace("http:","https:",$site_url);
+			}
+		}
+		
+		return $site_url;
+	}
+*/
+
+/*
+//TODO:REMOVE
+	
+	//force SSL for specific pages
+	function force_ssl($url)
+	{
+		$CI =& get_instance();
+		
+		//no SSL support on server
+		if (!is_ssl_enabled())
+		{
+			return $url;
+		}
+		return $url;
+				
+		//Force SSL for URLs containing /auth/
+		if (strpos(current_url(),"/auth/")!==FALSE)
+		{
+			$url=str_replace("http:","https:",$url);
+		}
+		return $url;
+	}
+*/
+	
+
+/**
+ * Base URL
+ *
+ * Returns the "base_url" item from your config file
+ *
+ * @access	public
+ * @return	string
+ */
+if ( ! function_exists('base_url'))
+{
+	function base_url()
+	{
+		$CI =& get_instance();
+		$base_url=$CI->config->slash_item('base_url');
+
+		if (is_ssl_enabled() && strpos(current_url(),"/auth/")!==FALSE)
+		{
+			return $url=str_replace("http:","https:",$base_url);
+		}
+		
+		return $base_url;
+	}
+}	
+
+
+/**
+ * Site URL
+ *
+ * Create a local URL based on your basepath. Segments can be passed via the
+ * first parameter either as a string or an array.
+ *
+ * @access	public
+ * @param	string
+ * @return	string
+ */
+if ( ! function_exists('site_url'))
+{
+	function site_url($uri = '')
+	{
+		$CI =& get_instance();
+		$url= $CI->config->site_url($uri);
+		
+		if (is_ssl_enabled() && strpos(current_url(),"/auth/")!==FALSE)
+		{
+			return $url=str_replace("http:","https:",$url);
+		}
+		
+		return $url;
+
+	}
+}
 
 /*
 if ( ! function_exists('sanitize_url'))
@@ -122,5 +334,7 @@ if ( ! function_exists('sanitize_url'))
 	}
 }
 */
+
+
 /* End of file MY_url_helper.php */
 /* Location: ./application/helpers/MY_url_helper.php */
