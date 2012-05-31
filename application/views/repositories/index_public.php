07fa212b2f327a9e085f07df6133a66aaffa2ecf
Mehmood Asghar <mehmood@ihsn.org>
2012-05-31 10:53:17 -0400

    Updated page layout

--- application/views/repositories/index_public.php
+++ application/views/repositories/index_public.php
@@ -1,17 +1,24 @@
 <style>
-.repo-table td{padding-top:10px;padding-bottom:10px;border-bottom:1px solid gainsboro;}
+.repo-table td{padding-top:10px;padding-bottom:0px;border-bottom:1px solid gainsboro;font-size:12px;line-height:140%;}
 .thumb{padding-right:10px;padding-bottom:5px;}
+.thumb img{padding-bottom:10px;}
 .page-title{border-bottom:1px solid gainsboro;}
-</style>
-<div class="contributing-repos" style="padding:10px;">
-<div style="padding-bottom:30px;">
-<a title="Central Catalog" href="<?php echo site_url();?>/catalog/central"><img style="float: left; display: block; margin-right: 10px;" src="files/logo-network.gif" alt="Central Catalog"></a>
-<h3 style="font-size:larger;font-weight:bold;"><a href="<?php echo site_url();?>/catalog/central">Central Microdata Catalog</a></h3>
-<p>
-The Central Microdata Catalog is a portal for all datasets held in catalogs maintained by the World Bank and a number of contributing external repositories. As of <?php echo date("F d, Y",date("U")); ?>, our central catalog contains <?php echo $survey_count;?> surveys. Users who wish to go directly to a specific catalog can visit the specific contributing repository through the links below.</p>
-</div>
-<br style="clear:both;margin-top:10px;"/>
 
+.contributing-repos h2 {border-bottom:0px solid gainsboro;font-size:12px; font-family:Arial, Helvetica, sans-serif; text-transform:uppercase; font-weight:bold; word-spacing:110%;}
+.contributing-repos p a, .central-repo p a{color:black;}
+.contributing-repos p a:hover, .central-repo p a:hover{text-decoration:underline}
+
+</style>
+<div class="contributing-repos" >
+<?php /*?>
+<div>
+<a title="Microdata Library" href="<?php echo site_url();?>/catalog/central"><img style="float: left; display: block; margin-right: 10px;" src="files/logo-central.gif" alt="Microdata Library"></a>
+<h3 style="font-size:larger;font-weight:bold;"><a href="<?php echo site_url();?>/catalog/central"><?php echo t('central_data_catalog');?></a></h3>
+<p>
+<a href="<?php echo site_url();?>/catalog/central">The <?php echo t('central_data_catalog');?> is a portal for all datasets held in repositories maintained by the World Bank and a number of contributing external repositories. Click here to search all repositories.</a></p>
+</div>
+<br style="clear:both;"/>
+<?php */?>
 <?php if ($rows):?>
 	<?php foreach($rows as $row): ?>
     	<?php 
@@ -23,12 +30,12 @@ The Central Microdata Catalog is a portal for all datasets held in catalogs main
 	<?php //show repositories divided by sections
 		
 		//internal catalogs
-		if (in_array('internal',$repo_sections))
+		if (in_array('regional',$repo_sections))
 		{
 			$data=array(
 						'rows'=>$rows,
-						'section'=>'internal',
-						'section_title'=>t('repositories_internal') 
+						'section'=>'regional',
+						'section_title'=>t('repositories_regional') 
 						);
 			$this->load->view("repositories/repos_by_section",$data);
 		}	
@@ -36,12 +43,12 @@ The Central Microdata Catalog is a portal for all datasets held in catalogs main
     <div style="height:20px;">&nbsp;</div>	
 	<?php	
 		//external catalogs
-		if (in_array('external',$repo_sections))
+		if (in_array('specialized',$repo_sections))
 		{
 			$data=array(
 						'rows'=>$rows,
-						'section'=>'external',
-						'section_title'=>t('repositories_external') 
+						'section'=>'specialized',
+						'section_title'=>t('repositories_specialized') 
 						);
 
 			$this->load->view("repositories/repos_by_section",$data);
