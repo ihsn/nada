9fe36b7e646ba6884aec4bae1f18e21856da0033
Mehmood Asghar <mehmood@ihsn.org>
2012-06-06 11:07:38 -0400

    some minor html changes

--- application/views/user_bar.php
+++ application/views/user_bar.php
@@ -31,17 +31,17 @@ if ($languages!==FALSE)
             <?php if ($this->ion_auth->is_site_admin()): ?>
 	            <li><a href="<?php echo site_url(); ?>/admin"><?php echo t('site_administration');?></a></li>
             <?php endif;?>
-            <li><a href="<?php echo site_url(); ?>/auth/profile"><?php echo t('profile');?></a></li>
-            <li><a href="<?php echo site_url(); ?>/auth/change_password"><?php echo t('password');?></a></li>                                    
+            <li class="profile"><a href="<?php echo site_url(); ?>/auth/profile"><?php echo t('profile');?></a></li>
+            <li class="password"><a href="<?php echo site_url(); ?>/auth/change_password"><?php echo t('password');?></a></li>                                    
             <li><a href="<?php echo site_url(); ?>/auth/logout"><?php echo t('logout');?></a></li>
             <?php echo $lang_ul;?>
         </ul>        
     </div>
 <?php else:?>
 <div class="user-box">
-    <a href="<?php echo site_url(); ?>/auth/login"><?php echo t('login');?></a> | 
+    <a href="<?php echo site_url(); ?>/auth/login"><?php echo t('login');?></a> 
     <?php if (!$this->config->item("site_user_register")=='no' || !$this->config->item("site_password_protect")=='yes'):?>	
-    <a href="<?php echo site_url(); ?>/auth/register"><?php echo t('register');?></a>
+     | <a class="register" href="<?php echo site_url(); ?>/auth/register"><?php echo t('register');?></a>
     <?php endif;?>
     <?php echo $lang_ul;?>
 </div>
