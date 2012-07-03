830225a1fe02a3527d345883740163c5cbce8f9a
Mehmood Asghar <mehmood@ihsn.org>
2012-07-03 13:10:04 -0400

    Fixed issue with the display of images caused by the additional slash(//) in the url

--- application/views/repositories/repos_by_section.php
+++ application/views/repositories/repos_by_section.php
@@ -14,7 +14,7 @@
 		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=" "; } ?>
         <?php if (!$row->ispublished || $row->section!==$section){continue;} //skip unpublished?>
     	<tr class="<?php echo $tr_class; ?>" valign="top">
-            <td class="thumb"><a href="<?php echo site_url();?>/catalog/<?php echo $row->repositoryid;?>/about"><img src="<?php echo base_url();?>/<?php echo $row->thumbnail; ?>"/></a></td>
+            <td class="thumb"><a href="<?php echo site_url();?>/catalog/<?php echo $row->repositoryid;?>/about"><img src="<?php echo base_url();?><?php echo $row->thumbnail; ?>"/></a></td>
             <td class="text">
 			<h3><a href="<?php echo site_url();?>/catalog/<?php echo $row->repositoryid;?>/about"><?php echo $row->title; ?></a></h3>
 			<p><a href="<?php echo site_url();?>/catalog/<?php echo $row->repositoryid;?>/about"><?php echo $row->short_text; ?></a></p>
