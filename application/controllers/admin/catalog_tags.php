95902dc34198f0626173f9ef89e454ad7ef49775
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-11-10 11:01:02 -0500

    clean user input

--- application/controllers/admin/catalog_tags.php
+++ application/controllers/admin/catalog_tags.php
@@ -23,34 +23,36 @@ class Catalog_Tags extends MY_Controller {
 	
 	public function add($id) 
 	{		
-		$tag=xss_clean($this->input->post('tag'));
-
 		if (!is_numeric($id))
 		{
 			return FALSE;
 		}
 	
+		$this->load->helper("string");
+		
+		$tag=xss_clean($this->input->post('tag'));
+		
+		//remove single/double quotes
+		$tag=strip_quotes($tag);
+		
+		//remove any tags
+		$tag=strip_tags($tag);
+		
 		if (!$this->Catalog_Tags_model->tag_exists($id, $tag)) 
 		{
-			$this->Catalog_Tags_model->insert($id,$tag);
+			$options=array(
+					'sid'=>$id,
+					'tag'=>$tag
+				);
+			$this->Catalog_Tags_model->insert($options);
 		}
 		
 		//get all tags associated with the survey	
 		$survey_tags = $this->Catalog_Tags_model->survey_tags($id);		
 		
 		//return new tag list for the survey
-		$this->_format_tags($survey_tags);	
+		$this->load->view("views/survey_tags_list",array('tags'=>$survey_tags));
 	}
-
-	//format tag unordered list from tags array
-	private function _format_tags($tags) 
-	{
-		foreach($tags as $tag) 
-		{
-			echo "<li id='{$tag['id']}'>{$tag['tag']} <span class=\"action\">-</span></li>", PHP_EOL;
-		}	 
-    }
-
 
 
 	public function delete($id) 
