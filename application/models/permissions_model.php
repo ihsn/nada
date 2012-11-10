72142c332669bdf7487ce711c33876d51f9ee0f8
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-11-10 10:49:11 -0500

    rewrite to support multiple groups per user, group repo access, etc.

--- application/models/permissions_model.php
+++ application/models/permissions_model.php
@@ -1,170 +1,344 @@
 <?php
-/**
- * User Permissions System
- * Schema:                
- *		Backend:       [ USER_PERMISSIONS ]
- *       		                |
- *								|
- *								|
- *					   [ PERMISSIONS_URLS ]
- *			        		    |
- *							    /
- *		Frontend:	     ___ __/
- *						/
- *				[ PERMISSIONS ]	
- * 	Group `permissions' will be tied via `permissions_urls'
- *
- *  `permissions' is a frontend template for coupling and decoupling these relationships, respectively.
- **/
-  
 class Permissions_model extends CI_Model {
     
 	public function __construct()
     {
         parent::__construct();
     }
-	
-	public function get_group_permissions($id) {
-		$permissions = $this->get_ordered_permissions();
-		$array       = array();
-		$value       = $this->_get_permission_value($id);
-		foreach($permissions as $permission) {
-			if ( !! ($value	 & (1 << (int) $permission->id))) {
-				$array[$permission->id] = true;
-			}
-		}
-		return $array;
-	}
-	
-	public function get_ordered_permissions() {
-		$q = $this->db->select('id, label, description, section, weight')
-			->from('permissions')
-			->order_by('section', 'asc')
-			->order_by('weight', 'asc')
-			->where('section IS NOT NULL', null);
-					
-		return $q->get()->result();
-	}
-	
-	public function get_permission($id) {
-		$q = $this->db->select('label, description, section, weight')
-			->from('permissions')
-			->order_by('section', 'asc')
-			->order_by('weight', 'asc')
-			->where('id', $id);
-					
-		return $q->get()->result();
-	}
-	
-	public function update_permission($id, $data) {
-		$this->db->where('id', $id);
-		$this->db->update('permissions', $data);
-	}
-	
-	public function create_permission($data) {
-		$this->db->insert('permissions', $data);
-	}
-	
-	public function update_urls($url, $data) {
-		$this->db->where('url', $url);
-		$this->db->update('permission_urls', $data);
-	}
-	
-	public function delete_permission($permission_id) {
-		$this->db->where('permission_id', $permission_id);
-		$this->db->delete('permission_urls');
 
-		$this->db->where('id', $permission_id);
-		$this->db->delete('permissions');
-	}
-	
-	
-	public function add_url($data) {
-		$this->db->insert('permission_urls', $data);
-	}
-	
-	private function _get_permission_value($id) {
-		$q                       = $this->db->select('permissions')
-									->from('user_permissions')
-									->where('usergroup_id', $id);
-		$result                  = $q->get()->result();
-		$this->_permission_value = (int) ($result) ? $result[0]->permissions : 0;
-		return $this->_permission_value;
-	}
-	
-	public function update_permission_value($group_id, $value) {
-		$q = $this->db->select('id')
-			->from('user_permissions')
-			->where('usergroup_id', $group_id);
-		$result = $q->get()->result();
-		var_dump(isset($result[0]));
-		if (!isset($result[0]->id)) {
-			$data = array(
-				'usergroup_id' => $group_id,
-				'permissions'  => $value
-			);
-			$this->db->insert('user_permissions', $data);
-		} else {
-			$this->db->where('usergroup_id', $group_id);
-			$this->db->update('user_permissions', array('permissions' => $value));
-		}
-	}
-	
-	public function get_permissions() {
-		$q = $this->db->select('permission_id, url')
-			->from('permission_urls');
+	/**
+	*
+	* Returns an array of all available permissions
+	**/
+	public function get_available_permissions()
+	{
+		$query = $this->db->select('p.*,pu.url')
+				->from('permissions p')
+				->join('permission_urls pu', 'p.id = pu.permission_id', 'inner')
+				->order_by('p.section', 'asc')
+				->order_by('p.weight', 'asc');
 					
-		return $q->get()->result();
+		return $query->get()->result_array();		
 	}
 	
-	public function get_permissions_by_section($section) {
-		$q = $this->db->select('id')
-			->from('permissions')
-			->where('section', $section);
-					
-		return $q->get()->result();
-	}
-	
-	public function group_has_url_access($group_id, $url) {
-		$q = $this->db->select('permission_id, url')
-			->from('permission_urls');
-		$result = $q->get()->result();
+	/**
+	*
+	* Return permissions divided in sections
+	**/
+	public function get_grouped_permission_list()
+	{
+		//get all permissions
+		$permissions=$this->get_available_permissions();
 		
-		foreach ($result as $permissions) {
-			// Convert wild-cards to RegEx
-			$key = str_replace('*', '.+', $permissions->url);
-			// Does the RegEx match?
-			if (preg_match('#^'.$key.'$#', $url))
+		//grouped by sections
+		$output=array();
+		
+		foreach($permissions as $perm)
+		{
+			$output[$perm['section']][$perm['id']]=$perm;
+		}
+		
+		return $output;
+	}
+	
+	/**
+	*
+	* Get group basic info by group id
+	**/
+	public function get_group_info($group_id)
+	{
+		$query=$this->db->select('name, group_type, access_type')
+					->from('groups')
+					->where('id',$group_id)
+					->get()->row_array();
+		
+		if ($query)
+		{			
+			return $query;	
+		}
+		
+		return FALSE;	
+	}
+	
+	/**
+	*
+	* Get permissions assigned to the group
+	**/
+	public function get_group_permissions($group_id)
+	{
+		$this->db->select('permission_id');
+		$this->db->where('group_id',$group_id);
+		$query=$this->db->get('group_permissions')->result_array();
+		
+		$output=array();
+		foreach($query as $row)
+		{
+			$output[]=$row['permission_id'];
+		}
+		
+		return $output;
+	}
+	
+	
+	/**
+	*
+	* Update group permissions
+	**/
+	public function update_perms($group_id,$perms_array)
+	{
+		//remove existing permissions for the group
+		$this->db->where('group_id',$group_id);
+		$this->db->delete('group_permissions');
+
+		if (!is_array($perms_array))
+		{
+			return;
+		}
+		
+		//assign new permissions
+		foreach($perms_array as $perm)
+		{
+			if (is_numeric($perm))
 			{
-				return $this->group_has_permission($group_id, $permissions->permission_id);
+				$options=array(
+							'group_id'		=>$group_id,
+							'permission_id'	=>$perm
+							);
+				
+				$this->db->insert('group_permissions',$options);			
 			}
 		}
-		return false;
 	}
+	
+	/**
+	*
+	* Set group access for repositories
+	**/
+	public function update_repo_perms($group_id,$repo_array)
+	{		
+		//remove existing permissions for the group
+		$this->db->where('group_id',$group_id);
+		$this->db->delete('group_repo_access');
+	
+		if (!is_array($repo_array))
+		{
+			return;
+		}
 
-	public function get_urls_by_permission_id($permission_id) {
-		$q = $this->db->select('permission_id, url, id')
-			->from('permission_urls')
-			->where('permission_id', $permission_id);
-		return $q->get()->result();
-	}
-
-	public function group_has_permission($group_id, $permission_id) {
-
-		return !!((int) $this->_get_permission_value($group_id) & (1 << (int) $permission_id));
+		//assign new permissions
+		foreach($repo_array as $repo)
+		{
+			if (is_numeric($repo))
+			{
+				$options=array(
+							'group_id'	=>$group_id,
+							'repo_id'	=>$repo
+							);
+				
+				$this->db->insert('group_repo_access',$options);			
+			}
+		}
 		
 	}
 	
-	public function group_add_permission($group_id, $permission_id) {
-		$permission   = (1 << (int) $permission_id);
-		$value        = (int) $this->_get_permission_value($group_id) | $permission;
-		
-		$this->update_permission_value($group_id, $value);
+	
+	/**
+	*
+	* Returns a list of all repositories
+	**/
+	public function get_repositories()
+	{
+		$this->db->select('*');
+		return $this->db->get('repositories')->result_array();
 	}
 	
-	public function group_delete_permission($group_id, $permission_id) {
-		$permission  = (1 << (int) $permission_id);
-		$value       =  (int) $this->_get_permission_value($group_id) & ~$permission;
-		$this->update_permission_value($group_id, $value);
+	/**
+	*
+	* Returns user group permissions for all repositories
+	**/
+	public function get_group_repositories($group_id)
+	{
+		$this->db->select('repo_id');
+		$this->db->where('group_id',$group_id);
+		$rows=$this->db->get('group_repo_access')->result_array();
+		
+		$repos=array();
+		foreach($rows as $row)
+		{
+			$repos[]=$row['repo_id'];
+		}
+		
+		return $repos;
 	}
-}
+	
+	
+	public function get_permission_labels()
+	{
+		$this->db->order_by('section,weight','ASC');
+		$permissions=$this->db->get('permissions')->result_array();
+		
+		//grouped by sections
+		$output=array();
+		
+		foreach($permissions as $perm)
+		{
+			$output[$perm['section']][$perm['id']]=$perm;
+		}
+		
+		return $output;
+	}
+	
+	public function get_permission_urls()
+	{
+		$rows=$this->db->get('permission_urls')->result_array();	
+		
+		$output=array();
+		foreach($rows as $row)
+		{
+			$output[$row['permission_id']][]=$row['url'];
+		}
+
+		return $output;
+	}
+	
+	public function get_permission_by_id($perm_id)
+	{
+		$permissions=$this->db->from('permissions')
+							->where('id',$perm_id)
+							->get()->row_array();
+		
+		//get associated URLs
+		$permissions['urls']=$this->get_permission_associated_urls($perm_id);
+		return $permissions;
+	}
+	
+	public function get_permission_associated_urls($perm_id)
+	{
+		$rows=$this->db->from('permission_urls')
+						->where('permission_id',$perm_id)
+						->get()->result_array();	
+						
+		$urls=array();
+		foreach($rows as $row)
+		{
+			$urls[]=$row['url'];
+		}			
+		return $urls;	
+	}
+
+	/**
+	*
+	* Update permission description + urls
+	**/
+	public function update_permission_options($perm_id,$options)
+	{		
+		$valid_fields=array('label','section','description','weight');
+		
+		$data=array();
+		//build update statement
+		foreach($options as $key=>$value)
+		{
+			if (in_array($key,$valid_fields) )
+			{
+				$data[$key]=$value;
+			}
+		}
+		
+		//update permission description table
+		$this->db->where('id', $perm_id);
+		$result=$this->db->update('permissions', $data); 
+		
+		if (!$result)
+		{
+			return FALSE;
+		}
+		
+		//update permission urls table
+		
+		//first remove any assigned urls
+		$this->db->where('permission_id',$perm_id);
+		$this->db->delete('permission_urls');
+		
+		//assign new urls
+		foreach($options['url'] as $url)
+		{
+			if (trim($url)==''){continue;}
+			
+			$options=array(
+							'url'=>$url,
+							'permission_id'=>$perm_id
+						);
+			$result=$this->db->insert('permission_urls',$options);			
+			
+			if (!$result)
+			{
+				return FALSE;
+			}
+		}
+		
+		return TRUE;
+	}
+	
+	
+	/**
+	*
+	* Add new permission description + urls
+	**/
+	public function add_permission($options)
+	{		
+		$valid_fields=array('label','section','description','weight');
+		
+		$data=array();
+		
+		//build update statement
+		foreach($options as $key=>$value)
+		{
+			if (in_array($key,$valid_fields) )
+			{
+				$data[$key]=$value;
+			}
+		}
+				
+		//add to permission description table
+		$result=$this->db->insert('permissions', $data); 
+		
+		if (!$result)
+		{
+			return FALSE;
+		}
+		
+		//id for newly added row
+		$perm_id=$this->db->insert_id();
+	
+		//assign new urls
+		foreach($options['url'] as $url)
+		{
+			if (trim($url)==''){continue;}
+			
+			$options=array(
+							'url'=>$url,
+							'permission_id'=>$perm_id
+						);
+			$result=$this->db->insert('permission_urls',$options);			
+			
+			if (!$result)
+			{
+				return FALSE;
+			}
+		}
+		
+		return TRUE;
+	}
+	
+	public function delete($perm_id)
+	{
+		//remove from permissions
+		$this->db->where('id',$perm_id);
+		$this->db->delete('permissions');
+		
+		//remove permission urls
+		$this->db->where('permission_id',$perm_id);
+		$this->db->delete('permission_urls');
+	}
+		
+}//end class
