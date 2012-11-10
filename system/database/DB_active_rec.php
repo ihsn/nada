d2c9d163f74be16bdd292f0142477034a0561773
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-11-10 10:53:58 -0500

    Modified system core to add a helper function "get_compiled_select".

--- system/database/DB_active_rec.php
+++ system/database/DB_active_rec.php
@@ -2039,6 +2039,35 @@ class CI_DB_active_record extends CI_DB_driver {
 
 		$this->_reset_run($ar_reset_items);
 	}
+	
+	
+	/**
+	 * Get SELECT query string
+	 *
+	 * Compiles a SELECT query string and returns the sql.
+	 *
+	 * @access  public
+	 * @param   string  the table name to select from (optional)
+	 * @param   boolean TRUE: resets AR values; FALSE: leave AR vaules alone
+	 * @return  string
+	 */
+	public function get_compiled_select($table = '', $reset = TRUE)
+	{
+		if ($table != '')
+		{
+			$this->_track_aliases($table);
+			$this->from($table);
+		}
+	
+		$select =  $this->_compile_select();
+	
+		if ($reset === TRUE)
+		{
+			$this->_reset_select();
+		}
+	
+		return $select;
+	}
 }
 
 /* End of file DB_active_rec.php */
