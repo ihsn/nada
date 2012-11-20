31d8691228b2194810900b35f34394e3f0a5f3e3
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-11-20 11:25:59 -0500

    Cleaned up view for survey tags

--- application/views/catalog/admin_tags.php
+++ application/views/catalog/admin_tags.php
@@ -1,63 +1,59 @@
-<style type="text/css">
-	div#admin_tags {
-		width: 100%;
-		height: 200px;
-	}
-
-	div#admin_tags form {
-	}
-	
-	ul#admin_tags {
-		height: 200px;
-		width: 100%;
-		float: left;
-		list-style-type: square !important;
-	}
-	
-	ul#admin_tags li {
-		padding: 5px 0;
-		float: left;
-		clear:both;
-		height: auto;
-		margin-left: 15px;
-		list-style-type: square !important;	
-	}
-	ul#admin_tags li small {
-		color: #666;
-		font-size:7pt;
-	}
-</style>
-<form method="post" action="">
+<?php
+$tag_array=array();
+foreach($tag_list as $t)
+{
+	$tag_array[]='&quot;'.$t['tag'].'&quot;';
+}
+$tag_str=implode(',',$tag_array);
+?>
 <div class="field">
-            <input id="tag" type="text" name="admin_tag" class="input-flex" >
-            <input type="button" value="+" name="admin_tag_submit" style="border:1px solid gainsboro;padding:3px 5px 3px 5px;">
-        </div>
-</form>
-<div style="overflow:auto;margin-left:20px">
+    <input id="tag" type="text" name="admin_tag" class="input-flex" style="width:70%" data-provide="typeahead" data-items="4" data-source="[<?php echo $tag_str;?>]">
+    <input id="admin_tag_submit" type="button" value="+" name="admin_tag_submit" style="border:1px solid gainsboro;padding:3px 5px 3px 5px;">
+</div>
+<div class="vscroll">
+<?php $this->load->view("catalog/survey_tags_list");?>    
+</div>
+
 <script type="text/javascript">
 $(function() {
-	$("ul#admin_tags li a").live('click', function() {
-		id=$(this).parent().attr('id');
-		$.get("<?php echo site_url('admin/catalog_tags/delete'); ?>/"+id);
-		$(this).parent().remove();
-	});
-	$("input[name='admin_tag_submit']").click(function(e) {
-		data = {
-			tag: $("input[name='admin_tag']").val(),
-			type: 'admin'
-		};
-		$.post("<?php echo site_url('admin/catalog_tags/add') . '/' . $this->uri->segment(4); ?>", data, function(data) {
-			$("ul#admin_tags").html(data);
-		});
-		$("input[name='admin_tag']").val('');
+	//remove tags
+	remove_tag_handler();
 	
+	//add tags
+	$("#admin_tag_submit").click(function(e) {
+		add_tag();
 		return false;
 	});
 });
+
+function remove_tag_handler(){
+	$("#admin_tags .remove").unbind("click").on('click', function() {
+		id=$(this).attr('itemid');
+		$.get("<?php echo site_url('admin/catalog_tags/delete'); ?>/"+id);
+		$(this).parent().remove();
+	});
+}
+
+function add_tag() {	
+	data = {
+		tag: $("#tag").val(),
+		type: 'admin'
+	};
+	url=CI.base_url+'/admin/catalog_tags/add/<?php echo $this->uri->segment(4); ?>';
+	$.ajax({
+        type: "POST",
+        url: url,
+        cache: false,
+		timeout:30000,
+		data:data,
+		success: function(data) {
+			$("ul#admin_tags").html(data);
+			$("#tag").val("");
+			remove_tag_handler();
+        },
+		error: function(XMLHttpRequest, textStatus, errorThrow) {
+			alert(XMLHttpRequest.responseText);
+        }		
+    });
+}
 </script>
-<ul id="admin_tags">
-	<?php foreach($tags as $tag) {
-		echo "	<li id='{$tag['id']}'>{$tag['tag']}&nbsp;&nbsp;<a href='javascript:void(0);' style='text-decoration:none'>-</a></li>", PHP_EOL;
-	} ?>
-</ul>
-</div>
