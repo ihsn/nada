2c255750160094e434a95ae660b6f3dd34a20c80
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-11-20 11:26:34 -0500

    minor

--- application/views/catalog/admin_notes.php
+++ application/views/catalog/admin_notes.php
@@ -28,7 +28,7 @@
 </style>
 <form method="post" action="">
 <div class="field">
-            <input id="tag" type="text" name="admin_note" class="input-flex" >
+            <input id="admin_note" type="text" name="admin_note" class="input-flex" >
             <input type="button" value="+" name="admin_note_submit" style="border:1px solid gainsboro;padding:3px 5px 3px 5px;">
         </div>
 </form>
