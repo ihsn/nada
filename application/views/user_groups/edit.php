b41ac574ea6476886ee4234e4878fdc72f68106e
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2012-12-07 13:45:03 -0500

    minor

--- application/views/user_groups/edit.php
+++ application/views/user_groups/edit.php
@@ -33,15 +33,16 @@
 		<select name="group_type">
        		<option selected="selected" value="<?php echo get_form_value('group_type',isset($group_type) ? $group_type : ''); ?>"><?php echo get_form_value('group_type',ucfirst(isset($group_type) ? $group_type : '')); ?></option>
   			<option value="admin">Admin</option>
+            <option value="reviewer">Reviewer</option>
   			<option value="user">User</option>
 
 		</select>
     </div>
     
     <div class="field">
-        <label for="repo_access"><?php echo t('repo_access');?></label>
-		<select name="repo_access">
-       		<option value="<?php echo get_form_value('repo_access',isset($repo_access) ? $repo_access : ''); ?>"><?php echo get_form_value('type',ucfirst(isset($repo_access) ? $repo_access : '')); ?></option>
+        <label for="access_type"><?php echo t('access_type');?></label>
+		<select name="access_type">
+       		<option value="<?php echo get_form_value('access_type',isset($access_type) ? $access_type : ''); ?>"><?php echo get_form_value('type',ucfirst(isset($access_type) ? $access_type : '')); ?></option>
   			<option value="none">None</option>
   			<option value="limited">Limited</option>
   			<option value="unlimited">Unlimited</option>
@@ -51,7 +52,7 @@
 <?php
  //edit user
 	echo form_submit('submit',t('update'),'id="btnupdate"'); 
- 	echo anchor('admin/user_groups',t('cancel'),array('class'=>'button') );	
+ 	echo anchor('admin/user_groups',t('cancel'));	
 ?>
 
 <? echo form_close(); ?>    
