254b021525fc2c453a4b20484a9632f3dc4b7a1e
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-01-18 16:01:01 -0500

    new additions

--- application/language/english/harvester_lang.php
+++ application/language/english/harvester_lang.php
@@ -29,5 +29,26 @@ $lang['msg_no_repo_access']='You don\'t have permissions to access any catalogs.
 $lang['repositories_management'] = "Manage repositories";
 $lang['repositories_regional'] = "Regional Repositories";
 $lang['repositories_specialized'] = "Specialized Repositories";
+$lang['callback_error_repositoryid_exists'] = "Choose a unique name [Characters and Numbers only) for Repository Identification.";
+$lang['manage_repo_sections'] = "Repository Sections";
+$lang['repository_admins'] = "Manage Users";
+$lang['da_by_collection'] = "Data Access by Collection";
+$lang['da_by_collection_description'] = "This allows users to download all Public Use Data from all surveys belonging to this repository.";
+
+$lang['repository_sections'] = 'Repository Sections';
+$lang['repository_sections_management'] = 'Repository Sections';
+$lang['description'] = 'Description';
+$lang['weight'] = 'Weight';
+$lang['repository_sections_home'] = 'Home';
+$lang['repository_section_edit'] = 'Edit Repository Section';
+$lang['repository_section_add'] = 'Add Repository Section';
+$lang['save_permissions'] = 'Save permissions';
+$lang['repository_permissions'] = 'Manage Repository Permissions';
+
+$lang['group_name'] = 'Group name';
+$lang['group_description'] = 'Description';
+$lang['group_type'] = 'Account type';
+$lang['group_access'] = 'Access';
+
 /* End of file harvester_lang.php */
 /* Location: ./system/language/english/harvester_lang.php */
