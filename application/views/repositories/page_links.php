60183b31c8dd3fe67d8bca2f613c743e5d6ae53f
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-01-18 16:14:42 -0500

    minor

--- application/views/repositories/page_links.php
+++ application/views/repositories/page_links.php
@@ -1,7 +1,9 @@
 <div class="page-links">
-	<a href="<?php echo site_url();?>/admin/repositories" class="button"><img src="images/icon_plus.gif"/><?php echo t('repositories');?></a> 
+	<?php if ($this->uri->segment(3)!=''):?>
+    	<a href="<?php echo site_url();?>/admin/repositories" class="button"><img src="images/icon_plus.gif"/><?php echo t('repositories');?></a> 
+    <?php endif;?>
     <a href="<?php echo site_url();?>/admin/repositories/add" class="button"><img src="images/icon_plus.gif"/><?php echo t('create_repository');?></a> 
-    <a href="<?php echo site_url();?>/admin/repositories/users" class="button"><img src="images/icon_plus.gif"/><?php echo t('repository_admins');?></a> 
-    <a href="<?php echo site_url();?>/admin/harvester/" class="button"><img src="images/icon_plus.gif"/><?php echo t('harvester_queue');?></a> 
+    <?php /* ?><a href="<?php echo site_url();?>/admin/repositories/users" class="button"><img src="images/icon_plus.gif"/><?php echo t('repository_admins');?></a> <?php */ ?>
+    <?php /*?><a href="<?php echo site_url();?>/admin/harvester/" class="button"><img src="images/icon_plus.gif"/><?php echo t('harvester_queue');?></a> <?php */?>    
     <a href="<?php echo site_url();?>/admin/repository_sections/" class="button"><img src="images/icon_plus.gif"/><?php echo t('manage_repo_sections');?></a> 
 </div>
