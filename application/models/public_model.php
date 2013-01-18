038a1f6d301ed4689763608aa3752fa16d65a1ae
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-01-18 16:18:41 -0500

    Added helper functions for PUF

--- application/models/public_model.php
+++ application/models/public_model.php
@@ -31,5 +31,70 @@ class Public_model extends CI_Model {
 	}
 	
 
+	/**
+	*
+	* Return all public use surveys
+	*
+	**/
+	function get_all_public_use_surveys()
+	{
+		$this->db->select('s.id,s.titl,s.nation,s.data_coll_start,s.data_coll_end');
+		$this->db->join('forms', 's.formid = forms.formid','left');
+		$this->db->where('forms.model','public');
+		return $this->db->get('surveys s')->result_array();
+	}
+
+
+	/**
+	* Get a list of PUF surveys by Collection
+	**/
+	function get_surveys_by_collection($repositoryid)
+	{
+		$this->db->select('s.id,s.titl,s.nation,s.data_coll_start,s.data_coll_end,forms.model');
+		$this->db->from('surveys s');
+		$this->db->join('survey_repos repos', 's.id = repos.sid','left');
+		$this->db->join('forms', 'forms.formid = s.formid','inner');
+		$this->db->where('repos.repositoryid',$repositoryid);
+		$this->db->where('forms.model','public');
+		return $this->db->get()->result_array();
+	}
+
+	
+	/**
+	* Check if user has already posted a request for public use for a
+	* survey in the collection
+	*
+	**/	
+	function check_user_public_request_by_collection($user_id,$repositoryid)
+	{
+		//get
+		$this->db->select('s.id');		
+		$this->db->from('public_requests pr');
+		$this->db->join('surveys s', 's.id= pr.surveyid','inner');
+		$this->db->join('survey_repos sr', 'sr.sid = s.id','inner');		
+		$this->db->where('sr.repositoryid',$repositoryid);
+		$this->db->where('pr.userid',$user_id);
+		
+        $result= $this->db->count_all_results();
+		
+		return $result;
+	}
+	
+	/**
+	* Check if user has already posted a request for public use
+	*
+	*
+	**/	
+	function check_user_public_request($user_id,$survey_id)
+	{
+		$this->db->select('id');		
+		$this->db->from('public_requests');		
+		$this->db->where('surveyid',$survey_id);		
+		$this->db->where('userid',$user_id);		
+		
+        $result= $this->db->count_all_results();
+		return $result;
+	}
+	
 }
 ?>
