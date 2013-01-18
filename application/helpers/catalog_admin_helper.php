c9cd982325ff123fcf19b879e74af058a814c6a6
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-01-18 15:59:59 -0500

    Added helpers for catalog admin

---
+++ application/helpers/catalog_admin_helper.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Catalog Helpers
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		Mehmood Asghar
+ * @link		
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Load email settings from database
+ *
+ *
+ * @access	public
+ * @return	array
+ */	
+if ( ! function_exists('filter'))
+{
+
+	function filter()
+	{
+		$ci =& get_instance();
+		$allowed=array(
+					'titl',
+					'surveyid',
+					'producer',
+					'published',
+					'tag',
+					'nation'
+					);
+					
+		$output=array();			
+		foreach($allowed as $key)
+		{
+			if ($ci->input->get($key))
+			{
+				$output[$key]=($ci->input->get($key));
+			}
+		}
+		
+		$result=array();
+		foreach($output as $key=>$value)
+		{
+			if (is_array($value))
+			{
+				$result[]=$key.': ['.implode(', ',$value).']';
+			}
+			else
+			{
+				$result[]=$key.': '.$value.']';
+			}	
+		}
+		
+		return $result;
+	}  
+  
+}
+
+/* End of file email_helper.php */
+/* Location: ./system/helpers/email_helper.php */
