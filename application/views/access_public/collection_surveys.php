4d1754964a6b9c3612e7629c6cca6b147ce2b4b6
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-01-30 11:46:09 -0500

    added a view to show a list of surveys in the collection

---
+++ application/views/access_public/collection_surveys.php
@@ -0,0 +1,10 @@
+<h1 class="page-title"><?php echo t('public_use_data_access_by_collection');?></h1>
+<p><?php echo t('public_use_data_access_by_collection_message');?></p>
+<table class="grid-table">
+<?php $k=1;foreach($surveys as $survey):?>
+<tr class="row">
+	<td><?php echo $k++;?></td>
+	<td><a target="_blank" href="<?php echo site_url('catalog/'.$survey['id']);?>"><?php echo $survey['nation'];?> - <?php echo $survey['titl'];?></a></td>
+</tr>
+<?php endforeach;?>
+</table>
