9edefce97026dd9d0a0c5bfa00b6f929dc44ccf6
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-24 09:41:13 -0400

    added helper for custom pager

---
+++ application/helpers/pagination_helper.php
@@ -0,0 +1,75 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Create custom pager
+ *
+ * @access	public
+ * @param	total_records	number of total records
+ * @param	limit			how many pages to show
+ * @param	current_page	current page - gets highlighted as active
+ * @param	adjacents		how many page numbers to show before and after current page
+ *
+ * @return	string	httml formatted pager
+ */	
+if ( ! function_exists('pager'))
+{
+	function pager($total_records, $limit = null, $current_page = null, $adjacents = null)
+	{
+		$total_pages=ceil($total_records / $limit);
+		
+		if ($total_pages<2)
+		{
+			return false;
+		}
+		
+		$result = range(1, ceil($total_records / $limit));	
+		$search_qs='?'.get_sess_querystring( array('ps','sk', 'vk', 'vf','view','topic','country','from','to'),'search');
+		
+		if (($adjacents = floor($adjacents / 2) * 2 + 1) >= 1)
+		{
+			$result = array_slice($result, max(0, min(count($result) - $adjacents, intval($current_page) - ceil($adjacents / 2))), $adjacents);
+		}
+		
+		$output=array();
+		if ($current_page>1 && $total_pages>1)
+		{
+			$output[]=sprintf('<a href="%s" class="page" data-page="%s">Prev</a>',
+						site_url('catalog/').$search_qs.'&page='.($current_page-1),
+						$current_page-1
+						);
+		}	
+		
+		foreach($result as $page)
+		{
+			$css='';
+			if($page==$current_page)
+			{
+				$css='active';
+			}
+			
+			$output[]=sprintf('<a href="%s" class="page %s" data-page="%s">%s</a>',
+						site_url('catalog/').$search_qs.'&page='.$page,
+						$css,
+						$page,
+						$page);
+		}
+		
+		if ($current_page<$total_pages)
+		{
+			$output[]=sprintf('<a href="%s" class="page" data-page="%s">Next</a>',
+					site_url('catalog/').$search_qs.'&page='.($current_page+1),
+					$current_page+1
+					);
+		}
+	
+		$pager='<div class="pager">';
+		$pager.=implode('',$output);
+		$pager.='</div>';
+		
+		return $pager;
+	}
+}
+
+
+
+/* End of file pagination_helper.php */
+/* Location: ./application/helpers/pagination_helper.php */
