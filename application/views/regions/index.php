0bdb0612ec1002daaf04cde2c067d37b994ace52
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-24 10:02:23 -0400

    Added views for country regions

---
+++ application/views/regions/index.php
@@ -0,0 +1,65 @@
+<style>
+.parent{font-weight:bold;}
+.child{margin-left:25px;}
+</style>
+<div class="body-container" style="padding:10px;">
+
+<div class="page-links">
+	<a href="<?php echo site_url(); ?>/admin/vocabularies/add" class="button"><img src="images/icon_plus.gif"/><?php echo t('add_vocabulary');?></a> 
+</div>
+        
+<?php $message=$this->session->flashdata('message');?>
+<?php echo ($message!="") ? '<div class="success">'.$message.'</div>' : '';?>
+
+<?php $error=$this->session->flashdata('error');?>
+<?php echo ($error!="") ? '<div class="error">'.$error.'</div>' : '';?>
+
+<h1 class="page-title"><?php echo t('Regions');?></h1>
+
+<?php if($tree):?>
+	 <!-- grid -->
+    <table class="grid-table" width="100%" cellspacing="0" cellpadding="0">
+    	<tr class="header">
+            <th><?php echo t('ID');?></th>
+            <th><?php echo t('PID');?></th>
+            <th><?php echo t('Region');?></th>            
+			<th>&nbsp;</th>
+        </tr>
+	<?php $tr_class=""; ?>
+	<?php /* ?>
+	<?php foreach($rows as $row): ?>
+    	<?php $row=(object)$row;?>        
+		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+    	<tr class="<?php echo $tr_class; ?>">
+	        <td><?php echo $row->id;?></td>
+            <td><?php echo $row->pid;?></td>
+            <td><a href="<?php echo site_url();?>/admin/regins/edit/<?php echo $row->id;?>"><?php echo $row->title; ?></a></td>            
+			<td>
+                <a href="<?php echo site_url();?>/admin/regions/edit/<?php echo $row->id;?>"><?php echo t('edit');?></a> | 
+                <a href="<?php echo site_url();?>/admin/regions/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a>
+            </td>
+        </tr>
+    <?php endforeach;?>
+	<?php */ ?>
+
+
+	<?php foreach($tree as $row): ?>
+    	<?php $row=(object)$row;?>        
+		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+    	<tr class="<?php echo $tr_class; ?>">
+	        <td><?php echo $row->id;?></td>
+            <td><?php echo $row->pid;?></td>
+            <td><a class="parent" href="<?php echo site_url();?>/admin/regins/edit/<?php echo $row->id;?>"><?php echo $row->title; ?></a></td>            
+			<td>
+                <a href="<?php echo site_url();?>/admin/regions/edit/<?php echo $row->id;?>"><?php echo t('edit');?></a> | 
+                <a href="<?php echo site_url();?>/admin/regions/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a>
+            </td>
+        </tr>
+        <?php if ($row->children):?>
+        	<?php $this->load->view('regions/region_children',array('children'=>$row->children, 'tr_class'=>$tr_class));?>
+        <?php endif;?>
+    <?php endforeach;?>
+<?php else:?>
+	<?php echo t('no_records_found');?>
+<?php endif;?>    
+</div>
---
+++ application/views/regions/region_children.php
@@ -0,0 +1,13 @@
+<?php foreach($children as $row): ?>
+	<?php $row=(object)$row;?>        
+	<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+	<tr class="<?php echo $tr_class; ?>">
+		<td><?php echo $row->id;?></td>
+		<td><?php echo $row->pid;?></td>
+		<td><a class="child" href="<?php echo site_url();?>/admin/regins/edit/<?php echo $row->id;?>"><?php echo $row->title; ?></a></td>            
+		<td>
+			<a href="<?php echo site_url();?>/admin/regions/edit/<?php echo $row->id;?>"><?php echo t('edit');?></a> | 
+			<a href="<?php echo site_url();?>/admin/regions/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a>
+		</td>
+	</tr>
+<?php endforeach;?>
