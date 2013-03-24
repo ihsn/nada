df3db4f60f27e25d2b69ff6a52a39f14c3fa03e4
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-24 10:01:45 -0400

    added views for countries vocabulary

---
+++ application/views/countries/edit.php
@@ -0,0 +1,68 @@
+<style>
+.input-fixed-1{width:300px;}
+</style>
+<div class='content-container'>
+    <div class="page-links">
+        <a href="<?php echo site_url(); ?>/admin/countries" class="button"><img src="images/house.png"/><?php echo t('home');?></a>
+    </div>
+	<h1><?php echo t('Edit Country'); ?></h1>
+	<?php if (validation_errors() ) : ?>
+        <div class="error">
+            <?php echo validation_errors(); ?>
+        </div>
+    <?php endif; ?>
+    
+    <?php $error=$this->session->flashdata('error');?>
+    <?php echo ($error!="") ? '<div class="error">'.$error.'</div>' : '';?>
+        
+    <?php $message=$this->session->flashdata('message');?>
+    <?php echo ($message!="") ? '<div class="success">'.$message.'</div>' : '';?>
+
+	<?php
+		//form action url
+		$uri_arr=$this->uri->segment_array();
+		$form_action_url=site_url().'/admin/countries';
+		$countryid=$this->uri->segment(4);
+		if (is_numeric($countryid))
+		{
+			$form_action_url.='/edit/'.$countryid;
+		}
+		else
+		{
+			$form_action_url.='/add/';
+		}
+		
+		$aliases=get_form_value('alias',isset($aliases) ? $aliases: array('') );
+		//echo '<PRE>';
+		//var_dump($aliases);
+	?>
+	
+    <?php echo form_open($form_action_url, array('class'=>'form'));?>
+
+    
+    <div class="field">
+        <label for="title"><?php echo t('name');?><span class="required">*</span></label>
+        <input class="input-fixed-1" name="name" type="text" id="name"  value="<?php echo get_form_value('name',isset($name) ? $name : ''); ?>"/>
+    </div>
+
+    <div class="field">
+        <label for="iso"><?php echo t('iso');?><span class="required">*</span></label>
+        <input class="input-fixed-1" name="iso" type="text" id="iso"  value="<?php echo get_form_value('iso',isset($iso) ? $iso : ''); ?>"/>
+    </div>
+
+    <div class="field">
+	    <label><?php echo t('Aliases');?></label>
+    	<?php for($k=0;$k<5;$k++):?>
+            <div class="alias">
+            <input class="input-fixed-1" name="alias[]" type="text" id="alias-<?php echo $k;?>"  value="<?php echo (isset($aliases[$k]) ? $aliases[$k] : ''); ?>"/>
+            </div>
+	    <?php endfor;?>
+    </div>
+	
+	
+      <?php echo form_submit('submit', t('update'));?>
+      <?php echo anchor('admin/countries/', t('cancel'));?>
+      
+    <?php echo form_close();?>
+
+</div>
---
+++ application/views/countries/index.php
@@ -0,0 +1,48 @@
+<div class="body-container" style="padding:10px;">
+
+<div class="page-links">
+	<a href="<?php echo site_url(); ?>/admin/vocabularies/add" class="button"><img src="images/icon_plus.gif"/><?php echo t('add_vocabulary');?></a> 
+</div>
+        
+<?php $message=$this->session->flashdata('message');?>
+<?php echo ($message!="") ? '<div class="success">'.$message.'</div>' : '';?>
+
+<?php $error=$this->session->flashdata('error');?>
+<?php echo ($error!="") ? '<div class="error">'.$error.'</div>' : '';?>
+
+<h1 class="page-title"><?php echo t('Countries');?></h1>
+
+<?php if($rows):?>
+	<div><?php echo t('Countries');?>: <?php echo count($rows);?></div>
+	 <!-- grid -->
+    <table class="grid-table" width="100%" cellspacing="0" cellpadding="0">
+    	<tr class="header">
+        	<th><?php echo t('ID');?></th>
+            <th><?php echo t('iso');?></th>
+            <th><?php echo t('country');?></th>
+            <th><?php echo t('aliases');?></th>
+			<th>&nbsp;</th>
+        </tr>
+	<?php $tr_class=""; ?>
+	<?php foreach($rows as $row): ?>
+    	<?php $row=(object)$row;?>        
+		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+    	<tr class="<?php echo $tr_class; ?>" valign="top">            
+            <td><?php echo $row->countryid;?></td>
+            <td><?php echo $row->iso;?></td>
+            <td><a href="<?php echo site_url();?>/admin/countries/edit/<?php echo $row->countryid;?>"><?php echo $row->name; ?></a></td>
+            <td>
+            	<?php if(array_key_exists($row->countryid,$aliases) ):?>
+                	<div><?php echo implode("<br/>",$aliases[$row->countryid]);?></div>
+                <?php endif;?>
+            </td>
+			<td>
+                <a href="<?php echo site_url();?>/admin/countries/edit/<?php echo $row->countryid;?>"><?php echo t('edit');?></a> | 
+                <a href="<?php echo site_url();?>/admin/countries/delete/<?php echo $row->countryid;?>"><?php echo t('delete');?></a>
+            </td>
+        </tr>
+    <?php endforeach;?>
+<?php else:?>
+	<?php echo t('no_records_found');?>
+<?php endif;?>    
+</div>
