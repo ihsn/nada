aaf80514a8e3511a6ffaac9fc62e4d59783e6b49
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-24 09:55:13 -0400

    updated licensed email notifications for by collection access

--- application/views/access_licensed/admin_notification_email.php
+++ application/views/access_licensed/admin_notification_email.php
@@ -4,9 +4,18 @@
 *
 */
 ?>
+<?php if($request_type='study'):?>
 <p>
-    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $survey_title);?></p>
+    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $surveys[0]['titl']);?></p>
     <p><?php echo t('to_view_request_instructions');?><p>
     <p>-- </p>
     <p><?php echo t('Website');?>: <a href="<?php echo site_url();?>"><?php echo site_url();?></a></p>
 </p>
+<?php else:?>
+<p>
+    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, 'collection['.$collection['title'].']');?></p>
+    <p><?php echo t('to_view_request_instructions');?><p>
+    <p>-- </p>
+    <p><?php echo t('Website');?>: <a href="<?php echo site_url();?>"><?php echo site_url();?></a></p>
+</p>
+<?php endif;?>
