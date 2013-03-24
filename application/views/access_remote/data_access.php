ad4b1aa9c1a0d4cbf950736102a482d963341ab1
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-24 09:58:06 -0400

    new view for showing link to remote site

---
+++ application/views/access_remote/data_access.php
@@ -0,0 +1,7 @@
+<div class="remote-data-access">
+	<h2><?php echo t('remote_title');?></h2>
+	<p><?php echo t('data_is_available_from_external_repository');?></p>
+    <div class="remote-access-link">
+        <?php echo anchor($link,$link);?>
+    </div>
+</div>
