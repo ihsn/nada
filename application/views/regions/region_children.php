7a8b0ff15cd243ebe5409e35575832086319d1f7
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-26 16:49:21 -0400

    misc

--- application/views/regions/region_children.php
+++ application/views/regions/region_children.php
@@ -4,7 +4,7 @@
 	<tr class="<?php echo $tr_class; ?>">
 		<td><?php echo $row->id;?></td>
 		<td><?php echo $row->pid;?></td>
-		<td><a class="child" href="<?php echo site_url();?>/admin/regins/edit/<?php echo $row->id;?>"><?php echo $row->title; ?></a></td>            
+		<td><a class="child" href="<?php echo site_url();?>/admin/regions/edit/<?php echo $row->id;?>"><?php echo $row->title; ?></a></td>            
 		<td>
 			<a href="<?php echo site_url();?>/admin/regions/edit/<?php echo $row->id;?>"><?php echo t('edit');?></a> | 
 			<a href="<?php echo site_url();?>/admin/regions/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a>
