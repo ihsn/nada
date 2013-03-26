8caeba9642dd12f24cba807887d8ea04058c5fee
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-26 19:48:18 -0400

    Added view for fixing coutry mappings

---
+++ application/views/countries/mappings.php
@@ -0,0 +1,57 @@
+<div class="body-container" style="padding:10px;">
+
+<?php $message=$this->session->flashdata('message');?>
+<?php echo ($message!="") ? '<div class="success">'.$message.'</div>' : '';?>
+
+<?php $error=$this->session->flashdata('error');?>
+<?php echo ($error!="") ? '<div class="error">'.$error.'</div>' : '';?>
+
+<h1 class="page-title"><?php echo t('Fix Country Mapping');?></h1>
+
+<div class="country-dropdown" style="display:none;">
+<?php echo form_dropdown('cid', $country_list,NULL); ?>
+</div>
+
+<?php if($rows):?>
+	<div><?php echo t('Total rows');?>: <?php echo count($rows);?></div>
+	 <!-- grid -->
+    <table class="grid-table" width="100%" cellspacing="0" cellpadding="0">
+    	<tr class="header">
+        	<th><?php echo t('Country');?></th>
+            <th><?php echo t('Assign Country');?></th>
+			<th>&nbsp;</th>
+        </tr>
+	<?php $tr_class=""; ?>
+	<?php foreach($rows as $row): ?>
+    	<?php $row=(object)$row;?>        
+		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+       
+    	<tr class="<?php echo $tr_class; ?>" valign="top">
+            <td><a href="<?php echo site_url();?>/admin/countries/edit/<?php echo $row->country_name;?>"><?php echo $row->country_name; ?></a> (<?php echo $row->total;?>)</td>
+            <td> 
+            	<form method="get" action="<?php echo site_url('admin/countries/fix_mappings');?>">
+                    <input type="hidden" name="name" value="<?php echo $row->country_name;?>"/>
+                    <span class="country-list"></span>
+                    <input type="submit" name="Submit" value="Update"/>
+            	</form>
+            </td>
+			<td>
+                
+            </td>
+        </tr>
+        
+    <?php endforeach;?>
+<?php else:?>
+	<?php echo t('no_records_found');?>
+<?php endif;?>    
+</div>
+
+
+<script type="application/javascript">
+$(document).ready(function() 
+{	
+	$(".country-list").html($(".country-dropdown select"));
+	
+});
+
+</script>
