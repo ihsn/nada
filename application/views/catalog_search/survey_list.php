a220036b078a81e03c216dcca2805f6945135a36
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-27 17:12:13 -0400

    Minor clean up

--- application/views/catalog_search/survey_list.php
+++ application/views/catalog_search/survey_list.php
@@ -154,8 +154,6 @@
 		}*/
 	?>
 	<div class="survey-row">
-        <div class="left">
-        
         	<div class="data-access-icon data-access-<?php echo $row['form_model'];?>"></div>
             <h2 class="title">
                 <a href="<?php echo site_url(); ?>/catalog/<?php echo $row['id']; ?>"  title="<?php echo $row['titl']; ?>" >
@@ -199,29 +197,6 @@
             	<?php endif;?> 
             </div>
 		
-		</div>
-		<div class="right">
-        
-       		<?php if ($row['form_model']!=''):?>
-	            <a href="<?php echo site_url(); ?>/catalog/<?php echo $row['id']; ?>"  title="<?php echo $row['titl']; ?>" >        
-		        <?php if($row['form_model']=='direct'): ?>
-                    <span title="<?php echo t('link_data_direct_hover');?>"><img src="images/form_direct.gif" /></span>                    
-                <?php elseif($row['form_model']=='public'): ?>                    
-                    <span  title="<?php echo t('link_data_public_hover');?>"><img src="images/form_public.gif" /></span>
-                <?php elseif($row['form_model']=='licensed'): ?>
-                    <span title="<?php echo t('link_data_licensed_hover');?>"><img src="images/form_licensed.gif" /></span>
-                <?php elseif($row['form_model']=='data_enclave'): ?>
-                    <span title="<?php echo t('link_data_enclave_hover');?>"><img src="images/form_enclave.gif" /></span>
-                <?php elseif($row['form_model']=='remote'): ?>
-                	<?php //if (isset($row['link_da']) && strlen($row['link_da'])>1):?>
-                    <span title="<?php echo t('link_data_remote_hover');?>"><img src="images/form_remote.gif" /></span>
-                	<?php //endif; ?>
-				<?php endif; ?>
-                </a>
-            <?php endif;?> 
-			           
-        </div>
-        
         <?php if ( isset($row['var_found']) ): ?>
             <div class="variables-found" style="clear:both;">
                     <a class="vsearch" style="outline:none;" href="<?php echo site_url(); ?>/catalog/vsearch/<?php echo $row['id']; ?>/?<?php echo $variable_querystring; ?>">
@@ -231,89 +206,6 @@
                     <span class="vsearch-result"></span>
             </div>
             <?php endif; ?>
-        <?php /* ?>
-        <div class="survey-icons" >
-            <div style="float: left;">                
-
-                <a target="_blank" title="<?php echo t('link_browse_metadata_hover');?>" href="<?php echo site_url().'/ddibrowser/'.$row['id'];?>">
-                <span><img src="images/page_white_cd.png" /><?php echo t('link_browse_metadata');?></span>
-                </a>
-                
-                <a id="ap-<?php echo $row['id'];?>" class="accesspolicy"  title="<?php echo t('link_access_policy_hover');?>" href="<?php echo site_url().'/catalog/access_policy/'.$row['id'];?>">
-                	<span><img src="images/page_white_key.png" /><?php echo t('link_access_policy');?></span>
-                </a>
-                                
-                <?php if($row['form_model']=='direct'): ?>
-                    <a href="<?php echo site_url().'/access_direct/'.$row['id'];?>" class="accessform" title="<?php echo t('link_data_direct_hover');?>">
-                    <span><img src="images/form_direct.gif" /><?php echo t('link_data');?></span>
-                    </a>                    
-                <?php elseif($row['form_model']=='public'): ?>                    
-                    <a href="<?php echo site_url().'/access_public/'.$row['id'];?>" class="accessform"  title="<?php echo t('link_data_public_hover');?>">
-                    <span><img src="images/form_public.gif" /><?php echo t('link_data');?></span>
-                    </a>                    
-                <?php elseif($row['form_model']=='licensed'): ?>
-                    <a href="<?php echo site_url().'/access_licensed/'.$row['id'];?>" class="accessform"  title="<?php echo t('link_data_licensed_hover');?>">
-                    <span><img src="images/form_licensed.gif" /><?php echo t('link_data');?></span>
-                    </a>                    
-                <?php elseif($row['form_model']=='data_enclave'): ?>
-                    <a href="<?php echo site_url().'/access_enclave/'.$row['id'];?>" class="accessform"  title="<?php echo t('link_data_enclave_hover');?>">
-                    <span><img src="images/form_enclave.gif" /><?php echo t('link_data');?></span>
-                    </a>                    
-                <?php elseif($row['form_model']=='remote'): ?>
-                	<?php if (isset($row['link_da']) && strlen($row['link_da'])>1):?>
-                    <a target="_blank" href="<?php echo $row['link_da'];?>"  title="<?php echo t('link_data_remote_hover');?>">
-                    <span><img src="images/form_remote.gif" /><?php echo t('link_data');?></span>
-                    </a>                    
-                	<?php endif; ?>
-				<?php endif; ?>
-
-                <?php if (in_array($row['id'],$citations)): ?>
-                    <a href="<?php echo site_url().'/catalog/citations/'.$row['id'];?>" title="<?php echo t('link_citations_hover');?>" >
-                    <span><img src="images/book_open.png" /><?php echo t('link_citations');?></span>
-                    </a>                    
-                <?php endif;?>
-                
-            </div>
-            <div style="float: right;">
-				<?php if($row['link_report']!=''): ?>
-                    <a target="_blank" href="<?php echo site_url().'/catalog/download/'.$row['id'].'/'.base64_encode($row['link_report']);?>" title="<?php echo t('link_reports_hover');?>">
-                        <img border="0" title="<?php echo t('link_reports');?>" alt="<?php echo t('link_reports');?>" src="images/report.png" />
-                    </a>
-                <?php endif; ?>
-
-                <?php if($row['link_indicator']!=''): ?>
-                    <a target="_blank"  href="<?php echo site_url().'/catalog/download/'.$row['id'].'/'.base64_encode($row['link_indicator']);?>" title="<?php echo t('link_indicators_hover');?>">
-                        <img border="0" alt="<?php echo t('link_indicators');?>" src="images/page_white_database.png" />
-                    </a>
-                <?php endif; ?>
-
-                <?php if($row['link_questionnaire']!=''): ?>
-                    <a target="_blank"  href="<?php echo site_url().'/catalog/download/'.$row['id'].'/'.base64_encode($row['link_questionnaire']);?>" title="<?php echo t('link_questionnaires_hover');?>">
-                        <img border="0" alt="<?php echo t('link_questionnaires');?>" title="<?php echo t('link_questionnaires');?>" src="images/page_question.png" />
-                    </a>
-                <?php endif; ?>
-
-                <?php if($row['link_technical']!=''): ?>
-                    <a  target="_blank" href="<?php echo site_url().'/catalog/download/'.$row['id'].'/'.base64_encode($row['link_technical']);?>" title="<?php echo t('link_technical_hover');?>">
-                        <img border="0" alt="<?php echo t('link_technical');?>" title="<?php echo t('link_technical_hover');?>" src="images/page_white_compressed.png" />
-                    </a>
-                <?php endif; ?>
-
-                <?php if($row['link_study']!=''): ?>
-                    <a  target="_blank" href="<?php echo site_url().'/catalog/download/'.$row['id'].'/'.base64_encode($row['link_study']);?>" title="<?php echo t('link_study_website_hover');?>">
-                        <img border="0" title="<?php echo t('link_study_website_hover');?>" alt="<?php echo t('link_study_website');?>" src="images/page_white_world.png" />
-                    </a>
-                <?php endif; ?>
-
-				 <?php //if($row['isshared']!=''): ?>
-                    <a href="<?php echo site_url().'/catalog/ddi/'.$row['id'];?>" title="<?php echo t('link_ddi_hover');?>">
-                        <img border="0" title="<?php echo t('link_ddi');?>" alt="DDI" src="images/ddi2.gif" />
-                    </a>
-                <?php //endif; ?>
-            </div>
-            <br/>
-        </div>
-		<?php */?>      		
     </div>    
 <?php endforeach;?>
 
