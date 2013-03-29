dc1389611ea78bcb9fd12d1bfc9385cdc655e115
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-29 14:00:36 -0400

    added new view for collection tabs [about,datasets,citations] and collections about page

---
+++ application/views/catalog_search/study_collection_tabs.php
@@ -0,0 +1,28 @@
+<?php
+//active tab=active_tab
+$active_class="ui-tabs-active ui-state-active";
+if(!isset($active_tab))
+{
+	$active_tab='catalog';
+}
+
+if ($repo['repositoryid']=='central'){echo $content;return;}
+?>
+<h2><?php echo $repo['title'];?></h2>
+<div class="tab-style-1">
+<div id="collection-tabs" style="" class="ui-tabs ui-widget ui-widget-content ui-corner-all study-tabs">
+
+	<div class="tab-heading">
+		<div class="content-container" style="overflow:auto;"></div><a name="tab"></a></div>
+
+  <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist" style="background:none;margin-top:-35px;border-bottom:1px solid gainsboro;">
+    <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='about') ? $active_class : '' ?>" role="tab" tabindex="0" aria-controls="tabs-1" aria-labelledby="ui-id-1" aria-selected="false"><a href="<?php echo site_url('catalog/'.$repo['repositoryid']);?>/about" class="ui-tabs-anchor" role="presentation" tabindex="-1" >About</a></li>
+    <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='catalog') ? $active_class : '' ?>" role="tab" tabindex="-1" aria-controls="tabs-23" aria-labelledby="ui-id-23" aria-selected="true"><a href="<?php echo site_url('catalog/'.$repo['repositoryid']);?>" class="ui-tabs-anchor" role="presentation" tabindex="-1" >Datasets</a></li>
+    <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='citations') ? $active_class : '' ?>" role="tab" tabindex="-1" aria-controls="tabs-3" aria-labelledby="ui-id-4" aria-selected="false"><a href="<?php echo site_url('citations/?collection='.$repo['repositoryid']);?>" class="ui-tabs-anchor" role="presentation" tabindex="-1" >Citations</a></li>
+  </ul>
+  <div id="tabs-1" aria-labelledby="ui-id-1" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" style="" aria-expanded="true" aria-hidden="false">  	
+	  <?php echo $content;?>  
+  </div>
+  
+</div>
+</div>
