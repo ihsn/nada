fbddb91218b4434e4cb9966ae01125785d01a159
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-28 23:17:56 -0400

    minor

--- application/views/catalog_search/variable_list.php
+++ application/views/catalog_search/variable_list.php
@@ -1,51 +1,9 @@
-<div class="active-filters-container">
-
-<div class="search-count">
-<?php if ($found==1):?>
-	<?php echo sprintf(t('found_variable'),$found,$total);?>
-<?php else:?>
-	<?php echo sprintf(t('found_variables'),$found,$total);?>
-<?php endif;?>
-</div>
-
-<div class="active-filters">
-	<?php if (is_array($search_options->country)):?>
-		<?php foreach($search_options->country as $country):?>
-        	<?php if (array_key_exists($country,$countries)):?>
-            <span class="remove-filter country" data-type="country" data-value="<?php echo $country;?>"><?php echo $countries[$country]['nation'];?></span>
-            <?php endif;?>
-        <?php endforeach;?>
-    <?php endif;?>
-    <?php if (isset($search_options->dtype) && is_array($search_options->dtype)):?>
-		<?php foreach($search_options->dtype as $dtype):?>
-            <span class="remove-filter dtype" data-type="dtype" data-value="<?php echo $dtype;?>"><?php echo $data_access_types[$dtype];?></span>
-        <?php endforeach;?>
-    <?php endif;?>
-    <?php if ($search_options->from!='' && $search_options->to!=''):?>
-		<?php if ( $search_options->from!=$min_year || $search_options->to!=$max_year ):?>
-            <span class="remove-filter years" data-type="years" data-value="0">between <?php echo $search_options->from;?>-<?php echo $search_options->to;?></span>
-        <?php endif;?>
-    <?php endif;?>
-    
-    <?php if (isset($search_options->sk) && $search_options->sk!=''):?>
-    <span class="remove-filter sk" data-type="sk" data-value=""><?php echo $search_options->sk;?></span>
-    <?php endif;?>
-    
-	<?php if (isset($search_options->vk) && $search_options->vk!=''):?>
-    <span class="remove-filter vk" data-type="vk" data-value=""><?php echo $search_options->vk;?></span>
-    <?php endif;?>
-</div>
-<div class="filter-action-bar">
-	<a href="#save-search" class="save-search">Save this search</a>
-    <a href="#share-search" class="share-search">Share this search</a>
-    <a href="#print-search" class="print-search">Print list</a>
-</div>
-</div>
+<?php $this->load->view("catalog_search/active_filter_tokens");?>
 
 <?php if (isset($rows)): ?>
 <?php if ($rows): ?>
 
-<?php		
+<?php
 	//sort
 	$sort_by=$search_options->sort_by;
 	$sort_order=$search_options->sort_order;
