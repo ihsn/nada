311b517fd21c04c829738a27de022282c78781fd
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-03-30 16:23:01 -0400

    minor

--- application/views/catalog_search/filter_countries.php
+++ application/views/catalog_search/filter_countries.php
@@ -4,14 +4,11 @@
 <div class="filter-box filter-by-country">
 <h3><?php echo t('filter_by_country');?></h3> 
 
-<a class="clear-filter" href="#"><?php echo t('reset');?></a>
-
 <div id="countries-container" >
     <div class="country any">    	
         <input type="checkbox" class="chk-country-any chk-any" id="country-any"  <?php echo $search_options->country!="" ? '' : 'checked="checked"';?> />
         <label for="country-any">Any</label>
     </div>
-    <div class="select-specific">Or select specific:</div>
 	<div class="country-items items-container <?php echo (count($countries)>10) ? 'scrollable' : ''; ?>">
 	<?php $k=0;foreach($countries as $country):$k++; ?>
         <div class="country item <?php echo ($k>$item_limit) ? 'more' : 'less'; ?>">
@@ -34,7 +31,7 @@
     <?php endif;?>
     
     <div class="filter-footer">
-    <input type="button" class="btn-select" value="View / Select More" id="btn-country-selection" data-dialog-id="dialog-countries" data-dialog-title="Select Countries" data-url="index.php/catalog/country_selection"/>
+    <input type="button" class="btn-select" value="View / Select More" id="btn-country-selection" data-dialog-id="dialog-countries" data-dialog-title="Select Countries" data-url="index.php/catalog/country_selection/<?php echo $active_repo;?>"/>
     </div>
     
 </div>
