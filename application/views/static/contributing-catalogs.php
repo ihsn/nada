ab1cce7b905982657996c321992d2c4fc93e1439
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-04-09 20:48:32 -0400

    Updated to use repository sections and etc.

--- application/views/static/contributing-catalogs.php
+++ application/views/static/contributing-catalogs.php
@@ -1,12 +1,24 @@
 <div style="background:white;overflow:auto;">
-<h1>Contributing Catalogs</h1>
-<p>Our <a href="<?php echo site_url();?>/catalog/central">Central Microdata Catalog</a> operates as a portal for datasets originating from the World Bank and other international, regional and national organizations. These datasets and  the related metadata are provided by various contributing catalogs.</p>
-<div class="wb-box-main with-bottom-spacing" style="width:100%;">
-      <div class="wb-box">
-	<?php 
-        $data['rows']=$this->repository_model->get_repositories($published=TRUE, $system=FALSE);//list of repos                    
-        $this->load->view("repositories/contributing_catalogs",$data);                    
-    ?>
-	</div>
-</div>
+
+<h1><?php echo t('contributing_catalogs');?></h1>
+<p><?php echo sprintf(t('msg_about_contributing_catalogs'),anchor('catalog/central',t('central_data_catalog'))); ?></p>
+
+<?php
+$this->load->model("repository_model");
+$this->load->model("repository_sections_model");
+
+$collections=$this->repository_model->get_repositories($published=TRUE, $system=FALSE);
+$sections=array();
+
+foreach($collections as $key=>$collection)
+{
+	$sections[$collection['section']]=$collection['section_title'];
+}
+
+$data['sections']=$sections;		
+$data['rows']=$collections;
+$data['show_unpublished']=TRUE;
+$content=$this->load->view("repositories/index_public",$data);
+?>
+
 </div>
