d932cb77c366dabed1ec2f3345eda335e4219ddb
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-04-19 09:52:20 -0400

    Updated collection about page formatting

--- application/views/catalog_search/about_collection.php
+++ application/views/catalog_search/about_collection.php
@@ -1,13 +1,21 @@
 <style>
 	.about-photo{float:left;margin-right:10px;}
-	.repository-about{}
-	.repository-about .photo-big{float:left;margin-right:10px;}
-	.visit-catalog{float:right;}
+	.repository-container{overflow:auto;}
+	.repository-container .photo-big{float:left;margin-right:10px;}
+	.visit-catalog{text-align:right;margin:10px;padding-top:10px;}
 	a.btn-style-1{color:white;}
+	ul.bull,.bull li{list-style-type:disc;margin-left:20px;}
+	ul.bull{margin-bottom:10px;}
 </style>
-<div class="repository-about">
-<?php echo $row->long_text;?>
+<div class="repository-container">
+	<div class="body"><?php echo $row->long_text;?></div>
+</div>
 <div class="visit-catalog">
 	<a class="btn-style-1" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
     </div>
-</div>
+
+<?php /*
+<?php if($additional):?>
+<?php echo $additional;?>
+<?php endif;?>
+*/ ?>
