a9c010b360988dabd52816dd4a2409ef195cb694
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-04-19 09:28:59 -0400

    added config file for collection configurations

---
+++ application/config/collections.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Settings for Collections
+|--------------------------------------------------------------------------
+|
+*/
+
+//thumbnails folder
+$config['collection_image_path']='files/images';//don't add a slash at the end
+
+//default thumbnail
+$config['collection_default_thumb']='files/icon-blank.png';
+
+//Collection about page template file
+$config['collection_about_template']='views/repositories/default_about_template';
+
+
+
+/* End of file collections.php */
+/* Location: ./system/application/config/collections.php */
