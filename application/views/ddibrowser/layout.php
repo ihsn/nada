bf597665be1849f4065d76fea5f32e38528454bd
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-04-19 09:57:10 -0400

    Moved all styles to global stylesheet and some minor formatting fixes

--- application/views/ddibrowser/layout.php
+++ application/views/ddibrowser/layout.php
@@ -1,89 +1,3 @@
-<style>
-.tab-heading {
-text-align: left;
-padding: 10px;
-padding-bottom: 40px;
-z-index: -1;
-background-color:#F3F3F3;
-background: transparent url(<?php echo js_base_url();?>/themes/wb/tab-bg.png) repeat-x scroll bottom;
-padding-left: 20px;
-border:1px solid gainsboro;
-font-size:small;
-}
-
-.ui-tabs .ui-tabs-panel{
-background:white;border:1px solid gainsboro;overflow:auto;border-top:0px;
-}
-
-.ui-tabs .ui-tabs-nav li.ui-state-active {
-position: relative;
-background:gray;
-background: url(<?php echo js_base_url();?>/themes/wb/tab-active-arrow.png) bottom center no-repeat;
-}
-
-/*active tab*/
-.ui-tabs .ui-tabs-nav li.ui-state-active a {
-	background: url(<?php echo js_base_url();?>/themes/wb/active-tab-bg.png);
-	color: white;
-	margin-bottom:0px;
-}
-
-/*set tab borders*/
-.ui-tabs .ui-tabs-nav li a, 
-.ui-tabs-collapsible .ui-tabs-nav li.ui-tabs-active a{
-	border: 1px solid gainsboro;
-	border-bottom: 0px;
-	background:white;
-	xfont-size:12px;
-	padding:5px 10px;
-	line-height:140%;
-}
-
-.ui-tabs .ui-tabs-nav li {
-	border: 0px;
-}
-
-.ui-tabs .ui-tabs-nav li.ui-tabs-active{margin-bottom:-4px;padding-bottom:4px;}
-
-.tab-sidebar{float:left;width:160px;font-size:smaller;border-right:1px solid gainsboro;margin-right:10px;padding-right:10px;}
-
-.page-body h1{margin-bottom:15px;}
-
-.ui-tabs li a .get-microdata{display:block;background:url(<?php echo js_base_url();?>/themes/wb/data-access-small.gif) no-repeat 0% 2%;width:20px;height:16px;float:left;margin-right:5px;}
-.tab-body{font-size:small;margin-left:170px;border-left:1px solid gainsboro;padding-left:20px;}
-.tab-sidebar .study-items{text-align:right;}
-.tab-sidebar .filetree li a{font-size:11px;}
-
-.tab-sidebar li.item{padding:0px;line-height:150%;background:none;margin-bottom:10px;}
-.tab-sidebar li.item a,
-.tab-sidebar .filetree li a{
-	color:maroon;
-}
-
-.tab-sidebar li.item a:hover,
-.tab-sidebar .filetree li a:hover
-{
-	color:black;
-}
-
-.tab-sidebar li.sub-item a{color:maroon;font-size:small;}
-.tab-sidebar li.sub-item a:hover{color:black;}
-
-
-.filetree{padding:0px;}
-.filetree li{overflow:hidden;}
-
-.dictionary-search{margin-bottom:15px;border:1px solid gainsboro;padding:5px;overflow:auto;}
-.dictionary-search .search-keywords{width:105px;border:1px solid gainsboro;margin-right:2px;padding:2px;font-size:11px;margin:0px;float:left;display:block;height:15px;}
-.dictionary-search .btn-search{border:0px solid gainsboro;background:gainsboro;float:left;display:block;height:21px;outline:0;margin-left:2px;}
-.selected{font-weight:bold;}
-
-.var-info-panel{border:2px solid gray;}
-
-/*citations row number*/
-.grid-table .row-num{color:gray;font-size:smaller;}
-
-</style>
 <script type="text/javascript"> 
    var CI = {
 				'base_url': '<?php echo site_url(); ?>',
@@ -94,17 +8,22 @@ background: url(<?php echo js_base_url();?>/themes/wb/tab-active-arrow.png) bott
 
 <script type="text/javascript">
 $(function(){
+
+	//data-dictionary row-click
+	$(".data-file-row").click(function(){
+		window.location=$(this).attr("data-url");
+		return false;
+	});
+
 	//tree-view 
 	$(".filetree").treeview({collapsed: false});
-	//$( "#tabs" ).tabs();
-	
 	$(".tab-sidebar li.item a,.tab-sidebar li.sub-item a").click(function(){
 		$('.study-tabs .tab-body').html('<img src="images/loading.gif"/> loading...');
 		var hash={
 					tab:$("#tabs .ui-tabs-active a").attr("data-id"),
 					page:$(this).attr("data-id")
 				};
-		console.log(hash);		
+
 		$.bbq.pushState(hash);
 		
 		$.get($(this).attr("href"),{ ajax: "1"}, function(data) {
@@ -123,12 +42,10 @@ $(function(){
 	//hashchange event handler
 	$(window).bind( 'hashchange', function(e) {		
 		fragments=$.deparam.fragment();
-		console.log(fragments);
 		
 		if(typeof fragments.tab != 'undefined'){
 			//$("#from option[value='"+fragments.from+"']").prop('selected', 'selected');
-			$("#tabs .ui-tabs-nav a").each(function(){ 
-				console.log( $(this).attr("data-id") );
+			$("#tabs .ui-tabs-nav a").each(function(){ 				
 				if (fragments.tab==$(this).attr("data-id")){
 					$("#tabs .ui-tabs-active").removeClass("ui-tabs-active ui-state-active");
 					$(this).closest("li").addClass("ui-tabs-active ui-state-active");
@@ -195,7 +112,7 @@ $(function(){
 	}
 </script>
 
-<div class="page-body-full" >
+<div class="page-body-full study-metadata-page" >
 
 <?php if (isset($survey_title)):?>
 	<h1><?php echo $survey_title;?></h1>
@@ -226,7 +143,7 @@ $tab_urls['get_microdata']=array(
 ?>
     
 <!-- tabs -->
-<div id="tabs" style="" class="ui-tabs ui-widget ui-widget-content ui-corner-all study-tabs" >
+<div id="tabs" style="" class="study-metadata ui-tabs ui-widget ui-widget-content ui-corner-all study-tabs" >
 
 	<div class="tab-heading"><?php echo $survey_info;?><a name="tab"></a></div>
 
@@ -236,7 +153,7 @@ $tab_urls['get_microdata']=array(
 	<li class="ui-state-default ui-corner-top <?php echo (in_array($tab,$tab_urls['data_dictionary'])) ? $active_tab_class : '';?>" role="tab" tabindex="-1" aria-controls="tabs-2" aria-labelledby="ui-id-2" aria-selected="false"><a href="<?php echo site_url('catalog/'.$survey_id.'/data_dictionary');?>#tab" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-2" data-id="data-dictionary">Data Dictionary</a></li>
     <?php endif;?>
     <?php if($page_tabs['get_microdata']>0):?>
-	<li class="ui-state-default ui-corner-top <?php echo ($tab=='get_microdata') ? $active_tab_class : '';?>" role="tab" tabindex="-1" aria-controls="tabs-23" aria-labelledby="ui-id-23" aria-selected="false"><a href="<?php echo site_url('catalog/'.$survey_id.'/get_microdata');?>#tab" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-23" data-id="get-microdata"><span class="get-microdata"></span>Get Microdata</a></li>
+	<li class="ui-state-default ui-corner-top <?php echo ($tab=='get_microdata') ? $active_tab_class : '';?>" role="tab" tabindex="-1" aria-controls="tabs-23" aria-labelledby="ui-id-23" aria-selected="false"><a href="<?php echo site_url('catalog/'.$survey_id.'/get_microdata');?>#tab" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-23" data-id="get-microdata"><span class="get-microdata da-icon-small da-<?php echo $data_access_type;?>"></span>Get Microdata</a></li>
     <?php endif;?>
     <?php if($page_tabs['related_materials']>0):?>
     <li class="ui-state-default ui-corner-top <?php echo ($tab=='related_materials') ? $active_tab_class : '';?>" role="tab" tabindex="-1" aria-controls="tabs-3" aria-labelledby="ui-id-4" aria-selected="false"><a href="<?php echo site_url('catalog/'.$survey_id.'/related_materials');?>#tab" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-3" data-id="related-materials">Related Materials</a></li>
