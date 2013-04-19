a83f8fff5c123c24c411f48324c0ba2f2ebe2bd7
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-04-19 09:56:37 -0400

    removed print, open in new window icons for study information pages

--- application/views/ddibrowser/ajax_view.php
+++ application/views/ddibrowser/ajax_view.php
@@ -8,12 +8,13 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
 <?php endif; ?>
 
 <div style="overflow:hidden;">
-
+<?php /* 
 <div style="float:right">    
     <a target="blank_" href="<?php echo $section_url.'?print=yes';?>"><img alt="<?php echo t('print');?>" src="images/print.gif" border="0"/></a>
     <a target="blank_" href="<?php echo $section_url.'?pdf=yes';?>"><img alt="<?php echo t('pdf');?>" src="images/pdf.gif" border="0"/></a>
     <a target="blank_" href="<?php echo $section_url;?>" title="<?php echo t('new_window');?>"><img alt="<?php echo t('new_window');?>" src="images/new_window.png" border="0"/></a>
 </div>
+*/ ?>
 <?php if ($this->input->get("title")):?>
     <div style="float:left">
         <?php if (isset($this->survey)):?>
