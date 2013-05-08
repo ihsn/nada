d016b410bb16780455c88afaeab0c32155ee7e5b
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-08 16:54:31 -0400

    added helper for site menu

---
+++ application/helpers/site_menu_helper.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Back-end site menu helper functions
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author		Mehmood Asghar
+ * @link		
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Load menu items per user access
+ *
+ *
+ * @access	public
+ * @return	array
+ */	
+if ( ! function_exists('get_site_menu'))
+{
+  
+function get_site_menu()
+{
+  	$ci =& get_instance();
+	//$ci->load->model("Catalog_model");
+	
+	$ci->load->library('site_menu');
+
+	if ($ci->acl->user_has_unlimited_access())
+	{
+		return $ci->site_menu->get_formatted_menu_tree();
+	}
+	
+	//parent and children menu items [level1 and level2 only]
+	$menu_array=$ci->site_menu->get_menu_items_array();
+
+	$items=array();
+
+	//remove menu items that user has no access
+	foreach($menu_array['parents'] as $item)
+	{
+		if ($ci->acl->user_has_url_access($user_id=NULL,$url=$item['url']))
+		{
+			$items['parents'][]=$item;
+		}
+	}
+	
+	foreach($menu_array['children'] as $item)
+	{
+		if ($ci->acl->user_has_url_access($user_id=NULL,$url=$item['url']))
+		{
+			$items['children'][]=$item;
+		}
+	}
+	
+	return $ci->site_menu->get_formatted_menu_tree($items);
+}
+  
+}
+
+/* End of file site_menu_helper.php */
+/* Location: ./system/helpers/site_menu_helper.php */
