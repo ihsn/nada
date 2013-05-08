2a88c83ae4d8ce661abcc223e53ea9e95a82ccc1
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-08 17:01:32 -0400

    minor

--- application/views/catalog/index_sidebar.php
+++ application/views/catalog/index_sidebar.php
@@ -5,10 +5,10 @@
 */?>
 <form id="form_filter">
 	<div style="font-size:smaller;text-align:right;">
-    <a href="<?php echo site_url();?>/admin/catalog/?reset=reset">Clear filter</a>
+    <a href="<?php echo site_url();?>/admin/catalog/?reset=reset"><?php echo t('clear_filter');?></a>
 	</div>
     <div class="box">
-		<div class="box-header">Filter
+		<div class="box-header"><?php echo t('filter');?>
 		   <span class="sh" title="toggle_box">&nbsp;</span>
 		</div>
 		<div class="box-body">
@@ -19,35 +19,42 @@
 		?>
 		<?php foreach($search_fields as $field): $c++?>
 			<div class="field">
-			<label for="field-<?php echo $field;?>"><?php echo $field;?></label><br/>
+			<label for="field-<?php echo $field;?>"><?php echo t($field);?></label><br/>
 			<input class="mini" type="textbox" id="field-<?php echo $field;?>" name="<?php echo $field;?>" value="<?php echo get_form_value($field,'') ; ?>"/>
-			<!--<input type="button" value="Apply" class="btn-tiny"/>-->
 			<span class="icon icon-search apply-filter"></span>
 			</div>
 		<?php endforeach;?>
         
         	<div class="field">
-                <label for="field-published">Survey status</label><br/>
+                <label for="field-published"><?php echo t('study_status');?></label><br/>
                 <select name="published" id="survey-status">
-	                <option value="">All</option>
-                	<option value="1">Published</option>
-                    <option value="0">Draft</option>                    
+	                <option value=""><?php echo t('all');?></option>
+                	<option value="1"><?php echo t('published');?></option>
+                    <option value="0"><?php echo t('unpublished');?></option>                    
                 </select>
-                <!--<input type="checkbox" id="field-published" name="published" value="1"/>-->
 			</div>
-            <!--
+
         	<div class="field">
-                <label for="field-unpublished">Unpublished only</label><br/>
-                <input type="checkbox" id="field-unpublished" name="published" value="0"/>
+                <label for="field-no-questionnaire">
+				<input type="checkbox" id="field-no-questionnaire" name="no_question" value="1"/>
+				<?php echo t('study_no_questionnaire');?>
+                </label>                
 			</div>
-			-->
+        	<div class="field">
+                <label for="field-no-datafile">
+				<input type="checkbox" id="field-no-datafile" name="no_datafile" value="1"/>
+				<?php echo t('study_no_datafile');?>
+                </label>                
+			</div>
+
+
 		</div>
 		</div>
    </div>
 
 
 	<div class="box">
-		<div class="box-header">Countries
+		<div class="box-header"><?php echo t('countries');?>
 		   <span class="sh" title="toggle_box">&nbsp;</span>
 		</div>
 		<div class="box-body">
@@ -67,7 +74,7 @@
 	
 	
 	<div class="box survey-countries">
-		<div class="box-header">Tags
+		<div class="box-header"><?php echo t('tags');?>
 		   <span class="sh" title="toggle_box">&nbsp;</span>
 		</div>
 		<div class="survey-tags-body box-body">
@@ -87,7 +94,7 @@
 
 	<!-- data access filter-->
     <div class="box da-filter">
-		<div class="box-header">Data Access
+		<div class="box-header"><?php echo t('data_access');?>
 		   <span class="sh" title="toggle_box">&nbsp;</span>
 		</div>
 		<div class="filter-da-body box-body">
