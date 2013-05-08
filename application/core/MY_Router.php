ad816433d78f99de28ac22fc54777beb4973c727
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-08 16:54:18 -0400

    Extended core router to search multi level deep folders

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -3,4 +3,51 @@
 /* load the MX_Router class */
 require APPPATH."third_party/MX/Router.php";
 
-class MY_Router extends MX_Router {}
+class MY_Router extends MX_Router {
+
+//copied from CI core Router class
+function _validate_request($segments)
+    {
+        if (file_exists(APPPATH.'controllers/'.$segments[0].EXT))
+        {
+            return $segments;
+        }
+ 
+        if (is_dir(APPPATH.'controllers/'.$segments[0]))
+        {
+            $this->set_directory($segments[0]);
+            $segments = array_slice($segments, 1);
+ 
+            //support for multi level sub-folders
+            while(count($segments) > 0 && is_dir(APPPATH.'controllers/'.$this->directory.$segments[0]))
+            {
+            	$this->directory = $this->directory . $segments[0] . '/';
+				$segments = array_slice($segments, 1);
+            }
+			//end-change
+ 
+            if (count($segments) > 0)
+            {
+                if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$segments[0].EXT))
+                {
+                    show_404($this->fetch_directory().$segments[0]);
+                }
+            }
+            else
+            {
+                $this->set_class($this->default_controller);
+                $this->set_method('index');
+ 
+                if ( ! file_exists(APPPATH.'controllers/'.$this->fetch_directory().$this->default_controller.EXT))
+                {
+                    $this->directory = '';
+                    return array();
+                }
+            }
+ 
+            return $segments;
+        }
+ 
+        show_404($segments[0]);
+    }
+}
