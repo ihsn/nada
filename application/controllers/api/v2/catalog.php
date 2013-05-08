24e4aeebf8a6724fbdbac77ee272133687fa8976
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-08 17:05:22 -0400

    initialized v2 of API

---
+++ application/controllers/api/v2/catalog.php
@@ -0,0 +1,230 @@
+<?php
+
+require(APPPATH.'/libraries/REST_Controller.php');
+
+class Catalog extends REST_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Catalog_model'); 	
+		$this->load->library('DDI_Browser','','DDI_Browser');
+	}
+	
+	function search_get()
+	{
+		$params=array(
+				'study_keywords'	=>	$this->security->xss_clean($this->input->get("sk")),
+				'variable_keywords'	=>	$this->security->xss_clean($this->input->get("vk")),
+				'variable_fields'	=>	array('name','labl'),
+				'countries'			=>	$this->security->xss_clean($this->input->get("country")),
+				'from'				=>	$this->security->xss_clean($this->input->get("from")),
+				'to'				=>	$this->security->xss_clean($this->input->get("to")),
+				'collections'		=>	$this->security->xss_clean($this->input->get("collection")),
+				'dtype'				=>	$this->security->xss_clean($this->input->get("dtype")),
+				'repo'				=>	$this->security->xss_clean($this->input->get("repo")),
+		);
+		
+		$limit=5;
+		$page=$this->input->get('page');
+		$page= ($page >0) ? $page : 1;
+		$offset=($page-1)*$limit;
+
+		$this->load->library('catalog_search',$params);
+		$content=$this->catalog_search->search($limit,$offset);
+		$this->response($content, 200); 
+	}
+
+	
+	
+	/**
+	*
+	* Returns the most recent studies
+	*
+	* @country	string	filter by single country name
+	* @order	bit		order by date created 0=desc;1=asc
+	**/
+	function latest_get()
+	{
+		$country=$this->get("country");
+		$limit=(int)$this->get("limit");
+		
+		if (!$limit>0 && !$limit<=20 )
+		{
+			$limit=5;
+		}
+		
+		if ($country)
+		{
+			$this->db->where("nation",$country);
+		}
+		
+		$this->db->select("id,refno,titl,nation,authenty,created");
+		$this->db->where("published",1);
+		$this->db->limit($limit);
+		$this->db->order_by("created","desc");
+		
+		$query=$this->db->get("surveys");
+		$content=NULL;
+		
+		if ($query)
+		{
+			$content=$query->result_array();
+		}
+				
+		if (!$content)
+		{
+    		$content=array('error'=>'NO_RECORDS_FOUND');    	
+		}
+		else
+		{
+			foreach($content as $key=>$value)
+			{
+				$content[$key]['url']=site_url().'/catalog/'.$value['id'];
+				$content[$key]['created']=date("M-d-Y",$value["created"]);
+			}		
+		}
+		$this->response($content, 200); 
+	}
+
+	/**
+	*
+	* Returns all country names from db
+	*
+	**/
+	function countries_get()
+	{
+		$this->db->select("nation,count(*) as found");
+		$this->db->group_by("nation");
+		$query=$this->db->get("surveys");
+		$content=NULL;
+		
+		if ($query)
+		{
+			$content=$query->result_array();
+		}
+				
+		if (!$content)
+		{
+    		$content=array('error'=>'NO_RECORDS_FOUND');    	
+		}
+		$this->response($content, 200); 
+	}
+	
+	
+	
+	
+	/**
+	*
+	*Returns a list of published collections
+	**/
+	function collections_get()
+	{
+		
+	}
+	
+	
+	function _remap($method)
+	{
+		$method=strtolower($method);
+        
+		if (in_array(($method.'_get'), array_map('strtolower', get_class_methods($this))) ) 
+		{
+            $uri = $this->uri->segment_array();
+            unset($uri[1]);
+            unset($uri[2]);
+            call_user_func_array(array($this, $method.'_get'), $uri);return;
+        }
+		
+		
+		$id=$this->get('id');
+		
+		if(is_numeric($id))
+        {
+        	$this->_get_study_part($method);
+        }
+		
+		
+	}
+	
+	
+	private function _get_study_part($method)
+	{
+		$id=$this->get('id');
+		
+		if(!is_numeric($id))
+        {
+        	$this->response(NULL, 400);
+        }
+
+		$ddi_file=$this->Catalog_model->get_survey_ddi_path($id);
+		
+		if ($ddi_file===FALSE)
+		{
+			$this->response(array('error'=>t('file_not_found')),404);exit;
+		}
+	
+		$html='';
+		
+		switch($method)
+		{
+			case 'questionnaires':
+				$html=$this->DDI_Browser->get_questionnaires_html($ddi_file);
+			break;
+			
+			case 'sampling':
+				$html=$this->DDI_Browser->get_sampling_html($ddi_file);
+			break;
+			
+			case 'overview':
+				$html=$this->DDI_Browser->get_overview_html($ddi_file);
+			break;
+			
+			case 'accesspolicy':
+				$html=$this->DDI_Browser->get_accesspolicy_html($ddi_file);
+			break;
+			
+			case 'dataprocessing':
+				$html=$this->DDI_Browser->get_dataprocessing_html($ddi_file);
+			break;
+			
+			case 'datacollection':
+				$html=$this->DDI_Browser->get_datacollection_html($ddi_file);
+			break;
+			
+			case 'dataappraisal':
+				$html=$this->DDI_Browser->get_dataappraisal_html($ddi_file);
+			break;
+			
+			case 'technicaldocuments':
+				$html=$this->DDI_Browser->get_technicaldocuments_html($ddi_file);
+			break;
+			
+			/*
+			case 'datafiles':
+				$html=$this->DDI_Browser->get_datafiles_html($ddi_file);
+			break;
+			
+			case 'datafile':
+				$html=$this->DDI_Browser->get_datafile_html($ddi_file);
+			break;
+			
+			case 'variables_by_group':
+				$html=$this->DDI_Browser->get_variables_by_group_html($ddi_file);
+			break;
+			
+			case 'variable':
+				$html=$this->DDI_Browser->get_variable_html($ddi_file);
+			break;		
+			*/
+			
+			case 'ddi':
+				$this->DDI_Browser->download_ddi($ddi_file);exit;
+			break;
+		}
+		
+		$this->response($html, 200); 
+	}
+	
+	
+}
