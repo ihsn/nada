a28a5a8d4be1e538e0232839d7c66b8d4724f8d6
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-15 12:53:24 -0400

    Updated collections' about page to show list of collections on central catalog

--- application/views/catalog_search/about_collection.php
+++ application/views/catalog_search/about_collection.php
@@ -6,16 +6,27 @@
 	a.btn-style-1{color:white;}
 	ul.bull,.bull li{list-style-type:disc;margin-left:20px;}
 	ul.bull{margin-bottom:10px;}
+	.about-collection .repo-thumbnail{width:82px;height:82px;}
+	.about-central{border-bottom: 1px solid gainsboro;}
 </style>
+<div class="about-collection">
+<?php if($row->repositoryid!=='central'):?>
 <div class="repository-container">
 	<div class="body"><?php echo $row->long_text;?></div>
 </div>
 <div class="visit-catalog">
 	<a class="btn-style-1" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
+</div>
+<?php elseif($row->repositoryid=='central'):?>
+<div class="about-central">
+	<p><?php echo t('about_central_catalog');?></p>    
+    <div class="visit-catalog">
+        <a class="btn-style-1" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
     </div>
-
-<?php /*
-<?php if($additional):?>
-<?php echo $additional;?>
+</div>
 <?php endif;?>
-*/ ?>
+
+<?php if($additional):?>
+	<?php echo $additional;?>
+<?php endif;?>
+</div>
