a001aa50d3d3aafa4a2cc06f211d2d42732806b4
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-15 12:50:06 -0400

    minor

--- application/views/access_licensed/edit_request_files.php
+++ application/views/access_licensed/edit_request_files.php
@@ -21,16 +21,18 @@
         <td></td>
         <td colspan="3"><h3><?php echo $survey_list[$key];?></h3></td>
         </tr>
-    <?php foreach($survey_data as $row): ?>
-    	<?php $row=(object)$row;//echo '<pre>';var_dump($row); ?>
-		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
-    	<tr class="<?php echo $tr_class; ?>">
-	        <td><input class="chk" type="checkbox" value="<?php echo $row->resource_id;?>" name="fileid-<?php echo $row->resource_id;?>" <?php  echo isset($row->download['download_limit']) ? 'checked="checked"' : ''; ?>/></td>
-            <td><?php echo basename($row->filename); ?></td>
-            <td><input type="text" class="download-limit" name="download-limit-<?php echo $row->resource_id;?>" maxlength="2" size="2" value="<?php echo isset($row->download['download_limit']) ? $row->download['download_limit'] : 3; ?>"/></td>
-			<td><input maxlength="10" class="expiry" name="expiry-<?php echo $row->resource_id;?>" type="text" size="10" value="<?php echo isset($row->download['expiry']) ? date("m/d/Y",$row->download['expiry']) : date("m/d/Y",date("U")+(60*60*24*5)); ?>"/></td>
-        </tr>
-    <?php endforeach;?>
+		<?php if($survey_data):?>
+		<?php foreach($survey_data as $row): ?>
+            <?php $row=(object)$row;//echo '<pre>';var_dump($row); ?>
+            <?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+            <tr class="<?php echo $tr_class; ?>">
+                <td><input class="chk" type="checkbox" value="<?php echo $row->resource_id;?>" name="fileid-<?php echo $row->resource_id;?>" <?php  echo isset($row->download['download_limit']) ? 'checked="checked"' : ''; ?>/></td>
+                <td><?php echo basename($row->filename); ?></td>
+                <td><input type="text" class="download-limit" name="download-limit-<?php echo $row->resource_id;?>" maxlength="2" size="2" value="<?php echo isset($row->download['download_limit']) ? $row->download['download_limit'] : 3; ?>"/></td>
+                <td><input maxlength="10" class="expiry" name="expiry-<?php echo $row->resource_id;?>" type="text" size="10" value="<?php echo isset($row->download['expiry']) ? date("m/d/Y",$row->download['expiry']) : date("m/d/Y",date("U")+(60*60*24*5)); ?>"/></td>
+            </tr>
+        <?php endforeach;?>
+        <?php endif;?>
     <?php endforeach;?>
     	<tr class="<?php echo $tr_class; ?>" style="background-color:#FFFFCC">
 	        <td>&nbsp;</td>
