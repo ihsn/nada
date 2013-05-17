c1677ae50a0dc95c58bd26e464dd05ab12af87a3
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-17 09:50:21 -0400

    Added configuration settings for google recaptcha.

--- application/config/captcha.php
+++ application/config/captcha.php
@@ -1,5 +1,24 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+
+/*
+|--------------------------------------------------------------------------
+| Which captcha driver to use?
+|--------------------------------------------------------------------------
+|
+| options are: image_captcha, recaptcha
+*/
+
+$config['captcha_driver']='recaptcha';
+
+/*
+|--------------------------------------------------------------------------
+| Image Captcha options
+|--------------------------------------------------------------------------
+|
+| Note: requires the GD extension
+*/
+
 //folder where images will be created. It needs to be an absolute path.
 $config['captcha_img_path'] = APPPATH.'/../cache/captcha';
 
@@ -17,3 +36,22 @@ $config['captcha_img_height'] = 100;
 
 //captcha expiration time (seconds)
 $config['captcha_expiration'] = 7200;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Google's Recaptcha library options
+|--------------------------------------------------------------------------
+|
+*/
+$config['recaptcha']['apiserver']="http://www.google.com/recaptcha/api";
+$config['recaptcha']['apisecureserver']="https://www.google.com/recaptcha/api";
+$config['recaptcha']['verifyserver']="www.google.com";
+$config['recaptcha']['publickey']="";
+$config['recaptcha']['privatekey']="";
+$config['recaptcha']['language']="en";
+$config['recaptcha']['theme']="clean";
+//$config['recaptcha']['mailhide']['publickey']="yourpublicemailhidekey";
+//$config['recaptcha']['mailhide']['privatekey']="yourprivateemailhidekey";
+
