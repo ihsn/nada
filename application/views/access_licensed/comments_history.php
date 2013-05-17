21f33359ca6edfcde8cf87c1a97e621e0a64d837
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-17 10:37:19 -0400

    updated formatting of request comments history

--- application/views/access_licensed/comments_history.php
+++ application/views/access_licensed/comments_history.php
@@ -10,9 +10,10 @@
 <?php endif; ?>
 
 <!-- grid -->
-<table class="grid-table" width="100%" cellspacing="0" cellpadding="0">
+<table class="grid-table comments-history-table" width="100%" cellspacing="0" cellpadding="0">
     <tr class="header">
 	    <th><?php echo t('dated');?></th>
+	    <th><?php echo t('status');?></th>        
 	    <th><?php echo t('comment_by');?></th>        			
         <th><?php echo t('comment');?></th>                    
     </tr>
@@ -20,10 +21,11 @@
 <?php foreach($comments_history as $row): ?>
     <?php $row=(object)$row; ?>
     <?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
-    <tr class="<?php echo $tr_class; ?>">
-        <td><?php echo date("m/d/Y",$row->created); ?></td>
-        <td><?php echo $row->user_id;?></td>        
-        <td><?php echo $row->description;?></td>
+    <tr class="<?php echo $tr_class; ?>" style="vertical-align:top">
+	    <td><?php echo date("m/d/Y",$row->created); ?></td>
+        <td><?php echo $row->request_status;?></td>        
+        <td nowrap="nowrap"><?php echo $row->first_name;?> <?php echo $row->last_name;?></td>
+        <td><div class="admin-comment"><?php echo nl2br(form_prep($row->description));?></div></td>
     </tr>
 <?php endforeach;?>
 </table>
