e6032576174cc65bc298a3ef18452e96a3a38efc
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-23 21:27:31 -0400

    Updated collection header bar formatting and added link to view licensed requests per collection

--- application/views/repositories/repo_sticky_bar.php
+++ application/views/repositories/repo_sticky_bar.php
@@ -6,7 +6,7 @@
 .collection-content-container .collection-body{float:left;}
 .collection-content-container .collection-title{font-size: 24px;text-shadow: 0 1px 3px rgba(0, 0, 0, .4), 0 0 30px rgba(0, 0, 0, .075);color: #47475C;}
 .collection-content-container .collection-id{font-weight:bold;color:#A5A1A1}
-.collection-content-container .page-links{position:absolute;right:50px;top:30px;}
+.collection-content-container .page-links{position:absolute;right:50px;top:43px;}
 .collection-content-container .page-links .button{
 background: #3A87AD;
 color: white;
@@ -15,6 +15,7 @@ margin-right: 7px;
 -moz-border-radius: 2px;
 -webkit-border-radius: 2px;
 box-shadow: 1px 1px 1px #888;
+font-size:12px;
 }
 .collection-content-container .page-links .button:hover{background:black;color:white;border:1px solid black;}
 </style>
@@ -30,13 +31,12 @@ box-shadow: 1px 1px 1px #888;
     </div>
     
 <div class="page-links">
-	<?php if ($this->uri->segment(2)=='catalog' && $this->uri->segment(3)!=''):?>
 	<a href="<?php echo site_url(); ?>/admin/catalog" class="button" title="<?php echo t('catalog_home');?>"><?php echo t('catalog_home');?></a>
-    <?php endif;?>
 	<a href="<?php echo site_url(); ?>/admin/catalog/upload" class="button" title="<?php echo t('upload_ddi_hover');?>"><?php echo t('upload_ddi');?></a> 
     <a href="<?php echo site_url(); ?>/admin/catalog/batch_import" class="button" title="<?php echo t('import_ddi_hover');?>"><?php echo t('import_ddi');?></a>
     <?php if(strtolower($repositoryid)!=='central'):?>
-	<a href="<?php echo site_url(); ?>/admin/catalog/copy_study" class="button" title="<?php echo t('copy_studies');?>"><?php echo t('copy_studies');?></a>
+		<a href="<?php echo site_url(); ?>/admin/catalog/copy_study" class="button" title="<?php echo t('copy_studies');?>"><?php echo t('copy_studies');?></a>
     <?php endif;?>
+    <a href="<?php echo site_url(); ?>/admin/licensed_requests" class="button" title="<?php echo t('licensed_requests');?>"><?php echo t('licensed_requests');?></a>
 </div>    
 </div>
