11e406150d7d4a749466b6b0ccdbe021fd05fba2
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-23 21:28:15 -0400

    Fixed user groups translations and other minor clean up

--- application/views/user_groups/edit.php
+++ application/views/user_groups/edit.php
@@ -1,3 +1,18 @@
+<?php
+$options_groups=array(
+	'user'=>t('group_type_user'),	
+	'reviewer'=>t('group_type_reviewer'),
+	'admin'=>t('group_type_admin')	
+);
+
+
+$options_access=array(
+	'none'=>t('access_type_none'),	
+	'limited'=>t('access_type_limited'),
+	'unlimited'=>t('access_type_unlimited')	
+);
+
+?>
 <div class="content-container">
 <?php 	$this->load->view('user_groups/navigation_links');  ?>
 
@@ -30,25 +45,13 @@
 
     <div class="field">
         <label for="group_type"><?php echo t('group_type');?></label>
-		<select name="group_type">
-       		<option selected="selected" value="<?php echo get_form_value('group_type',isset($group_type) ? $group_type : ''); ?>"><?php echo get_form_value('group_type',ucfirst(isset($group_type) ? $group_type : '')); ?></option>
-  			<option value="admin">Admin</option>
-            <option value="reviewer">Reviewer</option>
-  			<option value="user">User</option>
-
-		</select>
+        <?php echo form_dropdown('group_type', $options_groups, get_form_value("group_type",isset($group_type) ? $group_type : '')); ?>
     </div>
     
     <div class="field">
         <label for="access_type"><?php echo t('access_type');?></label>
-		<select name="access_type">
-       		<option value="<?php echo get_form_value('access_type',isset($access_type) ? $access_type : ''); ?>"><?php echo get_form_value('type',ucfirst(isset($access_type) ? $access_type : '')); ?></option>
-  			<option value="none">None</option>
-  			<option value="limited">Limited</option>
-  			<option value="unlimited">Unlimited</option>
-
-		</select>
-    </div>
+		<?php echo form_dropdown('access_type', $options_access, get_form_value("access_type",isset($access_type) ? $access_type : '')); ?>
+	</div>
 <?php
  //edit user
 	echo form_submit('submit',t('update'),'id="btnupdate"'); 
