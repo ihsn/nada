bb5b01001e7a6b2cc4961c7d5dd494ef5651711f
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-05-23 21:29:33 -0400

    added column to show user groups and a link to manage all user permissions

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -70,10 +70,9 @@
         	<th><input type="checkbox" value="-1"/></th>
             <th><?php echo create_sort_link($sort_by,$sort_order,'username',t('username'),$page_url); ?></th>
             <th><?php echo create_sort_link($sort_by,$sort_order,'email',t('email'),$page_url); ?></th>  
-            <!--<th><?php echo create_sort_link($sort_by,$sort_order,'group_name',t('group'),$page_url); ?></th>-->
+            <th><?php echo create_sort_link($sort_by,$sort_order,'group_name',t('group'),$page_url); ?></th>
 			<th><?php echo create_sort_link($sort_by,$sort_order,'active',t('status'),$page_url); ?></th>
             <th><?php echo create_sort_link($sort_by,$sort_order,'country',t('country'),$page_url); ?></th>
-            <th><?php echo create_sort_link($sort_by,$sort_order,'company',t('company'),$page_url); ?></th>
             <th><?php echo create_sort_link($sort_by,$sort_order,'created_on',t('join_date'),$page_url); ?></th>
             <th><?php echo create_sort_link($sort_by,$sort_order,'last_login',t('last_login'),$page_url); ?></th>			
 			<th><?php echo t('actions');?></th>
@@ -82,14 +81,23 @@
 	<?php foreach($rows as $row): ?>
     	<?php $row=(object)$row;?>
 		<?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
-    	<tr class="<?php echo $tr_class; ?>">
+    	<tr class="<?php echo $tr_class; ?>" valign="top">
         	<td><input type="checkbox" value="<?php echo $row->id; ?>"/></td>
-            <td><a href="<?php echo current_url().'/edit/'.$row->id; ?>"><?php echo form_prep($row->username); ?></a></td>
+            <td>
+            	<div><a href="<?php echo site_url('admin/users/edit/'.$row->id); ?>"><?php echo form_prep($row->username); ?></a></div>
+            </td>
             <td><?php echo form_prep($row->email); ?>&nbsp;</td>
-       		<!--<td><?php //echo strtoupper($row->group_name); ?></td>-->
+       		<td>
+            	<div>
+                	<?php if (array_key_exists($row->id,$user_groups) ):?>
+                    	<?php foreach($user_groups[$row->id] as $group):?>
+                        	<div><?php echo $group['name'];?></div>
+                        <?php endforeach;?>
+                    <?php endif;?>
+                </div>
+			</td>
 			<td><?php echo ((int)$row->active)==1 ? t('ACTIVE') : t('DISABLED'); ?></td>
             <td><?php echo form_prep($row->country); ?></td>
-            <td><?php echo form_prep(substr($row->company,0,60)); ?></td>
             <td><?php echo date("m-d-Y",$row->created_on); ?></td>
             <?php if ($row->last_login>$row->created_on): ?>
 				<td><?php echo date("m-d-Y",$row->last_login); ?></td>
@@ -98,7 +106,8 @@
             <?php endif; ?>
             <td>
             	<a href="<?php echo current_url();?>/edit/<?php echo $row->id;?>"><?php echo t('edit');?></a> |
-            	<a href="<?php echo current_url();?>/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a>
+            	<a href="<?php echo current_url();?>/delete/<?php echo $row->id;?>"><?php echo t('delete');?></a> | 
+                <a href="<?php echo site_url('admin/users/permissions/'.$row->id);?>"><?php echo t('permissions');?></a>
             </td>
         </tr>
     <?php endforeach;?>
