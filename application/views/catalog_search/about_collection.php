ef4edd3cc25afc1d494730282225e6c42297a831
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-03 01:21:03 -0400

    minor

--- application/views/catalog_search/about_collection.php
+++ application/views/catalog_search/about_collection.php
@@ -9,6 +9,7 @@
 	.about-collection .repo-thumbnail{width:82px;height:82px;}
 	.about-central{border-bottom: 1px solid gainsboro;}
 </style>
+
 <div class="about-collection">
 <?php if($row->repositoryid!=='central'):?>
 <div class="repository-container">
