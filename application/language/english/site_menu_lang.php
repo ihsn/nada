baa91da35f3c262120cd5eb65c77b8dc5bd8ee67
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-03 01:19:11 -0400

    added language file for site admin menu

---
+++ application/language/english/site_menu_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['Dashboard'] = "Dashboard";
+$lang['Studies'] = "Studies";
+$lang['Manage studies'] = "Manage studies";
+$lang['Licensed requests'] = "Licensed requests";
+$lang['Manage collections'] = "Manage collections";
+$lang['Citations'] = "Citations";
+$lang['All citations'] = "All citations";
+$lang['Import citations'] = "Import citations";
+$lang['Export citations'] = "Export citations";
+$lang['Users'] = "Users";
+$lang['All users'] = "All users";
+$lang['Add user'] = "Add user";
+$lang['Impersonate user'] = "Impersonate user";
+$lang['Menu'] = "Menu";
+$lang['All pages'] = "All pages";
+$lang['Reports'] = "Reports";
+$lang['All reports'] = "All reports";
+$lang['Settings'] = "Settings";
+$lang['Countries'] = "Countries";
+$lang['Regions'] = "Regions";
+$lang['Vocabularies'] = "Vocabularies";
+$lang['Central Data Catalog'] = "Central Data Catalog";
+
+
+
+
+/* End of file site_menu_lang.php */
+/* Location: ./system/language/english/site_menu_lang.php */
