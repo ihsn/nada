615c0b7887aa1294b79f4234c70990aedc2bc2a3
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-03 01:20:51 -0400

    minor

--- application/views/catalog/study_notes_edit.php
+++ application/views/catalog/study_notes_edit.php
@@ -3,7 +3,7 @@
 </style>
 <form method="post" class="form-edit-note" action="<?php echo $action_url; ?>" style="padding:10px;">
 
-<?php if ($show_note_tpes===TRUE):?>
+<?php if (isset($show_note_types) && $show_note_types===TRUE):?>
 <div class="field">
     <label class="inline"><?php echo t('select_note_type');?>
     <?php echo form_dropdown('type', array('admin'=>t('admin_note'),'reviewer'=>t('reviewer_note'),'public'=>t('public_note')), get_form_value("type",isset($type) ? $type : ''),'class="edit_note_type"'); ?>
