f983466e349c5d8dc061298a4185c4e8b8251494
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-03 01:21:45 -0400

    added js translations for data catalog page

---
+++ application/views/catalog_search/js_translations.php
@@ -0,0 +1,15 @@
+var i18n=
+{
+'searching':"<?php echo t('js_searching');?>",
+'loading':"<?php echo t('js_loading');?>",
+'invalid_year_range_selected':"<?php echo t('js_invalid_year_range_selected');?>",
+'topic_selected':"<?php echo t('js_topic_selected');?>",
+'topics_selected':"<?php echo t('js_topics_selected');?>",
+'collection_selected':"<?php echo t('js_collection_selected');?>",
+'collections_selected':"<?php echo t('js_collections_selected');?>",
+'country_selected':"<?php echo t('js_country_selected');?>",
+'countries_selected':"<?php echo t('js_countries_selected');?>",
+'collection_selected':"<?php echo t('js_collection_selected');?>",
+'collections_selected':"<?php echo t('js_collections_selected');?>",
+'cancel':"<?php echo t('cancel');?>"
+};
