7fde164450390ec4d6feb68f2277ddf9f67af779
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-03 02:00:17 -0400

    fixed button formatting

--- application/views/catalog_search/about_collection.php
+++ application/views/catalog_search/about_collection.php
@@ -3,7 +3,7 @@
 	.repository-container{overflow:auto;}
 	.repository-container .photo-big{float:left;margin-right:10px;}
 	.visit-catalog{text-align:right;margin:10px;padding-top:10px;}
-	a.btn-style-1{color:white;}
+	a.color-white{color:white;}
 	ul.bull,.bull li{list-style-type:disc;margin-left:20px;}
 	ul.bull{margin-bottom:10px;}
 	.about-collection .repo-thumbnail{width:82px;height:82px;}
@@ -16,13 +16,13 @@
 	<div class="body"><?php echo $row->long_text;?></div>
 </div>
 <div class="visit-catalog">
-	<a class="btn-style-1" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
+	<a class="btn btn-primary color-white" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
 </div>
 <?php elseif($row->repositoryid=='central'):?>
 <div class="about-central">
 	<p><?php echo t('about_central_catalog');?></p>    
     <div class="visit-catalog">
-        <a class="btn-style-1" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
+        <a class="btn btn-primary color-white" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
     </div>
 </div>
 <?php endif;?>
