5ca477dc1c155ad3d40c504ef8431cc4de31dd74
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-05 10:37:45 -0400

    added new translation file for spanish

---
+++ application/language/spanish/collection_lang.php
@@ -0,0 +1,52 @@
+<?php 
+$lang['create_repository']="Crear nueva colección";
+$lang['edit_repository']="Editar colección";
+$lang['repositories']="Colecciones";
+$lang['repositoryid']="Identificación de la Colección";
+$lang['organization']="Organización";
+$lang['country']="País";
+$lang['scan_interval_in_days']="Intervalo de exploración (en días)";
+$lang['published']="Publicado";
+$lang['status']="Estado";
+$lang['select_repo_type']="Seleccionar tipo de colección";
+$lang['thumbnail']="Vista en miniatura";
+$lang['short_description']="Descripción corta";
+$lang['long_description']="Descripción detallada (esta será la página &quot;acerca de&quot; en la colección)";
+$lang['internal']="Interna";
+$lang['external']="Externa";
+$lang['scan']="Exploración";
+$lang['manage_repository_users']="Administrar el acceso a la colección";
+$lang['manage_users']="Administrar usuarios";
+$lang['msg_select_active_repo']="Hacer clic en la colección que desea administrar para la sesión actual.";
+$lang['select_active_repository']="Seleccionar la Colección a Administrar";
+$lang['msg_no_repo_access']="Usted no tiene permisos para acceder a ningún catálogo.";
+$lang['repositories_management']="Administrar colecciones";
+$lang['repositories_regional']="Repositorios Regionales";
+$lang['repositories_specialized']="Repositorios Especializados";
+$lang['callback_error_repositoryid_exists']="Seleccionar un nombre único (sólo caracteres y números) como Identificación del Repositorio.";
+$lang['manage_repo_sections']="Grupos de Colecciones";
+$lang['repository_admins']="Administrar Usuarios";
+$lang['da_by_collection']="Acceso a los Datos por Colección";
+$lang['da_by_collection_description']="Esto permite a los usuarios descargar los datos de todas las operaciones estadísticas que pertenecen a esta colección.";
+$lang['repository_sections']="Grupos de colecciones";
+$lang['repository_sections_management']="Grupos de Colecciones";
+$lang['description']="Descripción";
+$lang['weight']="Peso";
+$lang['repository_sections_home']="Principal";
+$lang['repository_section_edit']="Editar Grupo de Colecciones";
+$lang['repository_section_add']="Agregar Grupo de Colecciones";
+$lang['save_permissions']="Guardar permisos";
+$lang['repository_permissions']="Administrar Permisos de Colecciones";
+$lang['collection_history']="Historia de la Colección";
+$lang['study_title']="Título de la operación estadística";
+$lang['total_studies_in_collection']="Total de operaciones estadísticas en la colección";
+$lang['year']="Año";
+$lang['changed']="Modificado";
+$lang['preview']="Vista previa";
+$lang['user']="Usuario";
+$lang['none']="Ninguno";
+$lang['confirm_collection_status_change']="¿Está seguro/a que desea continuar? Esto cambiará el estado de todas las operaciones estadísticas pertenecientes a esta colección.";
+
+
+/* End of file collection_lang.php */
+/* Location: ./application/language/spanish/collection_lang.php */
