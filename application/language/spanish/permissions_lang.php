08487b7237e49d42b2be70a6c1951baf95b8a457
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-05 10:38:00 -0400

    new file added for spanish

---
+++ application/language/spanish/permissions_lang.php
@@ -0,0 +1,22 @@
+<?php 
+$lang['permission']="Permiso";
+$lang['permissions']="Permisos";
+$lang['administrator']="Administrador";
+$lang['permissions_list']="Lista de permisos";
+$lang['edit_permission']="Editar permiso";
+$lang['add_permission']="Agregar permiso";
+$lang['urls']="URLs";
+$lang['catalog']="Catálogo";
+$lang['site_admin']="Admin";
+$lang['vocab']="Vocabulario";
+$lang['user_admin']="Usuario";
+$lang['ddibrowser']="Navegador del DDI";
+$lang['menu_admin']="Menú";
+$lang['general_site']="Sitio General";
+$lang['manage_repo_permissions']="Administrar acceso al repositorio";
+$lang['manage_group_permissions_for']="Administrar permisos del grupo para";
+$lang['apply_changes']="Aplicar cambios";
+
+
+/* End of file permissions_lang.php */
+/* Location: ./application/language/spanish/permissions_lang.php */
