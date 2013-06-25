a6840337bf43ff771b1013e1665422aa938d4bdf
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-06-24 20:42:36 -0400

    minor

--- application/views/auth/email/admin_notice_new_registration.php
+++ application/views/auth/email/admin_notice_new_registration.php
@@ -5,8 +5,6 @@
     	<?php echo t('first_name');?>: <?php echo $first_name; ?><br/>
         <?php echo t('last_name');?>: <?php echo $last_name; ?><br/>
         <?php echo t('email');?>: <?php echo $email; ?><br/>
-        <?php echo t('company');?>: <?php echo $company; ?><br/>
-        <?php echo t('phone');?>: <?php echo $phone; ?><br/>
         <?php echo t('country');?>: <?php echo $country; ?><br/>
       </p>  
 </body>
