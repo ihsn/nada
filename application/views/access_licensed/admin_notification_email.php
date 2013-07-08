b88b1d66e94731a318d35cfd2b83cea88d281209
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-07-08 17:29:10 -0400

    Updated admin notification email to include a link to the request page and show study title,country and years info

--- application/views/access_licensed/admin_notification_email.php
+++ application/views/access_licensed/admin_notification_email.php
@@ -4,9 +4,29 @@
 *
 */
 ?>
+
 <?php if($request_type='study'):?>
+
+<?php
+
+$study_years=array(
+	$surveys[0]['data_coll_start'],
+	$surveys[0]['data_coll_end']			
+);
+$study_years=array_unique($study_years);
+$study_years=implode(" - ",$study_years);
+
+$study_title=$surveys[0]['nation']. ' - '.$surveys[0]['titl']; //. ' - '.$study_years;
+
+?>
+
 <p>
-    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $surveys[0]['titl']);?></p>
+    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $study_title);?></p>
+    <div style="margin:10px;padding:10px;border:1px solid orange;">
+    	<h3 style="margin:0px;"><a href="<?php echo site_url('admin/licensed_requests/edit/'.$id);?>"><?php echo $surveys[0]['titl'];?></a></h3>
+        <div><?php echo $surveys[0]['nation'];?>, <?php echo $study_years;?></div>
+    </div>
+    
     <p><?php echo t('to_view_request_instructions');?><p>
     <p>-- </p>
     <p><?php echo t('Website');?>: <a href="<?php echo site_url();?>"><?php echo site_url();?></a></p>
