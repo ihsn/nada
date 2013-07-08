df2f27b051a7cd26a0183a56d766459f60376396
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-07-08 17:30:50 -0400

    Show citations tab only if collection has citations

--- application/views/catalog_search/study_collection_tabs.php
+++ application/views/catalog_search/study_collection_tabs.php
@@ -32,7 +32,9 @@ else{
   <ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist" style="background:none;margin-top:-35px;border-bottom:1px solid gainsboro;">
     <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='about') ? $active_class : '' ?>" role="tab" tabindex="0" aria-controls="tabs-1" aria-labelledby="ui-id-1" aria-selected="false"><a href="<?php echo site_url('catalog/'.$repo['repositoryid']);?>/about" class="ui-tabs-anchor" role="presentation" tabindex="-1" ><?php echo t('tab_about');?></a></li>
     <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='catalog') ? $active_class : '' ?>" role="tab" tabindex="-1" aria-controls="tabs-23" aria-labelledby="ui-id-23" aria-selected="true"><a href="<?php echo site_url('catalog/'.$repo['repositoryid']);?>" class="ui-tabs-anchor" role="presentation" tabindex="-1" ><?php echo t('tab_datasets');?></a></li>
+    <?php if ($repo_citations_count>0):?>
     <li class="ui-state-default ui-corner-top <?php echo ($active_tab=='citations') ? $active_class : '' ?>" role="tab" tabindex="-1" aria-controls="tabs-3" aria-labelledby="ui-id-4" aria-selected="false"><a href="<?php echo site_url('citations/?collection='.$repo['repositoryid']);?>" class="ui-tabs-anchor" role="presentation" tabindex="-1" ><?php echo t('tab_citations');?></a></li>
+    <?php endif;?>
   </ul>
   <div id="tabs-1" aria-labelledby="ui-id-1" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" style="" aria-expanded="true" aria-hidden="false">  	
 	  <?php echo $content;?>  
