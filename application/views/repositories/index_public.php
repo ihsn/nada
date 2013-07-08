52cec9e9fd51137a150e9683d92e78a8136d4fcc
Mehmood Asghar <wb329102@PN3023029.wb.ad.worldbank.org>
2013-07-08 17:31:38 -0400

    Updated formatting for the collection about page - hides the no records found message if no collections were found for central

--- application/views/repositories/index_public.php
+++ application/views/repositories/index_public.php
@@ -3,13 +3,14 @@
 .thumb{padding-right:10px;padding-bottom:5px;}
 .thumb img{padding-bottom:10px;}
 .page-title{border-bottom:1px solid gainsboro;}
-.contributing-repos h2 {border-bottom:0px solid gainsboro;font-size:18px; font-family:Arial, Helvetica, sans-serif; text-transform:uppercase; font-weight:bold; word-spacing:110%;margin-top:20px;}
+.contributing-repos h2 {border-bottom:0px solid gainsboro;font-size:18px; font-family:Arial, Helvetica, sans-serif; text-transform:uppercase; font-weight:normal; word-spacing:110%;margin-top:20px;}
 .contributing-repos p a, .central-repo p a{color:black;}
 .contributing-repos p a:hover, .central-repo p a:hover{text-decoration:underline}
-
+.contributing-repos {border-top:1px solid gainsboro;}
 </style>
-<div class="contributing-repos" >
+
 <?php if ($sections):?>
+<div class="contributing-repos" >
 	<?php foreach($sections as $section_id=>$section): ?>    
     	<?php 
 			$data=array(
@@ -29,10 +30,5 @@
 		<?php endif;?>
         
     <?php endforeach;?>
-
-<?php else: ?>
-<?php echo t('no_records_found'); ?>
-<?php endif; ?>
-
-
 </div>
+<?php endif; ?>
