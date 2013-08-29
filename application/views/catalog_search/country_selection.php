1ad8b2fc8ce79c4c9fe4385708d3530f337fd6b3
Mehmood Asghar <mah0001@gmail.com>
2013-08-29 14:03:54 -0400

    Updated view to only return the tab list for the region selection, content for tabs is loaded using jquery tabs

--- application/views/catalog_search/country_selection.php
+++ application/views/catalog_search/country_selection.php
@@ -1,87 +1,21 @@
-<?php 
-//fix for UI tabs
-$repo=$this->input->get("repo");
-
-if(!$repo && $repo!='central' && $repo!='')
-{
-	if (!$result=$this->repository_model->repository_exists($repo))
-	{
-		return false;
-	}	
-}
-
-$referer=site_url('catalog/'.$repo);
-?>
-
 <script>
 $(document).ready(function()  {
-	$( "#tabs" ).tabs();
+	$("#tabs").tabs({
+		beforeLoad: function (event, ui) {
+		//don't load content if already loaded
+        if ($(ui.panel).html()) {
+            event.preventDefault();
+        }
+		}
+	});
 });
 </script>
 <div id="tabs">
   <ul>
-    <li class="first"><a href="<?php echo $referer;?>#tabs-1"><?php echo t('in_alphabatic_order');?></a></li>
+    <li class="first"><a href="<?php echo site_url('catalog/get_country_selection_tab/'.$repositoryid.'/alphabatical');?>#tabs-1"><?php echo t('in_alphabatic_order');?></a></li>
     <?php foreach($regions as $region):?>
-    	<li><a href="<?php echo $referer;?>#tabs-region-<?php echo $region['id'];?>"><?php echo t($region['title']);?></a></li>
+    	<li><a href="<?php echo site_url('catalog/get_country_selection_tab/'.$repositoryid.'/region/'.$region['id']);?>#tabs-region-<?php echo $region['id'];?>"><?php echo t($region['title']);?></a></li>
   	<?php endforeach;?>  
   </ul>
-  <div id="tabs-1">
-    <!--tab-alphabatic-country-list-->
-    <div class="container" >
-	<?php 
-        $index=array();
-        $letters=array();
-		foreach (range('a', 'z') as $alphabet)
-		{
-			$letters[strtoupper($alphabet)]='';
-		}		
-    ?>
-    <?php if($countries):?>
-	<?php foreach($countries as $country):?>
-        <?php 
-            $letter=strtoupper(substr($country['nation'],0,1));
-            $index[$letter][]=$country;
-            $letters[$letter]=$letter;
-        ?>
-    <?php endforeach;?>
-    <?php endif;?>
-
-    <div class="index">
-    <?php foreach($letters as $key=>$value):?>
-    	<?php if($value==''):?>
-        <span class="in-active" data-id="country-<?php echo $key;?>"><?php echo $key;?> </span>
-        <?php else:?>
-        <span class="active" data-id="country-<?php echo $key;?>"><?php echo $key;?> </span>
-        <?php endif;?>
-    <?php endforeach;?>
-    </div>
-    
-    <div class="rows-container">
-    <?php foreach($index as $letter=>$countries): ?>
-        <div class="row" id="country-<?php echo $letter;?>">
-            <div class="col-1-s letter letter-caption" ><?php echo $letter;?></div>
-            <div class="col-2-s cnt">
-            <?php foreach($countries as $country): ?>
-                    <div class="country item" >
-                        <input class="chk-item" type="checkbox" 
-                            value="<?php echo form_prep($country['cid']); ?>" 
-                            id="ca-<?php echo form_prep($country['cid']); ?>"
-                            data-type="child"
-                            data-name="c-<?php echo form_prep($country['cid']); ?>"
-                         />
-                        <label for="ca-<?php echo form_prep($country['cid']); ?>">
-                            <?php echo $country['nation']; ?> <span class="count">(<?php echo $country['surveys_found']; ?>)</span>
-                        </label>
-                    </div>
-            <?php endforeach;?>
-            </div>    
-        </div>
-    <?php endforeach;?>
-    </div>
-    </div>
-    <!--end-tab-alphabatic-country-list-->
-  </div>
-
-    <?php $this->load->view('catalog_search/country_selection_regions');?>
+  <div id="country-selection-tab-content"></div>
 </div>
-
