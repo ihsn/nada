bef9d1ea4d3ca6d5670bf28ebdcaed32d2897d76
Mehmood Asghar <mah0001@gmail.com>
2013-10-28 16:09:15 -0400

    Update admin notification view for licensed requests

--- application/views/access_licensed/admin_notification_email.php
+++ application/views/access_licensed/admin_notification_email.php
@@ -5,37 +5,13 @@
 */
 ?>
 
-<?php if($request_type='study'):?>
-
-<?php
-
-$study_years=array(
-	$surveys[0]['data_coll_start'],
-	$surveys[0]['data_coll_end']			
-);
-$study_years=array_unique($study_years);
-$study_years=implode(" - ",$study_years);
-
-$study_title=$surveys[0]['nation']. ' - '.$surveys[0]['titl']; //. ' - '.$study_years;
-
-?>
-
 <p>
-    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $study_title);?></p>
+    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $request_title);?></p>
     <div style="margin:10px;padding:10px;border:1px solid orange;">
-    	<h3 style="margin:0px;"><a href="<?php echo site_url('admin/licensed_requests/edit/'.$id);?>"><?php echo $surveys[0]['titl'];?></a></h3>
-        <div><?php echo $surveys[0]['nation'];?>, <?php echo $study_years;?></div>
+    	<h3 style="margin:0px;"><a href="<?php echo site_url('admin/licensed_requests/edit/'.$id);?>"><?php echo $request_title;?></a></h3>
     </div>
     
     <p><?php echo t('to_view_request_instructions');?><p>
     <p>-- </p>
     <p><?php echo t('Website');?>: <a href="<?php echo site_url();?>"><?php echo site_url();?></a></p>
 </p>
-<?php else:?>
-<p>
-    <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, 'collection['.$collection['title'].']');?></p>
-    <p><?php echo t('to_view_request_instructions');?><p>
-    <p>-- </p>
-    <p><?php echo t('Website');?>: <a href="<?php echo site_url();?>"><?php echo site_url();?></a></p>
-</p>
-<?php endif;?>
