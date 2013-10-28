4b9d4d7a45fe88e615530506fd66ed360728416f
Mehmood Asghar <mah0001@gmail.com>
2013-10-28 16:11:55 -0400

    Added views to show study resources per licensed request

---
+++ application/views/access_licensed/survey_resources.php
@@ -0,0 +1,132 @@
+<?php if($resources):?>
+<div style="padding-top:20px;">
+<h3><?php echo t('study_resources');?></h3>
+<div class="resources">
+    <?php $class="resource"; ?>
+	<?php foreach($resources as $key=>$resourcetype):?>
+		<?php if (count($resourcetype)>0):?>
+        <fieldset>
+        <legend>
+			<?php 
+				switch($key) 
+				{
+					case 'technical':
+						echo t('technical_documents');
+					break;
+					
+					case 'reports':
+						echo t('reports');
+					break;
+					
+					case 'questionnaires':
+						echo t('questionnaires');
+					break;
+					
+					case 'other':
+					default:
+						echo t('other_materials');
+					break;
+				}
+			?>
+        </legend>
+				<?php foreach($resourcetype as $row):?>
+                <?php 
+						//clean up fields
+						$row['country']=strip_brackets($row['country']);
+						$row['language']=strip_brackets($row['language']);
+						
+                        $url=NULL;
+						$file_size='';
+						
+                        //check file/URL
+                        if (substr($row['filename'],0,4)=='www.' || substr($row['filename'],0,7)=='http://' || substr($row['filename'],0,8)=='https://' || substr($row['filename'],0,6)=='ftp://') 
+                        {
+                            $url=prep_url($row['filename']);
+                        }
+                        elseif (trim($row['filename'])!=='' && check_resource_file($survey_folder.'/'.$row['filename'])!==FALSE )
+                        {
+                            $url=site_url().'/catalog/'.$sid.'/download/'.$row['resource_id'];
+							$file_size=format_bytes(filesize($survey_folder.'/'.$row['filename']),2);
+                        }
+						//get file extension
+						$ext=get_file_extension($row['filename']);
+                ?>
+                <?php if($class=="resource") {$class="resource alternate";} else{ $class="resource"; } ?>
+                <div class="<?php echo $class;?>">    	
+                    <div class="resource-left-col">
+                    		<div class="resource-info" class="resource-info" title="<?php echo t('click_to_view_information');?>" alt="<?php echo t('view_more_information');?>" id="<?php echo $row['resource_id'];?>"><?php echo $row['title'];?></div>
+                    </div>
+                    <div class="resource-right-col">
+                    	<span class="resource-file-size">
+							<?php 
+								$link_text= '<img src="'.get_file_icon($ext).'" alt="'.$ext.'"  title="'.basename($row['filename']).'"/> ';
+								//$link_text.= strtoupper($ext);
+                            	if ($file_size!='')
+								{									
+									$link_text.= ' &nbsp; '.$file_size;
+                            	}
+								
+								if ($url!='')
+								{
+									$link_text= '<a target="_blank" href="'.$url.'" title="'.basename($row['filename']).'" class="download">'.$link_text.'</a>';
+								}
+								else
+								{
+									$link_text="";
+								}
+								
+								echo $link_text;
+								?>
+                        </span>        
+                    </div>
+                    <div style="float:right;font-size:11px;">
+                    </div>
+                    <br style="clear:both;"  />
+
+                    <?php if ($row['description']!='' || $row['title']!=''  || $row['toc']!='' ):?>                    
+						<div id="info_<?php echo $row['resource_id'];?>" class="abstract">
+                        
+                        <?php $fields_arr=array(
+									'author'=>		t('authors'),
+									'subtitle'=>	t('subtitle'),
+									'dcdate'=>		t('date'),
+									'country'=>		t('country'),
+									'language'=> 	t('language'),
+									'contributor'=> t('contributors'),
+									'publisher'=>	t('publishers'),
+									'rights'=>		t('rights'),
+									'description'=> t('description'),
+									'abstract'=>	t('abstract'),
+									'toc'=>			t('table_of_contents'),
+									'subjects'=>	t('subjects')
+									);
+						?>
+                        
+                        <table class="grid-table tbl-resource-info" >
+							<?php foreach ($row as $key=>$value):?>
+                                <?php if ($value!=""):?>
+                                <?php if (array_key_exists($key,$fields_arr)):?>
+                                <tr valign="top">
+                                    <td  class="caption"><?php echo $fields_arr[$key];?></td>
+                                    <td><?php echo nl2br($value);?></td>
+                                </tr>
+                                <?php endif;?>
+                                <?php endif;?>
+                            <?php endforeach;?>
+                            <tr>
+                                <td class="caption"><?php echo t('download');?></td>
+                                <td><?php echo ($link_text==="") ? "N/A" : '<a class="download" title="'.basename($row['filename']).'" href="'.$url.'">'.$url.'</a>';?></td>
+                            </tr>                        
+                        </table>
+                        
+                        </div>
+                    <?php endif;?>
+                
+                 </div>
+                <?php endforeach;?>
+    </fieldset>
+    <?php endif;?>
+    <?php endforeach;?>
+</div>
+</div>
+<?php endif;?>
---
+++ application/views/access_licensed/track_request_downloads.php
@@ -0,0 +1,132 @@
+<style>
+.survey-row{font-size:12px; line-height:140%;border-bottom:1px solid gainsboro;padding:5px;;}
+.survey-row:hover,
+.survey-active{
+	background:gainsboro;
+}
+
+.survey-row .title{font-weight:bold;}
+.survey-row .sub{}
+
+.survey-request-downloads{width:100%;border:0px solid gainsboro;}
+.survey-request-downloads tr td{vertical-align:top;}
+.survey-resources{padding:10px;}
+.resources-container{border:0px solid gainsboro;border-left:0px;}
+.survey-resources .survey-info{margin-bottom:15px;}
+.survey-list{width:250px;background:url(themes/wb/active-tab-bg.png) repeat-y right;}
+
+/*survey resources summary*/
+.resources h3{font-weight:bold;padding-top:10px;font-size:12px;}
+.abstract{display:none;margin-bottom:10px;background-color:none;}
+.resources .alternate, .resources .resource{border-bottom:1px solid #C1DAD7;padding:5px;width:98%;}
+.resources .alternate{background-color:#FBFBFB;}
+.resources .alternate:hover, .resources .resource:hover{background-color:#EAEAEA}
+.resources fieldset {border:0px;border-top:4px solid gainsboro;margin:0px;padding:0px;margin-top:20px;margin-bottom:10px;padding-top:5px;color:#333333;font-size:12px;}
+.resources fieldset legend{font-weight:bold;;padding:5px;text-transform:capitalize;margin-left:10px;}	
+.resource-info{cursor:pointer;}
+.resource-right-col{float:right;font-size:11px;width:15%;}
+.resource-left-col{float:left;width:85%;}
+.resource-file-size{display:inline-block;width:100px;text-align:left;color:#999999;}
+.tbl-resource-info{padding:0px;margin:0px; border-collapse:collapse}
+.resource-info{padding-left:20px;background:url('images/blue-add.png') no-repeat left top;}
+.active .resource-info{font-weight:bold;margin-bottom:10px;background:url('images/blue-remove.png') no-repeat left top;}
+.resources .active{border:1px solid gainsboro;margin-bottom:20px;}
+.resource .caption{font-weight:bold;}
+
+
+.survey-request-downloads-container {clear:both;}
+.survey-request-downloads-container .survey-list-row{float:left;width:250px;border-right:1px solid gainsboro;}
+.survey-request-downloads-container .resources-container{border-left:1px solid gainsboro;margin-left:250px;}
+.survey-request-downloads-container{overflow:auto;border-top:1px solid gainsboro;}
+</style>
+
+<?php
+$request_url=site_url('access_licensed/track/'.$id);
+?>
+
+<h2><?php echo t('download_microdata_and_resources');?></h2>
+
+<?php if (count($surveys)==1):?>
+<div class="survey-resources">
+    <?php $this->load->view('access_licensed/survey_resources_microdata',array('resources_microdata'=>$microdata_resources,'request_id'=>$id));?>
+    <br style="margin-top:20px;"/>
+    <?php $this->load->view('access_licensed/survey_resources',array('resources'=>$external_resources,'request_id'=>$id));?>
+</div>
+
+<?php else:?>
+<!--
+<table class="survey-request-downloads" >
+<tr>
+<td class="survey-list">
+<?php foreach ($surveys as $survey):?>
+	<div class="survey-row <?php echo ($sid==$survey['id']) ? 'survey-active' :'';?>">
+		<div class="title"><?php echo anchor($request_url.'?sid='.$survey['id'],$survey['titl']);?></div>
+        <div class="sub"><?php echo $survey['nation'];?>, <?php echo $survey['data_coll_start'];?></div>
+    </div>
+<?php endforeach;?>
+</td>
+<td class="resources-container">
+<div class="survey-resources">
+	<div class="survey-info">
+    	<h3><?php echo $surveys[$sid]['titl'];?></h3>
+    	<h4><?php echo $surveys[$sid]['nation'];?>, <?php echo $surveys[$sid]['data_coll_start'];?></h4>
+	</div>
+
+    <?php $this->load->view('access_licensed/survey_resources_microdata',array('resources_microdata'=>$microdata_resources,'request_id'=>$id));?>
+    <br style="margin-top:20px;"/>
+    <?php $this->load->view('access_licensed/survey_resources',array('resources'=>$external_resources,'request_id'=>$id));?>
+</div>
+</td>
+</tr>
+</table>
+-->
+
+<div class="survey-request-downloads-container" >
+    <div class="survey-list-row">
+    <?php foreach ($surveys as $survey):?>
+    	<?php if (!in_array($survey['id'],$surveys_with_files)){continue;}?>
+        <div class="survey-row <?php echo ($sid==$survey['id']) ? 'survey-active' :'';?>">
+            <div class="title"><?php echo anchor($request_url.'?sid='.$survey['id'],$survey['titl']);?></div>
+            <div class="sub"><?php echo $survey['nation'];?>, <?php echo $survey['data_coll_start'];?></div>
+        </div>
+    <?php endforeach;?>
+    </div>
+    <div class="resources-container">
+        <div class="survey-resources">
+            <div class="survey-info">
+                <h3><?php echo $surveys[$sid]['titl'];?></h3>
+                <h4><?php echo $surveys[$sid]['nation'];?>, <?php echo $surveys[$sid]['data_coll_start'];?></h4>
+            </div>
+        
+            <?php $this->load->view('access_licensed/survey_resources_microdata',array('resources_microdata'=>$microdata_resources,'request_id'=>$id));?>
+            <br style="margin-top:20px;"/>
+            <?php $this->load->view('access_licensed/survey_resources',array('resources'=>$external_resources,'request_id'=>$id));?>
+        </div>
+    </div>
+</div>
+
+
+<?php endif;?>
+
+<!--survey summary resources-->
+<script type="text/javascript">
+	function toggle_resource(element_id){
+		$("#"+element_id).parent(".resource").toggleClass("active");
+		$("#"+element_id).toggle();
+	}
+	
+	$(document).ready(function () { 
+		bind_behaviours();
+	});	
+	
+	function bind_behaviours() {
+		//show variable info by id
+		$(".resource-info").unbind('click');
+		$(".resource-info").click(function(){
+			if($(this).attr("id")!=''){
+				toggle_resource('info_'+$(this).attr("id"));
+			}
+			return false;
+		});			
+	}
+</script>
