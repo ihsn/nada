75d1aa97fdec79fbe4c7f28035aadfe7b7464857
Mehmood Asghar <mah0001@gmail.com>
2014-01-28 15:44:53 -0500

    removed email failure warning message

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -135,11 +135,11 @@
     
     <div class="yui-u span6">
     
-    			<?php if ($failed_email_count>0):?>
+    			<?php /*if ($failed_email_count>0):?>
                	<div class="alert alert-error">
                 	Check email settings - <?php echo $failed_email_count;?> errors were logged. <a href="<?php echo site_url('admin/logs?keywords=email-failed&field=logtype');?>">View details</a></b>
                	</div>
-                <?php endif;?>
+                <?php endif;*/?>
     
                 <div class="dashboard-box">
                     <div class="dashboard-box-title"><?php echo t('users');?></div>
