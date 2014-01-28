118654cf1150a5790826c0bbfa372e8bcdff60c7
Mehmood Asghar <mah0001@gmail.com>
2014-01-28 15:27:12 -0500

    Updated licensed request admin notification to include study title with country name

--- application/views/access_licensed/admin_notification_email.php
+++ application/views/access_licensed/admin_notification_email.php
@@ -8,7 +8,20 @@
 <p>
     <p><?php echo sprintf(t('user_has_requested_licensed'),$fname . ' ' . $lname, $request_title);?></p>
     <div style="margin:10px;padding:10px;border:1px solid orange;">
-    	<h3 style="margin:0px;"><a href="<?php echo site_url('admin/licensed_requests/edit/'.$id);?>"><?php echo $request_title;?></a></h3>
+		<h3 style="margin:0px;"><a href="<?php echo site_url('admin/licensed_requests/edit/'.$id);?>"><?php echo $request_title;?></a></h3>
+		<?php if (count($surveys)==1):?>
+		<?php $survey=reset($surveys);?>
+			<div><?php echo $survey['nation'];?>, <?php echo $survey['data_coll_start'];?> <?php echo isset($survey['data_coll_end']) ? ' - '.$survey['data_coll_end'] : '';?></div>
+		<?php else:?>
+		<ul>
+			<?php foreach($surveys as $survey):?>
+				<li><b><?php echo $survey['titl'];?></b><br/>
+					<?php echo $survey['nation'];?>, <?php echo $survey['data_coll_start'];?> <?php echo isset($survey['data_coll_end']) ? ' - '.$survey['data_coll_end'] : '';?>
+					<br/><br/>
+				</li>
+			<?php endforeach;?>
+		</ul>
+		<?php endif;?>
     </div>
     
     <p><?php echo t('to_view_request_instructions');?><p>
