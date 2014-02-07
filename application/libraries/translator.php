587894ef058146fed01bdae787ae7b2cc92d96e0
Mehmood Asghar <mah0001@gmail.com>
2014-02-07 10:06:10 -0500

    Added helper methods for translator

--- application/libraries/translator.php
+++ application/libraries/translator.php
@@ -64,6 +64,75 @@ class Translator{
 		return $lang;
 	}
 	
+	
+	/**
+	 * List language files
+	 *
+	 * @return array
+	 */
+	function get_language_files_array($language_path ) {
+		
+		$modules = array();
+		
+		$dir = $language_path;
+
+		$d = @dir( $dir );
+
+		if ( $d ) {
+			while (false !== ($entry = $d->read())) {
+			   	$file = $dir . '/' . $entry;
+				if ( is_file( $file ) ) {
+					$path_parts = pathinfo( $file );
+					if ( $path_parts[ 'extension' ] == 'php' ) {
+						$modules[] = $entry;
+					}
+			   }
+			}
+			$d->close();
+		} else {
+				return FALSE;
+		}
+
+		sort($modules);		
+		return $modules;		
+	}
+	
+	
+	//returns an array of merge source and target language translations
+	function merge_language_keys($source_file,$target_file)
+	{		
+		//load the translations from language file into an array
+		$source_lang_arr=(array)$this->get_translations_array($source_file);
+		$target_lang_arr=(array)$this->get_translations_array($target_file);
+		
+		foreach($source_lang_arr as $key=>$value)
+		{
+			//add missing translations to the target file 
+			if (!array_key_exists($key,$target_lang_arr))
+			{
+				$target_lang_arr[$key]=$value;
+			}
+		}
+		
+		return $target_lang_arr;		
+	}
+	
+	
+	//get language translations key/values as array
+	function get_translations_array($file_path)
+	{		
+		$lang=NULL;
+		
+		if (file_exists($file_path))
+		{
+			//fills the values in a local variable $lang
+			include $file_path;
+		}	
+		
+		return $lang;
+	}
+	
+	
 }// END Translator Class
 
 /* End of file Translator.php */
