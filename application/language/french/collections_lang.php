8c15d41ec533f66393232ebca65ebdfc93515471
Mehmood Asghar <mah0001@gmail.com>
2014-04-24 15:54:25 -0400

    added missing 'french' language files

---
+++ application/language/french/collections_lang.php
@@ -0,0 +1,14 @@
+<?php 
+$lang['collections']="Collections";
+$lang['collections_management']="Gestion des collections";
+$lang['description']="Description";
+$lang['weight']="Pondération";
+$lang['thumbnail']="URL de la vignette";
+$lang['collections_home']="Accueil";
+$lang['collections_add_page']="Ajouter une collection";
+$lang['collections_edit']="Editer une collection";
+$lang['collections_add']="Ajouter une collection";
+
+
+/* End of file collections_lang.php */
+/* Location: ./application/language/french/collections_lang.php */
