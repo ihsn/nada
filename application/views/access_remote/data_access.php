c40c3c9eeee42843816fbc87c14934918786ab1b
unknown <wb329102@PN3023029.wb.ad.worldbank.org>
2014-08-01 14:57:54 -0400

    open links in a new window

--- application/views/access_remote/data_access.php
+++ application/views/access_remote/data_access.php
@@ -2,6 +2,6 @@
 	<h2><?php echo t('remote_title');?></h2>
 	<p><?php echo t('data_is_available_from_external_repository');?></p>
     <div class="remote-access-link">
-        <?php echo anchor($link,$link);?>
+        <a href="<?php echo $link;?>" target="_blank"><?php echo $link;?></a>
     </div>
 </div>
