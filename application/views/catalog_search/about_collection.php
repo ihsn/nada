862e48a5a48b167c9f28e4b021b993034373f0c7
unknown <wb329102@PN3023029.wb.ad.worldbank.org>
2014-08-07 14:52:55 -0400

    Hide the central catalog about text, if no text is provided.

--- application/views/catalog_search/about_collection.php
+++ application/views/catalog_search/about_collection.php
@@ -6,7 +6,7 @@
 <div class="visit-catalog">
 	<a class="btn btn-primary color-white" href="<?php echo site_url(); ?>/catalog/<?php echo $row->repositoryid; ?>"><?php echo t('visit_catalog');?></a>
 </div>
-<?php elseif($row->repositoryid=='central'):?>
+<?php elseif($row->repositoryid=='central' && strlen( t('about_central_catalog')) > 50 ):?>
 <div class="about-central">
 	<p><?php echo t('about_central_catalog');?></p>    
     <div class="visit-catalog">
