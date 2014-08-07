2e8c3b3ec0639977a677513edf613067d6ebedb7
unknown <wb329102@PN3023029.wb.ad.worldbank.org>
2014-08-07 14:53:15 -0400

    removed inline styles

--- application/views/repositories/index_public.php
+++ application/views/repositories/index_public.php
@@ -1,13 +1,3 @@
-<style>
-.repo-table td{padding-top:10px;padding-bottom:0px;border-bottom:1px solid gainsboro;font-size:12px;line-height:140%;}
-.thumb{padding-right:10px;padding-bottom:5px;}
-.thumb img{padding-bottom:10px;}
-.page-title{border-bottom:1px solid gainsboro;}
-.contributing-repos h2 {border-bottom:0px solid gainsboro;font-size:18px; font-family:Arial, Helvetica, sans-serif; text-transform:uppercase; font-weight:normal; word-spacing:110%;margin-top:20px;}
-.contributing-repos p a, .central-repo p a{color:black;}
-.contributing-repos p a:hover, .central-repo p a:hover{text-decoration:underline}
-.contributing-repos {border-top:1px solid gainsboro;}
-</style>
 
 <?php if ($sections):?>
 <div class="contributing-repos" >
