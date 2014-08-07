c0edd8cd376913e22629447333e9b291b4aac83e
unknown <wb329102@PN3023029.wb.ad.worldbank.org>
2014-08-07 14:54:15 -0400

    added an admin view file for translations

---
+++ application/views/translator/index_admin.php
@@ -0,0 +1,21 @@
+<div class="body-container" style="padding:10px;">
+
+<h1 class="page-title"><?php echo t('translate');?></h1>
+
+Template language set to: BASE
+<table class="grid-table" width="100%" cellspacing="0" cellpadding="0">
+	<tr class="header">
+        <th>Language</th>
+        <th><?php echo t('actions');?></th>
+    </tr>
+	<?php $tr_class=""; ?>
+	<?php foreach($languages as $lang):?>
+            <?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
+        <tr class="<?php echo $tr_class; ?>" valign="top">
+            <td><a href="<?php echo site_url('admin/translate/edit/'.$lang);?>"><?php echo $lang;?></a></td>
+            <td><?php echo anchor('admin/translate/edit/'.$lang,t('edit'));?> | <?php echo anchor('admin/translate/download/'.$lang,t('download'));?></td>    
+        </tr>
+    <?php endforeach;?>
+</table>
+
+</div>
