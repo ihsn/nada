b0e1ee959cba7e09dc00c28b758b5f8df8a506c8
Mehmood Asghar <mah0001@gmail.com>
2014-08-12 14:40:54 -0400

    removed unused language files

--- application/language/english_ddp/breadcrumbs_lang.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$lang['Home']="Home";
-$lang['Data Catalog']="Data catalog";
-$lang['Citations']="Citations";
-
-
-/* End of file breadcrumbs_lang.php */
-/* Location: ./application/language/english_ddp/breadcrumbs_lang.php */
