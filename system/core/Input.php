90a94be4baa7aa39f7d529776cad829d777fd248
Mehmood Asghar <mah0001@gmail.com>
2015-10-24 23:36:40 -0400

    core fix: some cookie names cause the disallowed key char error.

--- system/core/Input.php
+++ system/core/Input.php
@@ -727,9 +727,9 @@ class CI_Input {
 	*/
 	function _clean_input_keys($str)
 	{
-		if ( ! preg_match("/^[a-z0-9:_\/-]+$/i", $str))
+		if ( ! preg_match("/^[~a-z0-9:_\/-]+$/i", $str))
 		{
-			exit('Disallowed Key Characters.');
+			exit('Disallowed Key Characters.'. $str);
 		}
 
 		// Clean UTF-8 if supported
