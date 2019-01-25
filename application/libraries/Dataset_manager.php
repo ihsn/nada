d93e2d66ddbd6e26d6b4e99f89555ecc8ba303b6
Mehmood Asghar <mah0001@gmail.com>
2019-01-25 11:30:40 -0500

    add wrapper methods for dataset models

--- application/libraries/Dataset_manager.php
+++ application/libraries/Dataset_manager.php
@@ -33,6 +33,14 @@ class Dataset_manager{
         return $this->ci->{'Dataset_'.$this->types[$type].'_model'}->create_dataset($type,$options);
     }
 
+
+    function update_dataset($sid,$type,$options)
+    {
+        $this->validate_type($type);
+        return $this->ci->{'Dataset_'.$this->types[$type].'_model'}->update_dataset($type,$options);
+    }
+
+
     private function validate_type($type)
     {
         if(!$this->is_valid_type($type)){
@@ -54,6 +62,24 @@ class Dataset_manager{
         return $this->ci->Dataset_model->get_idno($sid);    
     }
 
+
+    function find_by_idno($idno)
+    {
+        return $this->ci->Dataset_model->find_by_idno($idno);    
+    }
+
+
+    /**
+     * 
+     * Return all rows
+     * 
+     */
+    function get_all()
+    {
+        return $this->ci->Dataset_model->get_all();
+    }
+
+
     /**
      * 
      * Return a single row with minimum fields
@@ -62,6 +88,27 @@ class Dataset_manager{
     function get_row($sid)
     {
         return $this->ci->Dataset_model->get_row($sid);
+    }
+
+
+    /**
+     * 
+     * Get metadata
+     * 
+     */
+    function get_metadata($sid)
+    {
+        return $this->ci->Dataset_model->get_metadata($sid);
+    }
+
+    function validate_options($options)
+    {
+        return $this->ci->validate_options($options);
+    }
+
+    public function get_data_access_type_id($name)
+    {
+        return $this->ci->Dataset_model->get_data_access_type_id($name);
     }
 
     /**
@@ -86,9 +133,37 @@ class Dataset_manager{
     }
 
 
-    
+    function update_varcount($sid)
+    {
+        return $this->ci->Dataset_model->update_varcount($sid);
+    }
+
+    function delete($sid)
+    {
+        return $this->ci->Dataset_model->delete($sid);
+    }
+
+    function set_data_access_type($sid,$da_type,$da_link)
+    {
+        return $this->ci->Dataset_model->set_data_access_type($sid,$da_type,$da_link);
+    }
+
+    function update_sid($old_sid,$new_id)
+    {
+        return $this->ci->Dataset_model->update_sid($old_sid,$new_id);
+    }
+
+    function set_publish_status($sid,$publish_status)
+    {
+        return $this->ci->Dataset_model->set_publish_status($sid,$publish_status);
+    }
+
+    function remove_datafile_variables($sid,$file_id)
+    {
+        return $this->ci->Dataset_microdata_model->remove_datafile_variables($sid,$file_id);
+    }
 
 }
 
-/* End of file Datasets.php */
-/* Location: ./application/libraries/Datasets.php */
+/* End of file Dataset_manager.php */
+/* Location: ./application/libraries/Dataset_manager.php */
