7e27230b163e812b78a497270a645b9ad7d61fae
Mehmood Asghar <mah0001@gmail.com>
2019-02-21 13:57:44 -0500

    update home page

--- application/views/static/custom/home.php
+++ application/views/static/custom/home.php
@@ -154,25 +154,25 @@ $collections=array(
         'title'=> 'Global Financial Inclusion (Global Findex) Database',
         'url'=>site_url('catalog/findex'),
         'description'=>'The Global Findex is the first public database of indicators that measures people\'s use of financial services across economies and over time. Based on over 150,000 interviews across more than 140 economies, the database can be used to develop a deeper understanding of how people save, borrow, make payments, and manage risk.',
-        'thumb'=>'http://microdata.worldbank.org/files/findex-fp-01.jpg'
+        'thumb'=>base_url().'files/public/collections/findex-sm.jpg'
     ),
     'lsms'=>array(
         'title'=> 'Living Standards Measurement Study (LSMS)',
         'url'=>site_url('catalog/findex'),
         'description'=>'The LSMS is a research project that was initiated in 1980. It is a response to a perceived need for policy relevant data that would allow policy makers to move beyond simply measuring rates of unemployment, poverty and health care use, for example, to understanding the determinants of these observed social sector outcomes.',
-        'thumb'=>'http://microdata.worldbank.org/files/lsms-fp-01.gif'
+        'thumb'=>base_url().'files/public/collections/lsms-sm.jpg'
     ),
     'ipums'=>array(
         'title'=> 'Integrated Public Use Microdata Series (IPUMS)',
         'url'=>site_url('catalog/findex'),
         'description'=>'IPUMS provides census and survey data from around the world integrated across time and space. IPUMS integration and documentation makes it easy to study change, conduct comparative research, merge information across data types, and analyze individuals within family and community context. Data and services available free of charge.',
-        'thumb'=>'http://microdata.worldbank.org/files/ipums-fp-02.jpg'
+        'thumb'=>base_url().'files/public/collections/ipums-sm.jpg'
     ),
     'mics'=>array(
         'title'=> 'UNICEF Multiple Indicator Cluster Surveys (MICS)',
         'url'=>site_url('catalog/findex'),
         'description'=>'UNICEF supports countries to collect data on the situation of children and women through the Multiple Indicator Cluster Survey (MICS) program. MICS is designed to collect statistically sound, internationally comparable data on child-related indicators.',
-        'thumb'=>'http://microdata.worldbank.org/files/mics-fp-01.jpg'
+        'thumb'=>base_url().'files/public/collections/mics-sm.jpg'
     )
 
 );
@@ -191,9 +191,8 @@ $collections=array(
     <?php foreach($collections as $repoid=>$collection):?>
         <div class="col-md-3">
         <div class="card card-collection"  style="margin-bottom:20px;margin-top:10px;">
-        <!--<img class="card-img-top" data-src="holder.js/100px180/?text=Image cap" alt="Image cap [100%x180]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_166c15c23f5%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_166c15c23f5%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2299.4296875%22%20y%3D%2296.6%22%3EImage%20cap%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="height: 180px; width: 100%; display: block;">-->
         <a href="<?php echo $collection['url'];?>">
-            <img src="<?php echo $collection['thumb'];?>" class="card-img-top" style="height: 180px; width: 100%; display: block;"/>
+            <img src="<?php echo $collection['thumb'];?>" class="img-fluid xcard-img-top" xstyle="height: 180px; width: 100%; display: block;"/>
         </a>
         <div class="card-body"  style="padding:10px 0px;">
             <h5 class="card-title"><a href="<?php echo $collection['url'];?>"><?php echo $collection['title'];?></a></h5>
@@ -368,68 +367,4 @@ $collections=array(
 </div>
 
 </div>
-</div>
-
-
-
-
-<?php return;?>
-
-
-
-
-
-
-
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
-        <div class="ui-tabs wb-tab-heading pt-4 pb-2 pr-4 pl-4 mb-4">
-
-            <?php
-            //list of repos
-            $data['rows']=$this->repository_model->get_repositories($published=TRUE, $system=FALSE);
-            $this->load->view("catalog_search/recent_studies_list",array('rows'=>$latest_surveys));
-            ?>
-
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-        <!-- *****  Stats ***** -->
-        <div class="wb-box-sidebar wb-tab-heading pt-3 pb-3 pr-4 pl-4 text-center">
-
-            <p>As of <strong><?php echo date("F d, Y",date("U")); ?></strong><br> the catalog contains</p>
-            <h3 class="mb-0"><?php echo number_format($survey_count);?></h3>
-            <p>Surveys</p>
-            <?php if ($variable_count>0):?>
-                <h3 class="mb-0"><?php echo number_format($variable_count);?> </h3>
-                <p>Variables</p>
-            <?php endif;?>
-            <?php if ($citation_count>0):?>
-            <h3 class="mb-0"><?php echo number_format($citation_count);?></h3>
-            <p>Citations</p>
-            <?php endif;?>
-            <a class="btn btn-primary" href="<?php echo site_url('catalog/central');?>" title="Data catalog">Data Catalog</a>
-
-        </div>
-        <?php if (isset($popular_surveys) && is_array($popular_surveys) && count($popular_surveys)>0):?>
-            <!-- **** popular studies **** -->
-            <div class="wb-box-sidebar wb-tab-heading mt-4 pt-3 pb-3 pr-4 pl-4">
-                <h5>Most popular studies</h5>
-                <?php foreach($popular_surveys as $survey): ?>
-                    <div class="citation-row" data-url="<?php echo site_url();?>/catalog/<?php echo $survey['id'];?>">
-                        <div class="row">
-                            <div class="col-12 row-body">
-                                    <span class="sub-title">
-                                        <a href="<?php echo site_url();?>/catalog/<?php echo $survey['id'];?>" title="<?php echo $survey['title'];?>">
-                                            <?php echo $survey['title'];?>
-                                        </a>
-                                    </span>
-                                <br><span><?php echo $survey['nation'];?></span>
-                            </div>
-                        </div>
-                    </div>
-                <?php endforeach;?>
-            </div>
-        <?php endif;?>
-    </div>
 </div>
