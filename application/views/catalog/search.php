8898c358e3597f5823401b95157795c7c1755151
Mehmood Asghar <mah0001@gmail.com>
2019-05-15 11:39:28 -0400

    changes to the UI formatting for catalog admin page

--- application/views/catalog/search.php
+++ application/views/catalog/search.php
@@ -104,10 +104,13 @@ $qs_sort=array('ps','title','idno','published','nation','tag','no_question','no_
 <table class="table table-striped" width="100%" cellspacing="0" cellpadding="0" >
     <?php $tr_class=""; ?>
     <?php foreach($rows as $row): ?>
-    	<?php //var_dump($row);?>
         <?php 
 			//is_owned by collection
-			$study_ownership=($active_repo_obj->repositoryid==$row['repositoryid']) ? 'owned' : 'linked';
+            $study_ownership=($active_repo_obj->repositoryid==$row['repositoryid']) ? 'owned' : 'linked';
+            
+            if ($row['form_model']==''){
+                $row['form_model']='data_na';
+            }
 		?>
         <?php if($tr_class=="") {$tr_class="alternate";} else{ $tr_class=""; } ?>
         <tr class="<?php echo $tr_class; ?> study-<?php echo $study_ownership;?>" id="s_<?php echo $row['id']; ?>"  valign="top">
@@ -117,29 +120,36 @@ $qs_sort=array('ps','title','idno','published','nation','tag','no_question','no_
                     	<div class="data-access-icon data-access-<?php echo $row['form_model'];?>" title="<?php echo $row['form_model'];?>"></div>
                         <h3>
                             <a href="<?php echo site_url().'/admin/catalog/edit/'.$row['id'];?>"><?php echo $row['title'];?></a>
+                            <!--<span class="data-access-type"><?php echo t($row['form_model']);?></span>-->
+                            
                         </h3>
                         <?php 
 							$study_years=array_unique(array($row['year_start'],$row['year_end']));
 							$study_years=implode(" - ",$study_years);
 						?>
                         <div class="sub-title">
-							<?php echo $row['nation'];?>, 
+							<?php echo $row['nation'];?>
 							 <?php if ($study_years==0):?>
-                            	<span class="badge badge-warning"><?php echo t('Year Missing');?></span>
+                            	<!--<span class="label label-warning"><?php echo t('Year Missing');?></span>-->
                             <?php else:?>
 	                            <?php echo $study_years;?>
-							<?php endif;?>							
+                            <?php endif;?>
+                            - 
+                            <span class="dataset-idno"><?php echo $row['idno'];?></span>
                         </div>
 
+                        <!--
                         <div class="table-row">
                         	<span class="cell-label"><?php echo t('ID');?>:</span>
 							<span class="cell-value"><?php echo $row['idno'];?></span>
                         </div>						
-
+                        !-->
 
                         <div class="table-row">
-                        	<span class="cell-label"><?php echo t('collection');?>:</span>
-                            <span class="cell-value">
+                            
+                            <span class="subgroup">
+                        	<span><?php echo t('collection');?>:</span>
+                            <span>
                                 <!-- repository ownership -->
                                 <?php if ($row['repositories']):?>
                                     <?php foreach($row['repositories'] as $repo):?>
@@ -153,48 +163,44 @@ $qs_sort=array('ps','title','idno','published','nation','tag','no_question','no_
                                 	<span class="label label-primary"><?php echo strtoupper($row['repositoryid']);?></span>
                               <?php endif;?>  
                             </span>
-                        </div>
-                        
-                        <div class="table-row">
-                        	<span class="cell-label"><?php echo t('modified_on')?>:</span>
-							<span class="cell-value"><?php echo date($this->config->item('date_format'), $row['changed']); ?></span>
-                        </div>
-                        
-                        <?php if (isset($row['tags']) &&  count($row['tags'])>0):?>
-                        <div class="table-row">
-                        	<span class="cell-label"><?php echo t('tags');?>:</span>
-							<span class="cell-value">
-								<?php foreach($row['tags'] as $tag):?>
-                                    <span class="label label-info"><?php echo $tag;?></span>
-                                <?php endforeach;?>
                             </span>
-                        </div>                                                            
-                        <?php endif;?>
-                        
-                        <div class="actions">
-                        	<div class="status">
-                                <?php $published_checked=(!$row['published']) ? '' :'checked="checked"';?>                             
-                                <input class="publish-toggle" type="checkbox" data-sid="<?php echo $row['id'];?>" <?php echo $published_checked;?> data-toggle="toggle" data-on="<?php echo t('published');?>" data-off="<?php echo t('draft');?>" data-onstyle="success" data-offstyle="danger">
-                            </div>
+
                             
-                            <?php if (isset($row['citations'])):?>
-                            <div class="info"><span class="badge badge-info"><?php echo $row['citations'];?></span> <?php echo t('citations');?></div>
-                            <?php endif;?>
-                            <?php if (isset($row['pending_lic_requests'])):?>
-                            	<div class="info"><span class="badge badge-warning"><?php echo $row['pending_lic_requests'];?></span> <?php echo t('pending requests');?></div>
+                            <?php if (isset($row['tags']) &&  count($row['tags'])>0):?>
+                            <span class="subgroup">
+                                <span><?php echo t('tags');?>:</span>
+                                <span>
+                                    <?php foreach($row['tags'] as $tag):?>
+                                        <span class="label label-warning"><?php echo $tag;?></span>
+                                    <?php endforeach;?>
+                                </span>                                                       
+                            </span>
                             <?php endif;?>
                             
                         </div>
+                        
+                        
+                        
                         
                         <div class="links">
                         
                         <span style="float:left;">
+                        <span class="subgroup">
+                            <span class="date-changed">
+                                <?php echo t('modified_on')?>:    
+                                <?php echo date($this->config->item('date_format'), $row['changed']); ?>
+                            </span>
+                        </span>
+
                             <span><a href="<?php echo site_url();?>/admin/catalog/edit/<?php echo $row['id'];?>"><?php echo t('edit');?></a></span> | 
                             <?php if($study_ownership=='owned'):?>
                             	<span><a href="<?php echo site_url();?>/admin/catalog/delete/<?php echo $row['id'];?>"><?php echo t('delete');?></a></span>
                             <?php elseif($study_ownership=='linked'):?>
                             	<span><a title="<?php echo t('remove_from_collection_description');?>" href="<?php echo site_url('/admin/catalog/unlink/'.$active_repo_obj->repositoryid.'/'.$row['id']);?>"><?php echo t('remove_from_collection');?></a></span>
-                            <?php endif;?>                                
+                            <?php endif;?>
+                            
+                            
+
                         </span>
                         
                         <span class="survey-options">
@@ -203,7 +209,19 @@ $qs_sort=array('ps','title','idno','published','nation','tag','no_question','no_
                         
                         </div>
                     </div>
-            </td>            
+            </td>
+            <td class="col-published">
+                        <div class="actions">
+                        	<div class="status">
+                                <?php $published_checked=(!$row['published']) ? '' :'checked="checked"';?>                             
+                                <input class="publish-toggle" type="checkbox" data-sid="<?php echo $row['id'];?>" <?php echo $published_checked;?> data-toggle="toggle" data-on="<?php echo t('published');?>" data-off="<?php echo t('draft');?>" data-onstyle="success" data-offstyle="danger">
+                            </div>
+                            
+                            <?php if (isset($row['pending_lic_requests'])):?>
+                            	<div class="info"><span class="badge badge-warning"><?php echo $row['pending_lic_requests'];?></span> <?php echo t('pending requests');?></div>
+                            <?php endif;?>                            
+                        </div>
+            </td>
         </tr>
     <?php endforeach;?>
 </table>    
