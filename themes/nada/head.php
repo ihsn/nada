d2e4608b2d3b09bfe6e217d0c9fef2b2547cedef
Mehmood Asghar <mah0001@gmail.com>
2019-07-22 11:28:27 -0400

    Add option to load JS libraries from CDN

--- themes/nada/head.php
+++ themes/nada/head.php
@@ -22,13 +22,15 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <!--<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>-->
 
-<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+
 <!--    <script src="//code.jquery.com/jquery-migrate-3.0.1.min.js"></script>-->
 
 <?php if($use_cdn):?>
+    <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <?php else:?>
+    <script src="<?php echo base_url(); ?>/javascript/jquery/jquery.js"></script>
     <script src="<?php echo base_url().$bootstrap_theme ?>/js/popper.min.js"></script>
     <script src="<?php echo base_url().$bootstrap_theme ?>/js/bootstrap.min.js"></script>
 <?php endif;?>
