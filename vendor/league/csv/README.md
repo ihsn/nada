7150bc709f494bae57baf04816a92145ae76b0c5
Mehmood Asghar <mah0001@gmail.com>
2019-08-13 15:49:36 -0400

    add phpleague/csv library

---
+++ vendor/league/csv/README.md
@@ -0,0 +1,100 @@
+CSV
+=====
+
+[![Latest Version](https://img.shields.io/github/release/thephpleague/csv.svg?style=flat-square)](https://github.com/thephpleague/csv/releases)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+[![Build Status](https://img.shields.io/travis/thephpleague/csv/master.svg?style=flat-square)](https://travis-ci.org/thephpleague/csv)
+[![Coverage Status](https://img.shields.io/scrutinizer/coverage/g/thephpleague/csv.svg?style=flat-square)](https://scrutinizer-ci.com/g/thephpleague/csv/code-structure)
+[![Quality Score](https://img.shields.io/scrutinizer/g/thephpleague/csv.svg?style=flat-square)](https://scrutinizer-ci.com/g/thephpleague/csv)
+[![Total Downloads](https://img.shields.io/packagist/dt/league/csv.svg?style=flat-square)](https://packagist.org/packages/league/csv)
+
+Csv is a simple library to ease CSV parsing, writing and filtering in
+PHP. The goal of the library is to be powerful while remaining lightweight,
+by utilizing PHP native classes whenever possible.
+
+Highlights
+-------
+
+* Simple API
+* Read and Write to CSV documents in a memory efficient and scalable way
+* Use SPL to interact with the CSV documents
+* Support PHP Stream filtering capabilities
+* Transform CSV documents into popular format (JSON, XML or HTML)
+* Fully documented
+* Fully unit tested
+* Framework-agnostic
+* Composer ready, [PSR-2] and [PSR-4] compliant
+
+Documentation
+-------
+
+Full documentation can be found at [csv.thephpleague.com](https://csv.thephpleague.com).
+
+System Requirements
+-------
+
+You need **PHP >= 7.0.10** and the `mbstring` extension to use `Csv` but the latest stable version of PHP is recommended.
+
+Install
+-------
+
+Install `Csv` using Composer.
+
+```
+$ composer require league/csv
+```
+
+Configuration
+-------
+
+**Warning:** If your CSV document was created or is read on a Macintosh computer, add the following lines before using the library to help [PHP detect line ending](http://php.net/manual/en/function.fgetcsv.php#refsect1-function.fgetcsv-returnvalues).
+
+```php
+if (!ini_get("auto_detect_line_endings")) {
+    ini_set("auto_detect_line_endings", '1');
+}
+```
+
+Testing
+-------
+
+`League\Csv` has a :
+
+- a [PHPUnit](https://phpunit.de) test suite
+- a coding style compliance test suite using [PHP CS Fixer](http://cs.sensiolabs.org/).
+- a code analysis compliance test suite using [PHPStan](https://github.com/phpstan/phpstan).
+
+To run the tests, run the following command from the project folder.
+
+``` bash
+$ composer test
+```
+
+Contributing
+-------
+
+Contributions are welcome and will be fully credited. Please see [CONTRIBUTING](.github/CONTRIBUTING.md) and [CONDUCT](CONDUCT.md) for details.
+
+Security
+-------
+
+If you discover any security related issues, please email nyamsprod@gmail.com instead of using the issue tracker.
+
+Changelog
+-------
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+Credits
+-------
+
+- [ignace nyamagana butera](https://github.com/nyamsprod)
+- [All Contributors](https://github.com/thephpleague/csv/graphs/contributors)
+
+License
+-------
+
+The MIT License (MIT). Please see [LICENSE](LICENSE) for more information.
+
+[PSR-2]: http://www.php-fig.org/psr/psr-2/
+[PSR-4]: http://www.php-fig.org/psr/psr-4/
---
+++ vendor/league/csv/src/HTMLConverter.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * League.Csv (https://csv.thephpleague.com)
+ *
+ * (c) Ignace Nyamagana Butera <nyamsprod@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace League\Csv;
+
+use DOMDocument;
+use DOMElement;
+use DOMException;
+use Traversable;
+use function preg_match;
+
+/**
+ * Converts tabular data into an HTML Table string.
+ */
+class HTMLConverter
+{
+    /**
+     * table class attribute value.
+     *
+     * @var string
+     */
+    protected $class_name = 'table-csv-data';
+
+    /**
+     * table id attribute value.
+     *
+     * @var string
+     */
+    protected $id_value = '';
+
+    /**
+     * @var XMLConverter
+     */
+    protected $xml_converter;
+
+    /**
+     * New Instance.
+     */
+    public function __construct()
+    {
+        $this->xml_converter = (new XMLConverter())
+            ->rootElement('table')
+            ->recordElement('tr')
+            ->fieldElement('td')
+        ;
+    }
+
+    /**
+     * Converts a tabular data collection into a HTML table string.
+     *
+     * @param array|Traversable $records       The tabular data collection
+     * @param string[]          $header_record An optional array of headers outputted using the`<thead>` section
+     * @param string[]          $footer_record An optional array of footers to output to the table using `<tfoot>` and `<th>` elements
+     */
+    public function convert($records, array $header_record = [], array $footer_record = []): string
+    {
+        $doc = new DOMDocument('1.0');
+        if ([] === $header_record && [] === $footer_record) {
+            $table = $this->xml_converter->import($records, $doc);
+            $this->addHTMLAttributes($table);
+            $doc->appendChild($table);
+
+            return $doc->saveHTML();
+        }
+
+        $table = $doc->createElement('table');
+        $this->addHTMLAttributes($table);
+        $this->appendHeaderSection('thead', $header_record, $table);
+        $this->appendHeaderSection('tfoot', $footer_record, $table);
+        $table->appendChild($this->xml_converter->rootElement('tbody')->import($records, $doc));
+        $doc->appendChild($table);
+
+        return $doc->saveHTML();
+    }
+
+    /**
+     * Creates a DOMElement representing a HTML table heading section.
+     */
+    protected function appendHeaderSection(string $node_name, array $record, DOMElement $table)
+    {
+        if ([] === $record) {
+            return;
+        }
+
+        $node = $this->xml_converter
+            ->rootElement($node_name)
+            ->recordElement('tr')
+            ->fieldElement('th')
+            ->import([$record], $table->ownerDocument)
+        ;
+
+        /** @var DOMElement $element */
+        foreach ($node->getElementsByTagName('th') as $element) {
+            $element->setAttribute('scope', 'col');
+        }
+
+        $table->appendChild($node);
+    }
+
+    /**
+     * Adds class and id attributes to an HTML tag.
+     */
+    protected function addHTMLAttributes(DOMElement $node)
+    {
+        $node->setAttribute('class', $this->class_name);
+        $node->setAttribute('id', $this->id_value);
+    }
+
+    /**
+     * HTML table class name setter.
+     *
+     * @throws DOMException if the id_value contains any type of whitespace
+     */
+    public function table(string $class_name, string $id_value = ''): self
+    {
+        if (1 === preg_match(",\s,", $id_value)) {
+            throw new DOMException("the id attribute's value must not contain whitespace (spaces, tabs etc.)");
+        }
+        $clone = clone $this;
+        $clone->class_name = $class_name;
+        $clone->id_value = $id_value;
+
+        return $clone;
+    }
+
+    /**
+     * HTML tr record offset attribute setter.
+     */
+    public function tr(string $record_offset_attribute_name): self
+    {
+        $clone = clone $this;
+        $clone->xml_converter = $this->xml_converter->recordElement('tr', $record_offset_attribute_name);
+
+        return $clone;
+    }
+
+    /**
+     * HTML td field name attribute setter.
+     */
+    public function td(string $fieldname_attribute_name): self
+    {
+        $clone = clone $this;
+        $clone->xml_converter = $this->xml_converter->fieldElement('td', $fieldname_attribute_name);
+
+        return $clone;
+    }
+}
