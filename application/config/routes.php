e2facc02bd25215b53ec11fe64c73930b5f50d4c
Mehmood Asghar <mah0001@gmail.com>
2020-06-12 22:04:04 -0400

    Add routes for variable groups to support old url format with vargrp

--- application/config/routes.php
+++ application/config/routes.php
@@ -132,9 +132,11 @@ $route['catalog/(:num)/data-dictionary'] = "study/data_dictionary/$1";
 $route['catalog/(:num)/data_dictionary'] = "study/data_dictionary/$1";
 $route['catalog/(:num)/variable-groups'] = "study/variable_groups/$1";
 $route['catalog/(:num)/variable_groups'] = "study/variable_groups/$1";
+$route['catalog/(:num)/vargrp'] = "study/variable_groups/$1";
 
 $route['catalog/(:num)/variable-groups/(.*)'] = "study/variable_groups/$1/$2";
 $route['catalog/(:num)/variable_groups/(.*)'] = "study/variable_groups/$1/$2";
+$route['catalog/(:num)/vargrp/(.*)'] = "study/variable_groups/$1/$2";
 
 //data file page
 $route['catalog/(:num)/data-dictionary/(.*)'] = "study/data_file/$1/$2";
