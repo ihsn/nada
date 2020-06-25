62a473f93b9bffccb3dadc75440ad372e3a295cc
Mehmood Asghar <mah0001@gmail.com>
2020-06-25 15:18:13 -0400

    Add array helper functions for working with nested arrays

--- application/helpers/array_helper.php
+++ application/helpers/array_helper.php
@@ -36,28 +36,162 @@ if ( ! function_exists('array_get_value'))
 }
 
 
+
+
 /**
- * 
- * 
- * return an array with the nested path and value
- * 
- * @data - array that needs to be searched
- * @path - xpath like path to data e.g. study_desc/title_statement/title or study_desc.title_statement.title 
- * 
- * 
- **/ 
+ * @param array $array
+ * @param array|string $parents
+ * @param string $glue
+ * @return mixed
+ */
 if ( ! function_exists('get_array_nested_value'))
 {
-    function get_array_nested_value($data, $path, $glue = '/')
+    function get_array_nested_value(array &$array, $parents, $glue = '/')
     {
-        $paths = explode($glue, (string) $path);
-        $reference = $data;
-        foreach ($paths as $key) {
-            if (!array_key_exists($key, $reference)) {
-                return false;
-            }
-            $reference = $reference[$key];
+        if (!is_array($parents)) {
+            $parents = explode($glue, $parents);
         }
-        return $reference;
+
+        $ref = &$array;
+
+        foreach ((array) $parents as $parent) {
+            if (is_array($ref) && array_key_exists($parent, $ref)) {
+                $ref = &$ref[$parent];
+            } else {
+                return null;
+            }
+        }
+        return $ref;
+    }
+}
+
+
+/**
+ * @param array $array
+ * @param array|string $parents
+ * @param mixed $value
+ * @param string $glue
+ */
+if ( ! function_exists('set_array_nested_value'))
+{
+    function set_array_nested_value(array &$array, $parents, $value, $glue = '/')
+    {
+        if (!is_array($parents)) {
+            $parents = explode($glue, (string) $parents);
+        }
+
+        $ref = &$array;
+
+        foreach ($parents as $parent) {
+            if (isset($ref) && !is_array($ref)) {
+                $ref = array();
+            }
+
+            $ref = &$ref[$parent];
+        }
+
+        $ref = $value;
+    }
+}
+
+
+/**
+ * @param array $array
+ * @param array|string $parents
+ * @param string $glue
+ */
+if ( ! function_exists('array_unset_value'))
+{
+    function array_unset_value(&$array, $parents, $glue = '.')
+    {
+        if (!is_array($parents)) {
+            $parents = explode($glue, $parents);
+        }
+
+        $key = array_shift($parents);
+
+        if (empty($parents)) {
+            unset($array[$key]);
+        } else {
+            array_unset_value($array[$key], $parents);
+        }
+    }
+}
+
+
+if ( ! function_exists('array_remove_nulls'))
+{
+    function array_remove_nulls(array &$array)
+    {
+        foreach ($array as $key => &$value) {
+            if (is_array($value)) {
+                $value = array_remove_nulls($value);
+            }
+            if ($value===null) {
+                unset($array[$key]);
+            }
+        }
+        return $array;
+    }
+}
+
+
+
+if ( ! function_exists('is_indexed_array'))
+{
+    //check if array has numeric keys
+    function is_indexed_array($arr)
+    {
+        if (!is_array($arr)){
+            return false;
+        }
+        if (array() === $arr) return false;
+        $k = array_keys( $arr ); 
+        return $k === array_keys( $k );
+    }
+}
+
+
+if ( ! function_exists('is_simple_indexed_array'))
+{
+    //array has numeric keys and contain no sub arrays
+    function is_simple_indexed_array($arr)
+    {
+        $indexed=is_indexed_array($arr);
+
+        if ($indexed==true){
+            foreach($arr as $key=>$value){
+                foreach($value as $child){
+                    if (is_array($child)){
+                        return false;
+                    }
+                }
+            }        
+        }
+
+        return $indexed;
+    }
+}
+
+if ( ! function_exists('array_indexed_elements'))
+{
+    //return array nodes with simple arrays
+    function array_indexed_elements($arr,$parent=null)
+    {
+        $output=array();
+        foreach($arr as $node_key=>$node){
+
+            $el_path=implode("/",array_filter(array($parent, $node_key)));
+
+            if(is_simple_indexed_array($node)){
+                $output[]= $el_path;
+            }
+
+            if (is_array($node)){
+                $output=array_merge($output,array_indexed_elements($node,$el_path));
+            }
+        }
+
+        return $output;
     }
 }
