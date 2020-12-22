e2b75eb4fe2af506a52b8d640a74af113b3e29d0
Mehmood Asghar <mah0001@gmail.com>
2020-12-22 17:23:48 -0500

    Add multiple endpoints for collections api

--- application/controllers/api/Collections.php
+++ application/controllers/api/Collections.php
@@ -36,6 +36,9 @@ class Collections extends MY_REST_Controller
 				'thumbnail'=>'thumbnail',
 				'short_text'=>'short_text',
 				'long_text'=>'long_text',
+				'ispublished'=>'ispublished',
+				'weight'=>'weight',
+				'section'=>'section'
 			);
 
 			foreach($repos as $row){
@@ -57,6 +60,227 @@ class Collections extends MY_REST_Controller
 		}
 		catch(Exception $e){
 			$this->set_response($e->getMessage(), REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+
+	/**
+	 * 
+	 * 
+	 * Create new collection
+	 * 
+	 * 
+	 */
+	function index_post()
+	{
+		$this->is_admin_or_die();
+
+		//multipart/form-data
+		$options=$this->input->post(null, true);
+
+		//raw json input
+		if (empty($options)){
+			$options=$this->raw_json_input();
+		}
+
+		try{
+			$user_id=$this->get_api_user_id();
+			
+			//validate
+			$this->Repository_model->validate($options);
+
+			$upload_result=null;
+
+			if(!empty($_FILES)){
+				//upload file?
+				$upload_result=$this->Repository_model->upload_thumbnail('thumbnail');
+				//set path to uploaded file
+				$options['thumbnail']=$upload_result['rel_path'];
+			}
+
+			$collection=$this->Repository_model->insert($options);
+
+			if(!$collection){
+				throw new Exception("FAILED_TO_CREATE_COLLECTION");
+			}
+
+			$response=array(
+				'status'=>'success',
+				'collection'=>$this->Repository_model->select_single($options['repositoryid']),
+				'upload'=>$upload_result
+			);
+
+			$this->set_response($response, REST_Controller::HTTP_OK);
+		}
+		catch(ValidationException $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage(),
+				'errors'=>(array)$e->GetValidationErrors()
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}
+		catch(Exception $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage()				
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+
+
+
+	/**
+	 * 
+	 * 
+	 * Update collection
+	 * 
+	 * 
+	 */
+	function update_post()
+	{
+		$this->is_admin_or_die();
+
+		//multipart/form-data
+		$options=$this->input->post(null, true);
+
+		//raw json input
+		if (empty($options)){
+			$options=$this->raw_json_input();
+		}
+
+		try{
+			$user_id=$this->get_api_user_id();
+			
+			if(!isset($options['repositoryid'])){
+				throw new Exception("parameter `repositoryid` is missing");
+			}
+
+			$repository=$this->Repository_model->get_repository_by_repositoryid($options['repositoryid']);
+			
+			if(!$repository){
+				throw new Exception("Repository not found:: " .$options['repositoryid']);
+			}
+
+			$options=array_merge($repository,$options);
+
+			//validate
+			$this->Repository_model->validate($options);
+
+			$upload_result=null;
+
+			if(!empty($_FILES)){
+				//upload file?
+				$upload_result=$this->Repository_model->upload_thumbnail('thumbnail');
+				//set path to uploaded file
+				$options['thumbnail']=$upload_result['rel_path'];
+			}
+
+			$collection=$this->Repository_model->update($options['id'],$options);
+
+			if(!$collection){
+				throw new Exception("FAILED_TO_UPDATE_COLLECTION");
+			}
+
+			$response=array(
+				'status'=>'success',
+				'collection'=>$this->Repository_model->select_single($repository['id']),
+				'upload'=>$upload_result
+			);
+
+			$this->set_response($response, REST_Controller::HTTP_OK);
+		}
+		catch(ValidationException $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage(),
+				'errors'=>(array)$e->GetValidationErrors(),
+				'options'=>$options
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}
+		catch(Exception $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage()				
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}
+	}
+
+
+
+
+	/**
+	 * 
+	 * 
+	 * Rename collection ID
+	 * 
+	 * 
+	 */
+	function rename_post()
+	{
+		$this->is_admin_or_die();
+
+		//multipart/form-data
+		$options=$this->input->post(null, true);
+
+		//raw json input
+		if (empty($options)){
+			$options=$this->raw_json_input();
+		}
+
+		try{
+			$user_id=$this->get_api_user_id();
+			
+			if(!isset($options['old_repositoryid'])){
+				throw new Exception("parameter `old_repositoryid` is missing");
+			}
+
+			if(!isset($options['new_repositoryid'])){
+				throw new Exception("parameter `new_repositoryid` is missing");
+			}
+
+			$repository=$this->Repository_model->get_repository_by_repositoryid($options['old_repositoryid']);
+
+			if(!$repository){
+				throw new Exception("Repository not found:: " .$options['repositoryid']);
+			}
+
+			//set repositoryid to new id
+			$repository['repositoryid']=$options['new_repositoryid'];			
+
+			//validate
+			$this->Repository_model->validate($repository);
+
+			$result=$this->Repository_model->rename_repository($options['old_repositoryid'],$options['new_repositoryid']);
+
+			if(!$result){
+				throw new Exception("FAILED_TO_UPDATE_COLLECTION");
+			}
+
+			$response=array(
+				'status'=>'success',
+				'collection'=>$this->Repository_model->select_single($options['new_repositoryid'])
+			);
+
+			$this->set_response($response, REST_Controller::HTTP_OK);
+		}
+		catch(ValidationException $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage(),
+				'errors'=>(array)$e->GetValidationErrors(),
+				'options'=>$options
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}
+		catch(Exception $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage()				
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
 		}
 	}
 
@@ -87,10 +311,17 @@ class Collections extends MY_REST_Controller
 				'title'=>$repo['title'],
 				'short_text'=>$repo['short_text'],
 				'long_text'=>$repo['long_text'],
-				'thumbnail'=>$repo['thumbnail']
+				'thumbnail'=>$repo['thumbnail'],
+				'weight'=>$repo['weight'],
+				'ispublished'=>$repo['ispublished']
+			);
+
+			$response=array(
+				'status'=>'success',
+				'collection'=>$repo,
 			);
 			
-			$this->set_response($repo, REST_Controller::HTTP_OK);			
+			$this->set_response($response, REST_Controller::HTTP_OK);			
 		}
 		catch(Exception $e){
 			$error_output=array(
