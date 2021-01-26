77a0c97a81993d63ee29910b893fa0ed7eea57b2
Mehmood Asghar <mah0001@gmail.com>
2021-01-25 19:03:31 -0500

    Update reports api + documentation

--- application/controllers/api/Reports.php
+++ application/controllers/api/Reports.php
@@ -28,55 +28,40 @@ class Reports extends MY_REST_Controller
 	}
 	
 
-	/**
-	 * 
-	 * list files attached to a dataset
-	 * 
-	 * 
-	 *	Published datasets
-	 * # of datasets published per month & year
-	 * # of published datasets by catalog (region)
-	 * # of published datasets by country
-	 * # of published datasets by topic
-	**/
-	function datasets_get($limit=10,$offset=0)
-	{
-		try{
-			/*if($idno){
-				return $this->single_get($idno);
-			}*/
-
-			$offset=(int)$offset;
-			$limit=(int)$limit;
-			
-			$result=$this->dataset_manager->get_all($limit,$offset);
-			array_walk($result, 'unix_date_to_gmt',array('created','changed'));				
-			$response=array(
-				'status'=> 'success',
-				'total'=> $this->dataset_manager->get_total_count(),
-				'found'=> count($result),
-				'offset'=> $offset,
-				'limit'=> $limit,
-				'datasets'=> $result
-			);		
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}
-	}
-
 
 	//datasets published per month
-	function datasets_published_get()
+	/**
+	 * 
+	 * Reports by published datasets
+	 * 
+	 * 
+	 * # of datasets published per month & year
+	 * # of published datasets by catalog collection
+	 * # of published datasets by country
+	 * # of published datasets by topic
+	 * 
+	 * 
+	 * @type - month, collection, country, topic
+	 * 
+	**/
+	function datasets_published_get($type='month')
 	{
 		try{
-
-			$result=$this->Reports_model->get_datasets_published();
+			if ($type=='month'){
+				$result=$this->Reports_model->get_datasets_published();
+			}
+			else if($type=='collection'){
+				$result=$this->Reports_model->get_datasets_published_by_collection();
+			}
+			else if($type=='country'){
+				$result=$this->Reports_model->get_datasets_published_by_country();
+			}
+			else if($type=='topic'){
+				throw new exception("not implemented yet!");
+			}
+			else{
+				throw new Exception("Valid options for 'type' param are: month, collection, country");
+			}
 			
 			if($this->input->get("format")=="csv"){
 				$response=$result;
@@ -84,6 +69,7 @@ class Reports extends MY_REST_Controller
 			else{
 				$response=array(
 					'status'=> 'success',
+					'type'=>$type,
 					'result'=> $result
 				);	
 			}
@@ -97,151 +83,55 @@ class Reports extends MY_REST_Controller
 			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
 		}		
 	}
-
-	//datasets summary by collection
-	function datasets_by_collection_get()
-	{
-		try{
-
-			$result=$this->Reports_model->get_datasets_published_by_collection();
-			
-			if($this->input->get("format")=="csv"){
-				$response=$result;
-			}
-			else{
-				$response=array(
-					'status'=> 'success',
-					'result'=> $result
-				);	
-			}
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}		
-	}
-
-
-	//datasets summary by country
-	function datasets_by_country_get()
-	{
-		try{
-
-			$result=$this->Reports_model->get_datasets_published_by_country();
-			
-			if($this->input->get("format")=="csv"){
-				$response=$result;
-			}
-			else{
-				$response=array(
-					'status'=> 'success',
-					'result'=> $result
-				);	
-			}
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}		
-	}
-
-
-	//datasets summary by country
-	function licensed_requests_summery_get()
-	{
-		try{
-
-			$result=$this->Reports_model->get_licensed_requests_by_month();
-			
-			if($this->input->get("format")=="csv"){
-				$response=$result;
-			}
-			else{
-				$response=array(
-					'status'=> 'success',
-					'result'=> $result
-				);	
-			}
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}		
-	}
-
-
-	//datasets summary by country
-	function licensed_requests_by_collection_summary_get()
-	{
-		try{
-
-			$result=$this->Reports_model->get_licensed_requests_by_collection();
-			
-			if($this->input->get("format")=="csv"){
-				$response=$result;
-			}
-			else{
-				$response=array(
-					'status'=> 'success',
-					'result'=> $result
-				);	
-			}
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}		
-	}
-
-
-	//datasets summary by country
-	function licensed_requests_by_country_summery_get()
-	{
-		try{
-
-			$result=$this->Reports_model->get_licensed_requests_by_country();
-			
-			if($this->input->get("format")=="csv"){
-				$response=$result;
-			}
-			else{
-				$response=array(
-					'status'=> 'success',
-					'result'=> $result
-				);	
-			}
-			$this->set_response($response, REST_Controller::HTTP_OK);
-		}
-		catch(Exception $e){
-			$error_output=array(
-				'status'=>'failed',
-				'message'=>$e->getMessage()
-			);
-			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
-		}		
-	}
-
 
 	
 
+	/**
+	 * 
+	 * 
+	 * Reports for Licensed Requests
+	 * 
+	 * - type - month, country, collection
+	 * 
+	 * 
+	 */
+	function licensed_requests_get($type='month')
+	{
+		try{
+			if ($type=='month'){
+				$result=$this->Reports_model->get_licensed_requests_by_month();
+			}
+			else if ($type=='country'){
+				$result=$this->Reports_model->get_licensed_requests_by_country();
+			}
+			else if ($type=='collection'){
+				$result=$this->Reports_model->get_licensed_requests_by_collection();
+			}
+			else{
+				throw new Exception("Valid options for 'type' param are: month, collection, country");
+			}
+			
+			if($this->input->get("format")=="csv"){
+				$response=$result;
+			}
+			else{
+				$response=array(
+					'status'=> 'success',
+					'type'=>$type,
+					'result'=> $result
+				);	
+			}
+			$this->set_response($response, REST_Controller::HTTP_OK);
+		}
+		catch(Exception $e){
+			$error_output=array(
+				'status'=>'failed',
+				'message'=>$e->getMessage()
+			);
+			$this->set_response($error_output, REST_Controller::HTTP_BAD_REQUEST);
+		}		
+	}
 
-	
 
 	
 }
