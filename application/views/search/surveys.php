2d4725749a08a2a594dc3bb72c438e919fe095d3
Mehmood Asghar <mah0001@gmail.com>
2021-01-26 13:14:05 -0500

    show license icon for microdata type entries in the catalog

--- application/views/search/surveys.php
+++ application/views/search/surveys.php
@@ -162,15 +162,16 @@ else{
     ?>
     <div class="survey-row" data-url="<?php echo site_url('catalog/'.$row['id']); ?>" title="<?php echo t('View study');?>">
     <div class="row">
-        <div class="col-2 col-lg-1">            
-        <?php /* <i class="icon-da icon-da-<?php echo $row['form_model'];?>" title="<?php echo t("legend_data_".$row['form_model']);?>"></i> */?>
-            <span class="fa-stack fa-lg fa-nada-<?php echo $row['type'];?>">
+        <div class="col-2 col-lg-1">
+            <i class="icon-da icon-da-<?php echo $row['form_model'];?>" title="<?php echo t("legend_data_".$row['form_model']);?>"></i>
+            <?php /* <i class="icon-da icon-da-<?php echo $row['form_model'];?>" title="<?php echo t("legend_data_".$row['form_model']);?>"></i> */?>
+            <?php /*<span class="fa-stack fa-lg fa-nada-<?php echo $row['type'];?>">
             <i class="fa fa-circle fa-stack-2x"></i>
             <i class="fa <?php echo $type_icons[$row['type']];?> fa-stack-1x fa-inverse fa-nada-icon"></i>
-            </span> 
+            </span> */?>
         </div>        
         
-        <div class="col-10 col-lg-11">            
+        <div class="col-10 col-lg-11">
             <h5 class="title">
                 <a href="<?php echo site_url('catalog/'.$row['id']); ?>"  title="<?php echo $row['title']; ?>" >                
                     <?php echo $row['title'];?> 
