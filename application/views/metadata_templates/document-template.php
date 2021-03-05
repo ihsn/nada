3d7803a13ff335a50343b61d299c3d6dacce1755
Mehmood Asghar <mah0001@gmail.com>
2021-03-05 14:00:18 -0500

    Add document status field to display page

--- application/views/metadata_templates/document-template.php
+++ application/views/metadata_templates/document-template.php
@@ -81,6 +81,7 @@ $metadata['metadata']['resources']=$metadata['resources'];
         "metadata.document_description.title_statement.alternate_title"=>"text",
         "metadata.document_description.title_statement.abbreviated_title"=>"text",
         "metadata.document_description.type"=>"text",
+        "metadata.document_description.status"=>"text",
         "metadata.document_description.description"=>"text",
         "metadata.document_description.toc"=>"text",
         "metadata.document_description.toc_structured"=>"array",
