c55a5562f5c3c270401fdba9639df5e74bffc745
Mehmood Asghar <mah0001@gmail.com>
2021-04-05 13:41:23 -0400

    minor

--- application/views/catalog/data_access.php
+++ application/views/catalog/data_access.php
@@ -1,7 +1,12 @@
 <?php
     $selected_data_class= get_form_value("data_class_id",isset($data_class_id) ? $data_class_id : '');
     $selected_formid= get_form_value("formid",isset($formid) ? $formid : '');
+
 ?>
+
+
+<?php $data_access_list=array();?>
+
 <style>
 .info{
     font-style:italic;
@@ -12,7 +17,7 @@
 <form method="post" id="form-data-classification" action="<?php echo site_url('/admin/catalog/update');?>">
 
     <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-6">
     <input type="hidden" name="sid" value="<?php echo $id;?>"/>
 
     <div class="form-group"> 
@@ -21,7 +26,11 @@
         <select name="data-classification-code" id="data-classifications" class="form-control">            
             <option value="" data-target="select-none" >- Select -</option>
             <?php foreach($data_classifications as $class_code=>$class_row):?>
-                <option value="<?php echo $class_row['id'];?>" data-target="select-<?php echo $class_code;?>" ><?php echo t($class_row['title']);?></option>                
+                <option 
+                    value="<?php echo $class_row['id'];?>" 
+                    data-code="<?php echo $class_code;?>"
+                    <?php if ($class_row['id']==$data_class_id){ echo 'selected="selected"'; }?>
+                 ><?php echo t($class_row['title']);?></option>                
             <?php endforeach;?>
         </select>
 
@@ -33,60 +42,87 @@
             <?php endforeach;?>
         <?php endif;?>
     </div>
-    
+
     </div>
-    <div class="col-md-4">
+    <div class="col-md-6">
         <label><?php echo t('data_access');?></label>
-        <select  id="select-none" class="form-control data-access-dropdown">
-            <option>- Select -</option>
-        </select>
-        <?php foreach($data_access_options as $classification_code=>$data_licenses):?>            
-            <select style="display:none"  name="<?php echo $classification_code;?>" id="select-<?php echo $classification_code;?>" class="data-access-dropdown form-control">
-                <option> - Select - </option>
-                <?php foreach($data_licenses as $license_):?>
-                    <?php if(isset($data_access_types[$license_])):?>
-                    <option 
-                        value="<?php echo $data_access_types[$license_]['formid'];?>"
-                        <?php echo ($selected_formid==$data_access_types[$license_]['formid']) ? 'selected="selected"' :'';?>
-                    ><?php echo t($data_access_types[$license_]['fname']);?></option>
-                    <?php endif;?>
-                <?php endforeach;?>
-            </select>
-        <?php endforeach;?>
-        
-        <?php if(isset($formid)):?>
-            <span class="info"><?php echo $this->forms_list[$formid];?></span>
+        <div class="data-access-container">
+            <?php echo $data_access_dropdown;?>
+        </div>
+                
+        <?php if(isset($formid) && isset($data_licenses[$formid])):?>
+            <span class="info"><?php echo $data_licenses[$formid];?></span>
         <?php endif;?>
     </div>
 
-    <div class="col-md-4">
-        <div class="form-group" style="margin-top:25px;">            
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 form-group link-da">
+            <label for="link_da"><?php echo t('remote_data_access_url');?></label>
+            <input name="link_da" type="text" id="link_da" class="form-control" value="<?php echo get_form_value('link_da',isset($link_da) ? $link_da : ''); ?>"/>
+        </div>
+
+        <div class="col-md-12 study-microdata model-<?php echo $model;?>">
+        <?php if (count($microdata_files)>0):?>
+                <div class="microdata-applies-to"><?php echo t('data_selection_apply_to_files');?></div>
+                        <ul>
+                        <?php foreach($microdata_files as $mf):?>
+                        <li><?php echo basename($mf['filename']);?></li>
+                        <?php endforeach;?>
+                        </ul>
+        <?php else:?>
+                <div class="text-danger"><span class="glyphicon glyphicon-alert red" aria-hidden="true"></span> <?php echo sprintf(t('study_no_data_files_assigned'),'');?></div>
+        <?php endif;?>
+        </div>
+    </div>
+
+    <div class="form-group" style="margin-top:25px;">            
             <div>
             <input type="submit" id="data-access-submit" value="<?php echo t('update');?>" name="submit" class="btn btn-primary"/>
             <!--<a href="<?php echo site_url('admin/catalog/edit/'.$id);?>" class="btn btn-link"><?php echo t('cancel');?></a>-->
             </div>
         </div>
-    </div>
 
-    </div>
+    
+
 </form>
 
 
-
 <script>
-$(document).ready(function(){
-    $('#data-classifications').on('change', function() {
-       var target=$(this).find(":selected").attr("data-target");
-	   var id=$(this).attr("id");
-       console.log(target);
-      $(".data-access-dropdown").hide();
-     $("#"+target).show();
-    });
 
-    <?php if($selected_data_class>0):?>
-        //set initial selection
-        $("#data-classifications").val(<?php echo $selected_data_class;?>).change()
-    <?php endif;?>
+//show/hide remote data access text box
+function sh_remote_da_link()
+{
+    if ($(".data-access-dropdown").val()==5){
+		$(".link-da").show();
+	}
+	else{
+		$(".link-da").hide();
+	}
+}
+
+function load_data_access_dropdown()
+{
+    let url=CI.base_url + '/admin/catalog/da_by_class/' + $('#data-classifications').val();
+    $.get(url, function( data ) {
+        $(".data-access-container").html(data);
+    });   
+}
+
+$(document).ready(function(){
+    //show/hide remote da url depending on the da form selected
+    $( "#form-data-classification" ).on( "change", ".data-access-dropdown", function() {
+        sh_remote_da_link();
+    });    
+    
+
+    //show/hide da
+    //sh_remote_da_link();
+    $('#data-classifications').on('change', function() {
+        load_data_access_dropdown();
+    });
+    
 
     $('#data-access-submit').on('click', function() {
        set_data_access();
@@ -95,12 +131,14 @@ $(document).ready(function(){
 
 
     function set_data_access(){
-        let data_class_target=$("#data-classifications").find(":selected").attr("data-target");
         data = {
             sid: <?php echo $id;?>,
             data_class_id: $("#data-classifications").val(),
-            formid: $('#'+data_class_target).val()
+            formid: $('#select-data-access').val(),
+            link_da: $("#link_da").val()
         };
+
+        console.log(data);
         url=CI.base_url+'/admin/catalog/update';
         $.ajax({
             type: "POST",
