f4f93c12d92ff181fe9af66bb8ff93170d3fb588
Mehmood Asghar <mah0001@gmail.com>
2021-04-05 11:16:47 -0400

    minor

--- application/views/metadata_templates/fields/field_photo.php
+++ application/views/metadata_templates/fields/field_photo.php
@@ -21,7 +21,7 @@
     <?php foreach($data as $index=>$resource):?>
         <?php if (in_array($resource['extension'],array('jpg','jpeg','gif','png'))):?>
         <div class="">            
-            <img src="<?php echo $resource['download_link'];?>" title="<?php echo $resource['title'];?>" class="img-fluid rounded shadow-sm resource-img">            
+            <img src="<?php echo $resource['download_link'];?>" title="<?php echo $resource['title'];?>" class="img-fluid rounded shadow-sm resource-img border">
         </div>
         <a href="<?php echo $resource['download_link'];?>" target="_blank"><i class="fa fa-download" aria-hidden="true"></i> <?php echo t('Download');?></a>
         <?php return;?>
