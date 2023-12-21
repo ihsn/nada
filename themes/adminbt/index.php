aba44be9f997aab1aeac6badc867eeb9370f25d5
Mehmood Asghar <mah0001@gmail.com>
2023-12-21 12:37:34 -0500

    Add browser cache expiration headers

--- themes/adminbt/index.php
+++ themes/adminbt/index.php
@@ -1,7 +1,9 @@
 <?php
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header("Pragma: no-cache");
+header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+header( 'Cache-Control: no-store, no-cache, must-revalidate, max-age=0' );
+header( 'Cache-Control: post-check=0, pre-check=0', false );
+header( 'Pragma: no-cache' );
 ?>
 <?php //include_once APPPATH.'/config/site_menus.php'; ?>
 <?php
--- themes/nada52/layout.php
+++ themes/nada52/layout.php
@@ -1,6 +1,10 @@
 <?php
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+header( 'Cache-Control: no-store, no-cache, must-revalidate, max-age=0' );
+header( 'Cache-Control: post-check=0, pre-check=0', false );
+header( 'Pragma: no-cache' );
+
 $menu_horizontal = TRUE;
 $bootstrap_theme = 'themes/' . $this->template->theme();
 
