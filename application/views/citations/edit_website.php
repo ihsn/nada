a007829a8c20fd249dff748c246cb09b744e921d
Mehmood Asghar <mah0001@gmail.com>
2024-01-18 14:22:12 -0500

    Fix for issue #87: Add publication date for website type citation

--- application/views/citations/edit_website.php
+++ application/views/citations/edit_website.php
@@ -10,10 +10,31 @@
 
 <?php echo form_author_field("author",'Author(s)'); ?>
 
-<div class="form-group">
-    <label for="data_accessed"><?php echo t('website_access_date');?></label>
-    <input name="data_accessed" type="text" id="data_accessed" size="10" maxlength="10" class="form-control" value="<?php echo get_form_value('data_accessed',isset($data_accessed) ? $data_accessed : ''); ?>"/>
+
+<div class="row mt-3 mb-3">
+    <div class="col-md-6">
+        <div><label><?php echo t('publication_day_month_year');?></label></div>
+        <div class="form-inline">
+            <div class="form-group">                
+                <div class="publications">
+                    <input name="pub_day" placeholder="DD" class="form-control" type="text" size="2" maxlength="2" value="<?php echo get_form_value('pub_day',isset($pub_day) ? $pub_day: ''); ?>"/> / 
+                    <input name="pub_month" placeholder="MM" type="text" class="form-control" size="10" maxlength="20"  value="<?php echo get_form_value('pub_month',isset($pub_month) ? $pub_month: ''); ?>"/> / 
+                    <input name="pub_year" placeholder="YYYY" type="text" class="form-control" size="4" maxlength="4"  value="<?php echo get_form_value('pub_year',isset($pub_year) ? $pub_year: ''); ?>"/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="data_accessed"><?php echo t('website_access_date');?></label>
+            <input name="data_accessed"  type="text" id="data_accessed" size="10" maxlength="10" class="form-control" value="<?php echo get_form_value('data_accessed',isset($data_accessed) ? $data_accessed : ''); ?>"/>
+        </div>
+    </div>
+
 </div>
+
+
 
 <div class="form-group">
 	<label for="url"><?php echo t('url');?></label>
