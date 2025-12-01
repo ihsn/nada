2d410d7132579383cbe83973961fa7a59ecebfc9
Mehmood Asghar <mah0001@gmail.com>
2025-11-30 21:18:07 -0500

    Update bots list

--- application/config/bots.php
+++ application/config/bots.php
@@ -13,17 +13,33 @@
 //enable/disable logging for search engines, by default everything is logged
 $config['ignore_bot_logging'] = FALSE;
 
-//user-agents used by search engines
+//user-agents used by search engines and bots
 $config['bot_ignore'][] = 'google';
 $config['bot_ignore'][] = 'ia_archiver';
 $config['bot_ignore'][] = 'jeeves/teoma';
 $config['bot_ignore'][] = 'googlebot';
 $config['bot_ignore'][] = 'bot.html';
 $config['bot_ignore'][] = 'msnbot';
-$config['bot_ignore'][] = 'googlebot';
 $config['bot_ignore'][] = 'search.msn.com';
 $config['bot_ignore'][] = 'msnbot.htm';
 $config['bot_ignore'][] = 'Yahoo! Slurp';
 $config['bot_ignore'][] = 'help.yahoo.com/help/us/ysearch/slurp';
+$config['bot_ignore'][] = 'bot';
+$config['bot_ignore'][] = 'spider';
+$config['bot_ignore'][] = 'crawler';
+$config['bot_ignore'][] = 'crawl';
+$config['bot_ignore'][] = 'slurp';
+$config['bot_ignore'][] = 'bingpreview';
+$config['bot_ignore'][] = 'wget';
+$config['bot_ignore'][] = 'curl';
+$config['bot_ignore'][] = 'python-requests';
+$config['bot_ignore'][] = 'axios';
+$config['bot_ignore'][] = 'headless';
+$config['bot_ignore'][] = 'playwright';
+$config['bot_ignore'][] = 'puppeteer';
+$config['bot_ignore'][] = 'lighthouse';
+$config['bot_ignore'][] = 'httpclient';
+$config['bot_ignore'][] = 'phantom';
+$config['bot_ignore'][] = 'node-superagent';
 
 ?>
