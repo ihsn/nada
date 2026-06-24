4e70c65cbae87b8092224c7e0490d24601ab5a0b
Mehmood Asghar <mah0001@gmail.com>
2026-06-24 20:59:59 +0200

    minor

--- application/config/routes.php
+++ application/config/routes.php
@@ -314,8 +314,7 @@ $route['switch_language/(.*)'] = "page/switch_language/$1";
 //forms {pubic,direct, etc}
 $route['forms/(.*)'] = "forms";
 
-//$route['embed/(.*)'] = "embed/index/$1";
-//$route['embed/(.*)'] = "embed/index/$1";
+$route['embed/catalog/(:num)/chart'] = 'embed/catalog_chart/$1';
 
 //admin paths
 $route['admin'] = "admin/admin";
